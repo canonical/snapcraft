175e3edbc449b2750f342830e9aadc04838d3ca6
Michael Terry <michael.terry@canonical.com>
2015-06-23 17:06:33 +0100

    A little more work

--- handlers/autotools.py
+++ handlers/autotools.py
@@ -1,20 +1,37 @@
 # -*- Mode:Python; indent-tabs-mode:t; tab-width:4 -*-
 
+import os
 import snapcraft
+import sys
 
 class AutotoolsHandler(snapcraft.BaseHandler):
-	def init():
-		pass
-	def pull():
-		# branches to self.sourcedir by default, will be copied to self.builddir
+	def __init__(self, name, options):
+		super().__init__(name, options)
+	def init(self):
+		super().init()
+	def pull(self):
+		super().pull()
 		self.pullBranch(self.options.source)
-	def build():
-		self.run("env NOCONFIGURE=1 ./autogen.sh") # runs in self.builddir by default
+	def build(self):
+		super().build()
+		if not os.path.exists(os.path.join(self.builddir, "configure")):
+			self.run("env NOCONFIGURE=1 ./autogen.sh")
 		self.run("./configure --prefix= " + self.options.configflags)
 		self.run("make all")
-	def stage():
+	def stage(self):
+		super().stage()
 		self.run("make install DESTDIR=" + self.stagedir)
-	def deploy():
-		return ["bin", "share", **self.findFiles("lib/**/*.so")] # not "include"
-	def test():
+	def deploy(self):
+		super().deploy()
+		self.doDeploy(["bin", "share", "lib"]) # not "include"
+	def test(self):
+		super().test()
 		self.run("make check")
+
+
+if __name__ == "__main__":
+	class Options:
+		source = "git://git.sv.gnu.org/readline.git"
+		configflags = ""
+	handler = AutotoolsHandler("readline", Options())
+	getattr(handler, sys.argv[1])()
---
+++ snapcraft/__init__.py
@@ -0,0 +1,56 @@
+# -*- Mode:Python; indent-tabs-mode:t; tab-width:4 -*-
+
+import os
+import subprocess
+import sys
+
+class BaseHandler:
+
+	def __init__(self, name, options):
+		self.name = name
+		self.options = options
+		self.sourcedir = os.path.join(os.getcwd(), "parts", self.name, "src")
+		self.builddir = os.path.join(os.getcwd(), "parts", self.name, "build")
+		self.stagedir = os.path.join(os.getcwd(), "parts", "stage")
+		self.snapdir = os.path.join(os.getcwd(), "snap")
+
+    # The API
+	def init(self):
+		try: os.makedirs(self.sourcedir)
+		except: pass
+		try: os.makedirs(self.builddir)
+		except: pass
+		try: os.makedirs(self.stagedir)
+		except: pass
+	def pull(self):
+		pass
+	def build(self):
+		self.run("cp -Tr " + self.sourcedir + " " + self.builddir)
+	def stage(self):
+		pass
+	def deploy(self):
+		pass
+	def test(self):
+		pass
+
+    # Helpers
+	def run(self, cmd, cwd=None):
+		if cwd is None:
+			cwd = self.builddir
+		subprocess.call(cmd, shell=True, cwd=cwd)
+
+	def pullBranch(self, url):
+		if url.startswith("bzr:") or url.startswith("lp:"):
+			self.run("bzr branch " + url + " " + self.sourcedir)
+		elif url.startswith("git:"):
+			self.run("git clone " + url + " " + self.sourcedir)
+		else:
+			raise Exception("Did not recognize branch url: " + url)
+
+	def doDeploy(self, dirs):
+		try: os.makedirs(self.snapdir)
+		except: pass
+
+		for d in dirs:
+			self.run("cp -r " + d + " " + self.snapdir, self.stagedir)
+
