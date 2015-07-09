c04814f093a655e91524a38a0f8831fee3b4f717
Michael Terry <michael.terry@canonical.com>
2015-07-09 09:32:43 -0400

    Add simple cmake support

---
+++ snapcraft/plugins/cmake_project.py
@@ -0,0 +1,32 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import snapcraft
+
+
+class CMakePlugin(snapcraft.BasePlugin):
+    def __init__(self, name, options):
+        super().__init__(name, options)
+        if self.options.configflags is None:
+            self.options.configflags = ''
+
+    def pull(self):
+        return self.pullBranch(self.options.source)
+
+    def build(self):
+        return self.run('cmake . -DCMAKE_INSTALL_PREFIX= %s' % self.options.configflags) and \
+            self.run("make") and \
+            self.run("make install DESTDIR=" + self.installdir)
