51957410aa3fb813ca9219cf11f26161680d7e0e
Ted Gould <ted@gould.cx>
2015-07-14 17:09:51 -0500

    Better directories

--- snapcraft/plugins/simpleqml.sh
+++ snapcraft/plugins/simpleqml.sh
@@ -2,21 +2,19 @@
 set -e
 set -u
 
-# D=/home/ubuntu/thing
-D=/apps/qmlapp.sideload/0/usr/
-A=x86_64-linux-gnu
-export XDG_CONFIG_DIRS=$D/xdg:/etc/xdg
+LIBRARY_DIR=$SNAP_APP_PATH/usr/
+ARCHITECTURE=x86_64-linux-gnu
+
+export XDG_CONFIG_DIRS=$LIBRARY_DIR/xdg:/etc/xdg
 export QTCHOOSER_NO_GLOBAL_DIR=1
 export QT_QPA_PLATFORM=ubuntumirclient
-export QT_PLUGIN_PATH=$D/lib/$A/qt5/plugins
-export QML2_IMPORT_PATH=$$D/lib/$A/qt5/qml
+export QT_PLUGIN_PATH=$LIBRARY_DIR/lib/$ARCHITECTURE/qt5/plugins
+export QML2_IMPORT_PATH=$$LIBRARY_DIR/lib/$ARCHITECTURE/qt5/qml
 
 export MIR_SOCKET=/run/mir_socket
-export MIR_CLIENT_PLATFORM_PATH=$D/lib/$A/mir/client-platform
+export MIR_CLIENT_PLATFORM_PATH=$LIBRARY_DIR/lib/$ARCHITECTURE/mir/client-platform
 
-export LD_LIBRARY_PATH=$D/lib/$A/mesa-egl:$D/lib/$A/mesa:$D/lib/$A
+export LD_LIBRARY_PATH=$LIBRARY_DIR/lib/$ARCHITECTURE/mesa-egl:$LIBRARY_DIR/lib/$ARCHITECTURE/mesa:$LIBRARY_DIR/lib/$ARCHITECTURE:$LD_LIBRARY_PATH
 
-# $D/bin/$A/qmlscene
-
-./strace -o /tmp/trace -f -s 120 $D/lib/$A/qt5/bin/qmlscene
+$LIBRARY_DIR/bin/$ARCHITECTURE/qmlscene
 
