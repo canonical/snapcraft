f538bba9d5b1505c75c4d1d1fd64444a0a8ee39d
Michael Vogt <michael.vogt@ubuntu.com>
2015-07-15 11:21:42 +0200

    examples/py3-project/sha3sum.py: remove sys.path.insert() - Python3Plugin.env() will ensure its there

--- examples/py3-project/sha3sum.py
+++ examples/py3-project/sha3sum.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-import os.path
 import sys
 
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib", "python3.4", "site-package"))
 import spongeshaker.sha3
 
 
