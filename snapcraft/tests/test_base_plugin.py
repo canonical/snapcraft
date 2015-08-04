2942b2013a9df1ba1f87ed0af0936a75faab786e
Leo Arias <leo.arias@canonical.com>
2015-08-03 19:29:39 -0600

    Added a test for pull tarball.

--- snapcraft/tests/test_base_plugin.py
+++ snapcraft/tests/test_base_plugin.py
@@ -14,11 +14,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import http.server
 import fixtures
 import logging
+import os
+import threading
 
 import snapcraft
 from snapcraft.tests import TestCase
+
+
+class FakeTarballHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        data = 'Test fake tarball file'
+        self.send_response(200)
+        self.send_header('Content-Length', len(data))
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(data.encode())
+
+    def log_message(self, *args):
+        # Overwritten so the test does not write to stderr.
+        pass
 
 
 class TestBasePlugin(TestCase):
@@ -46,3 +64,23 @@ class TestBasePlugin(TestCase):
             "You can't specify both source-tag and source-branch for a git "
             "source (part 'test_plugin').\n")
         self.assertEqual(expected, fake_logger.output)
+
+    def test_pull_tarball_must_download_to_sourcedir(self):
+        server = http.server.HTTPServer(('', 0), FakeTarballHTTPRequestHandler)
+        server_thread = threading.Thread(target=server.serve_forever)
+        self.addCleanup(server_thread.join)
+        self.addCleanup(server.server_close)
+        self.addCleanup(server.shutdown)
+        server_thread.start()
+
+        plugin_name = 'test_plugin'
+        dest_dir = os.path.join('parts', plugin_name, 'src')
+        os.makedirs(dest_dir)
+        tar_file_name = 'test.tar'
+        source = 'http://{}:{}/{file_name}'.format(
+            *server.server_address, file_name=tar_file_name)
+        plugin = snapcraft.BasePlugin(plugin_name, 'dummy_options')
+        plugin.pull_tarball(source)
+
+        with open(os.path.join(dest_dir, tar_file_name), 'r') as tar_file:
+            self.assertEqual('Test fake tarball file', tar_file.read())
