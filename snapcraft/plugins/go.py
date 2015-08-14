9e1d3acc531617d573017b72ccbb63c4c0d6ade6
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-08-14 17:04:30 -0300

    go as build-deps instead of plugged

--- snapcraft/plugins/go.py
+++ snapcraft/plugins/go.py
@@ -14,10 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 import snapcraft
-from snapcraft.plugins import ubuntu
 
 
 class GoPlugin(snapcraft.BasePlugin):
@@ -25,34 +22,10 @@ class GoPlugin(snapcraft.BasePlugin):
     def __init__(self, name, options):
         super().__init__(name, options)
 
-        class UbuntuOptions:
-            packages = ['golang-go']
-        self.ubuntu = ubuntu.UbuntuPlugin(name, UbuntuOptions())
-
-        # Prefix our files so they don't conflict with anything else,
-        # especially since we aren't even installing them into the snap.
-        # TODO: This can go away if/when we have proper dependency flattening.
-        self.ubuntu.installdir = os.path.join(self.installdir, 'go')
-
-    def pull(self):
-        return self.ubuntu.pull()
-
-    def build(self):
-        if not self.ubuntu.build():
-            return False
-        # on older Ubuntus (like trusty), the installed executable is golang-go
-        if os.path.exists(os.path.join(self.ubuntu.installdir, 'usr', 'bin', 'golang-go')):
-            try:
-                os.symlink('golang-go', os.path.join(self.ubuntu.installdir, 'usr', 'bin', 'go'))
-            except FileExistsError:
-                pass
-        return True
-
     def env(self, root):
         # usr/lib/go/bin on newer Ubuntus, usr/bin on trusty
-        return self.ubuntu.env(root) + [
-            "PATH={0}/go/usr/lib/go/bin:{0}/go/usr/bin:$PATH".format(root),
-            "GOROOT={}/go/usr/lib/go".format(root),
+        return [
+            "GOPATH={}/go".format(root),
         ]
 
     def snap_files(self):
