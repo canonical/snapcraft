d341b2276e1759471ef7ff80221e375f7d808548
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-08-26 12:14:50 +0200

    Add find_latest_private_key
    This patch adds a helper function that can locate the most recently
    modified private ssh key. It is needed by the subsequent patch which
    addresses issues that can prevent logging into a test virtual machine in
    certain ssh configurations.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- snapcraft/cmds.py
+++ snapcraft/cmds.py
@@ -158,6 +158,42 @@ def assemble(args):
     common.run(['snappy', 'build', common.get_snapdir()])
 
 
+def _find_latest_private_key():
+    """
+    Find the latest private key in ~/.ssh.
+
+    :returns:
+        Path of the most-recently-modified private SSH key
+    :raises LookupError:
+        If no such key was found.
+
+    This function tries to mimic the logic found in ``ubuntu-device-flash``. It
+    will look for the most recently modified private key in the users' SSH
+    configuration directory.
+    """
+    candidates = []
+    ssh_dir = os.path.expanduser('~/.ssh/')
+    for filename in os.listdir(ssh_dir):
+        # Skip public keys, we want the private key
+        if filename.endswith('.pub'):
+            continue
+        ssh_key = os.path.join(ssh_dir, filename)
+        # Skip non-files
+        if not os.path.isfile(ssh_key):
+            continue
+        # Ensure that it is a real ssh key
+        with open(ssh_key, 'rb') as stream:
+            if stream.readline() != b'-----BEGIN RSA PRIVATE KEY-----\n':
+                continue
+        candidates.append(ssh_key)
+    # Sort the keys by modification time, pick the most recent key
+    candidates.sort(key=lambda f: os.stat(f).st_mtime, reverse=True)
+    logger.debug("Available ssh public keys: %r", candidates)
+    if not candidates:
+        raise LookupError("Unable to find any private ssh key")
+    return candidates[0]
+
+
 def run(args):
     qemudir = os.path.join(os.getcwd(), "image")
     qemu_img = os.path.join(qemudir, "15.04.img")
