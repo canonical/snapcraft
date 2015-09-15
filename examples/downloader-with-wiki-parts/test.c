d1c54f98a36dfb147ace0bf5b5f8031d8816d9ca
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-09-15 18:52:10 -0300

    review fixes in example

--- examples/downloader-with-wiki-parts/test.c
+++ examples/downloader-with-wiki-parts/test.c
@@ -4,12 +4,11 @@
 int main(void)
 {
   CURL *curl;
-  CURLcode res;
 
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "ubuntu.com");
-    res = curl_easy_perform(curl);
+    curl_easy_perform(curl);
 
     curl_easy_cleanup(curl);
   }
