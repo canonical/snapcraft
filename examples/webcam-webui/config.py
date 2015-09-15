53647d3eeaf202d506a06a564b12d1d6feabe7ba
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-09-15 02:17:55 -0300

    adding setup.py and config.py

---
+++ examples/webcam-webui/config.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+import sys
+import yaml
+
+_CONFIG = 'configuration'
+
+_DEFAULT_INTERVAL = 10
+
+
+def main():
+    config_file = os.path.join(os.environ['SNAP_APP_DATA_PATH'], _CONFIG)
+
+    config_yaml = yaml.load(sys.stdin)
+    if config_yaml:
+        set_config(config_file, config_yaml)
+
+    yaml.dump(get_config(config_file), stream=sys.stdout, default_flow_style=False)
+
+
+def set_config(config_file, config_yaml={}):
+    with open(config_file, 'w') as f:
+        yaml.dump(_config(config_yaml), stream=f, default_flow_style=False)
+
+    return config_yaml
+
+
+def get_config(config_file):
+    try:
+        with open(config_file) as f:
+            return yaml.load(f)
+    except FileNotFoundError:
+        return _config()
+
+
+def _config(config_yaml={}):
+    try:
+        config_yaml['config'][os.environ['SNAP_NAME']]['interval']
+    except KeyError:
+        interval = {
+            'config': {
+                os.environ['SNAP_NAME']: {
+                    'interval': _DEFAULT_INTERVAL
+                }
+            }
+        }
+        config_yaml.update(interval)
+
+    return config_yaml
+
+
+if __name__ == '__main__':
+    main()
