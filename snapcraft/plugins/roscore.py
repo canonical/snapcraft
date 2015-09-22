1dc909e3e14b73f392ea5f06a7411fa16bffe4c7
Ted Gould <ted@canonical.com>
2015-09-22 00:11:18 +0000

    Flesh out the basic ros plugin

--- snapcraft/plugins/roscore.py
+++ snapcraft/plugins/roscore.py
@@ -15,15 +15,34 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import snapcraft
+import os
 
 
 class RosCorePlugin(snapcraft.BasePlugin):
 
     _PLUGIN_STAGE_PACKAGES = [
-        'ros-jade-ros-base',
     ]
 
-    _PLUGIN_STAGE_SOURCES = ('deb http://packages.ros.org/ros/ubuntu/ trusty main\n'
-                             'deb http://archive.ubuntu.com/ubuntu/ trusty main universe\n'
-                             'deb http://archive.ubuntu.com/ubuntu/ trusty-updates main universe\n'
-                             'deb http://archive.ubuntu.com/ubuntu/ trusty-security main universe\n')
+    _PLUGIN_STAGE_SOURCES = ('deb http://packages.ros.org/ros/ubuntu/ vivid main\n'
+                             'deb http://archive.ubuntu.com/ubuntu/ vivid main universe\n'
+                             'deb http://archive.ubuntu.com/ubuntu/ vivid-updates main universe\n'
+                             'deb http://archive.ubuntu.com/ubuntu/ vivid-security main universe\n')
+
+    def __init__ (self, name, options):
+        self.rosversion = options.rosversion or 'jade'
+        self._PLUGIN_STAGE_PACKAGES.append('ros-' + self.rosversion + '-ros-core')
+        super().__init__(name, options)
+
+    def build (self):
+        os.makedirs(os.path.join(self.installdir, 'bin'), exist_ok=True)
+        with open(os.path.join(self.installdir, 'bin', self.name + '-rosmaster-service'), 'w') as f:
+            f.write('#!/bin/sh\n')
+            f.write('_CATKIN_SETUP_DIR=' + os.path.join('$SNAP_APP_PATH', 'opt', 'ros', self.rosversion) + '\n')
+            f.write('source ' + os.path.join('$SNAP_APP_PATH', 'opt', 'ros', self.rosversion, 'setup.sh') + '\n')
+            f.write('exec ' + os.path.join('$SNAP_APP_PATH', 'opt', 'ros', self.rosversion, 'bin', 'rosmaster') + '\n')
+        return True
+
+    def snap_fileset (self):
+        return [
+            os.path.join('bin', self.name + '-rosmaster-service')
+        ]
