279312c3ac74e1ad00cb4fb3b3d7c3b923753e4e
Leo Arias <leo.arias@canonical.com>
2015-10-05 12:15:14 -0600

    Fixed over-indent.

--- snapcraft/common.py
+++ snapcraft/common.py
@@ -78,7 +78,7 @@ def get_arch_triplet():
     if _arch_triplet is None:
         _arch_triplet = subprocess.check_output(
             ['dpkg-architecture', '-qDEB_BUILD_MULTIARCH']).decode(
-             'utf8').strip()
+            'utf8').strip()
     return _arch_triplet
 
 
