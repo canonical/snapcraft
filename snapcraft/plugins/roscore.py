650e84854dd5c9510996c2c9b445c956f02422c0
Ted Gould <ted@canonical.com>
2015-10-14 21:47:19 +0000

    Grab changes in the catkin branch

--- snapcraft/plugins/roscore.py
+++ snapcraft/plugins/roscore.py
@@ -22,9 +22,10 @@ class RosCorePlugin(snapcraft.BasePlugin):
 
     _PLUGIN_STAGE_SOURCES = (
         'deb http://packages.ros.org/ros/ubuntu/ vivid main\n'
-        'deb http://archive.ubuntu.com/ubuntu/ vivid main universe\n'
-        'deb http://archive.ubuntu.com/ubuntu/ vivid-updates main universe\n'
-        'deb http://archive.ubuntu.com/ubuntu/ vivid-security main universe\n')
+        'deb http://${prefix}.ubuntu.com/${suffix}/ trusty main universe\n'
+        'deb http://${prefix}.ubuntu.com/${suffix}/ trusty-updates main universe\n'
+        'deb http://${prefix}.ubuntu.com/${suffix}/ trusty-security main universe\n'
+        'deb http://${security}.ubuntu.com/${suffix} trusty-security main universe\n')
 
     @classmethod
     def schema(cls):
@@ -40,33 +41,37 @@ class RosCorePlugin(snapcraft.BasePlugin):
 
     def __init__(self, name, options):
         super().__init__(name, options)
-        self.rosversion = options.rosversion or 'jade'
+        self.rosversion = options.rosversion or 'indigo'
         self.stage_packages.append(
             'ros-' + self.rosversion + '-ros-core'
         )
 
     def build(self):
-        dirname = os.path.join(self.installdir, 'bin')
-        os.makedirs(dirname, exist_ok=True)
+        os.makedirs(os.path.join(self.installdir, 'bin'), exist_ok=True)
+        ros_dir = os.path.join('$SNAP_APP_PATH', 'opt', 'ros', self.rosversion)
+        service_wrapper = os.path.join(
+            self.installdir, 'bin', '{}-rosmaster-service'.format(self.name))
 
-        filename = os.path.join(dirname, self.name + '-rosmaster-service')
-        with open(filename, 'w') as f:
-            rosdir = os.path.join(
-                '$SNAP_APP_PATH',
-                'opt',
-                'ros',
-                self.rosversion
-            )
+        with open(os.path.join(service_wrapper), 'w') as f:
+            f.write('#!/bin/bash\n')
+            f.write('_CATKIN_SETUP_DIR={}\n'.format(ros_dir))
+            f.write('source {}/setup.bash\n'.format(ros_dir))
+            f.write('export PYTHONPATH={}:$PYTHONPATH\n'.format(
+                os.path.join(ros_dir, 'lib', 'python2.7', 'dist-packages')))
+            f.write('exec {}/bin/rosmaster\n'.format(ros_dir))
 
-            f.write('\n'.join([
-                '#!/bin/sh',
-                '_CATKIN_SETUP_DIR={}'.format(rosdir),
-                'source {}'.format(rosdir + '/setup.sh'),
-                'exec {}'.format(rosdir + '/bin/rosmaster')
-            ]))
+        os.chmod(service_wrapper, 0o755)
         return True
 
     def snap_fileset(self):
-        return [
-            os.path.join('bin', self.name + '-rosmaster-service')
-        ]
+        return ([
+            os.path.join('bin', self.name + '-rosmaster-service'),
+             'opt/ros/' + self.rosversion + '/bin/*',
+             'opt/ros/' + self.rosversion + '/lib/*',
+            '-opt/ros/' + self.rosversion + '/share/*/cmake/*',
+            '-opt/ros/' + self.rosversion + '/include',
+            '-opt/ros/' + self.rosversion + '/.catkin',
+            '-opt/ros/' + self.rosversion + '/.rosinstall',
+            '-opt/ros/' + self.rosversion + '/setup.sh',
+            '-opt/ros/' + self.rosversion + '/_setup_util.py'
+        ])
