9a469ac382dbc1fda9a628de1007a3dd4ab0aba3
Ted Gould <ted@canonical.com>
2015-10-23 17:42:40 +0000

    Switching to using the proper plugin and getting the cli from Github

--- snapcraft/plugins/awscli.py
+++ snapcraft/plugins/awscli.py
@@ -14,51 +14,49 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from snapcraft.plugins.python2 import Python2Plugin
-
 import os
 import os.path
+import snapcraft.plugins.python3
 
-class AWSCLIPlugin(Python3Plugin):
+class AWSCLIPlugin(snapcraft.plugins.python3.Python3Plugin):
 
     @classmethod
     def schema(cls):
-        return {
-            '$schema': 'http://json-schema.org/draft-04/schema#',
-            'type': 'object',
-            'properties': {
-                'accesskeyid': {
-                    'type': 'string',
-                    'default': ''
-                },
-                'secretaccesskey': {
-                    'type': 'string',
-                    'default': ''
-                },
-                'region': {
-                    'type': 'string',
-                    'default': 'us-east-1'
-                },
-            },
-            'required': ['accesskeyid', 'secretaccesskey']
+        schema = super().schema()
+        schema['properties']['accesskeyid'] = {
+            'type': 'string',
+            'default': ''
+        }
+        schema['properties']['secretaccesskey'] =  {
+            'type': 'string',
+            'default': ''
+        }
+        schema['properties']['region'] =  {
+            'type': 'string',
+            'default': 'us-east-1'
         }
 
+        schema['required'] = ['accesskeyid', 'secretaccesskey']
+
+        return schema
+
     def __init__(self, name, options):
-        options.pip_packages = ['awscli']
+        options.source = "https://github.com/aws/aws-cli.git"
+        options.source_type = 'git'
+        #options.pip_packages = ['py2exe', 'awscli']
         super().__init__(name, options)
-        self.accesskeyid = options.accesskeyid
-        self.secretaccesskey = options.secretaccesskey
-        self.region = options.region
 
     def build(self):
         if not super().build():
             return False
 
-        if not self.run(['aws','configure','set','region',self.region]):
+        aws = ['python3', os.path.join(self.installdir, 'usr', 'bin', 'aws')]
+
+        if not self.run(aws + ['configure','set','region',self.options.region]):
             return False
-        if not self.run(['aws','configure','set','aws_access_key_id',self.accesskeyid]):
+        if not self.run(aws + ['configure','set','aws_access_key_id',self.options.accesskeyid]):
             return False
-        if not self.run(['aws','configure','set','aws_secret_access_key',self.secretaccesskey]):
+        if not self.run(aws + ['configure','set','aws_secret_access_key',self.options.secretaccesskey]):
             return False
 
         #TODO remove hack when two python parts can run at the same time.
@@ -75,5 +73,7 @@ class AWSCLIPlugin(Python3Plugin):
         return True
 
     def env(self, root):
-        return ['AWS_ACCESS_KEY_ID=%s' % self.accesskeyid,
-            'AWS_SECRET_ACCESS_KEY=%s' % self.secretaccesskey]
+        env = super().env(root)
+        env.extend(['AWS_ACCESS_KEY_ID=%s' % self.options.accesskeyid,
+            'AWS_SECRET_ACCESS_KEY=%s' % self.options.secretaccesskey])
+        return env
