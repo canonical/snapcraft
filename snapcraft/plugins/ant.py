f08d16441f10b36d35cc6a57ae17fc0c8f5de9d3
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-10-27 16:24:18 -0300

    ant newline

--- snapcraft/plugins/ant.py
+++ snapcraft/plugins/ant.py
@@ -17,7 +17,6 @@
 """The ant plugin is useful for ant based parts.
 
 The ant build system is commonly used to build Java projects.
-
 The plugin requires a build.xml in the root of the source tree.
 
 This plugin uses the common plugin keywords as well as those for "sources".
