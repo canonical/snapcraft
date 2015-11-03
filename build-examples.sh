c771f7f560574309d9c9a391fa1704a10e637626
Ted Gould <ted@gould.cx>
2015-11-03 15:03:58 -0600

    Cleaning afterward to ensure we don't run out of disk space. No reason I could know that could happen. For a friend.

--- build-examples.sh
+++ build-examples.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 EXAMPLES=`find examples/ -maxdepth 1 -type d | tail -n +2`
 
 for example in $EXAMPLES; do
-	echo $example
-	(cd $example ; ../../bin/snapcraft $* )
+	echo "Building snap for: $example"
+	(cd $example && \
+		../../bin/snapcraft $* && \
+		../../bin/snapcraft clean )
 done
