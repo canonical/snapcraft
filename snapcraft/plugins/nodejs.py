0336e05fb4f29a04fd5f7bb36e24615577ba527f
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-11-18 17:41:47 -0300

    nodejs plugin: Initial implementation.
    This includes a small example.
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

---
+++ snapcraft/plugins/nodejs.py
@@ -0,0 +1,79 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The nodejs plugin is useful for node/npm based parts.
+
+The plugin uses node to install dependencies from `package.json`. It
+also sets up binaries defined in `package.json` into the `PATH`.
+
+This plugin uses the common plugin keywords as well as those for "sources".
+For more information check the 'plugins' topic for the former and the
+'sources' topic for the latter.
+"""
+
+import logging
+import os
+import platform
+
+import snapcraft
+from snapcraft import sources
+
+logger = logging.getLogger(__name__)
+
+_NODEJS_BASE = 'node-v{version}-linux-{arch}'
+_NODEJS_VERSION = '4.2.2'
+_NODEJS_TMPL = 'https://nodejs.org/dist/v{version}/{base}.tar.gz'
+_NODEJS_ARCHES = {
+    'x86_64': 'x64',
+    'armv7l': 'armv7l',
+}
+
+
+class NodePlugin(snapcraft.BasePlugin):
+
+    def __init__(self, name, options):
+        super().__init__(name, options)
+        self._nodejs_tar = sources.Tar(_get_nodejs_release(),
+                                       os.path.join(self.partdir, 'npm'))
+
+    def pull(self):
+        super().pull()
+        os.makedirs(os.path.join(self.partdir, 'npm'))
+        self._nodejs_tar.pull()
+
+    def build(self):
+        super().build()
+        self._nodejs_tar.provision(self.installdir)
+        self.run(['npm', 'install', '-g'])
+
+    def env(self, root):
+        env = super().env(root)
+        env.append('PATH=$PATH:{}/'.format(os.path.join(root, 'bin')))
+        return env
+
+
+def _get_nodejs_base():
+    machine = platform.machine()
+    if machine not in _NODEJS_ARCHES:
+        raise EnvironmentError('architecture not supported ({})'.format(
+            machine))
+    return _NODEJS_BASE.format(version=_NODEJS_VERSION,
+                               arch=_NODEJS_ARCHES[machine])
+
+
+def _get_nodejs_release():
+    return _NODEJS_TMPL.format(version=_NODEJS_VERSION,
+                               base=_get_nodejs_base())
