c1945a71aa4bf763c7cff5c343ee2075fe5254df
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-11-19 16:10:15 -0300

    Docstring polish: inserting a - between words
    So it reads *plugin-specific* instead of *plugin specific*
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- snapcraft/plugins/python3.py
+++ snapcraft/plugins/python3.py
@@ -27,7 +27,7 @@ This plugin uses the common plugin keywords as well as those for "sources".
 For more information check the 'plugins' topic for the former and the
 'sources' topic for the latter.
 
-Additionally, this plugin uses the following plugin specific keywords:
+Additionally, this plugin uses the following plugin-specific keywords:
 
     - requirements:
       (string)
