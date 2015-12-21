88e81384ddc6674cbc1a088797742e967dcd4eae
Robert <robert1999.g@gmail.com>
2015-12-21 13:08:49 +0300

    Change in test_wiki.py
    There is a line(#50) that is not tested by the test-code. In the added test we try to be sure that 'type' part of the part was deleted successfully. To do so, we check that the result of get_part function is not equal to the result where source-type part is present. In the case of part-not-in-wiki, we should check also that source-type isn't there so result must be 'None'

--- snapcraft/tests/test_wiki.py
+++ snapcraft/tests/test_wiki.py
@@ -53,8 +53,9 @@ class TestYaml(TestCase):
         self.assertEqual(self.w.get_part('part-not-in-wiki'), None)
     
     def test_get_part_type(self):
-        self.assertNotEqual(self.w.get_part('part-in-wiki'), {'source-type': 'type'})
-
+        self.assertNotEqual(self.w.get_part('part-in-wiki'), {'plugin': 'go', 'source': 'http://somesource', 'source-type': 'type'})
+        self.assertEqual(self.w.get_part('part-not-in-wiki'), None)
+    
     def test_compose_part_with_properties_from_the_wiki(self):
         properties = self.w.compose(
             'part-in-wiki', {'source': '.', 'another': 'different'})
