d64b0f5e365bbf96aefb3a900ceb81f77618bae6
Leonardo Arias Fonseca (¡paz y baile!) <yo@elopio.net>
2016-02-25 21:32:10 -0600

    examples_tests/libpipeline: skip the output assertion when skipping the install
    LP: #1550108

--- examples_tests/test_libpipeline.py
+++ examples_tests/test_libpipeline.py
@@ -24,11 +24,10 @@ class LibPipelineTestCase(examples_tests.ExampleTestCase):
     def test_libpipeline(self):
         self.build_snap(self.example_dir)
         self.install_snap(self.example_dir, 'pipelinetest', '1.0')
-        output = self.run_command_in_snappy_testbed(
-            '/snaps/bin/pipelinetest.pipelinetest')
         expected = (
             'running ls | grep c\n'
             'custom libpipeline called\n'
             'command-pipelinetest.wrapper\n'
             'include\n')
-        self.assertEqual(output, expected)
+        self.assert_command_in_snappy_testbed(
+            '/snaps/bin/pipelinetest.pipelinetest', expected)
