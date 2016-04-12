338a1ce168b1080dc8b5c3777e84a282cd1519b1
Sergio Schvezov <sergiusens@gmail.com>
2016-04-12 16:21:06 +0200

    Remove support and mentions of the config hook. (#437)
    LP: #1568826
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- integration_tests/test_python_plugin.py
+++ integration_tests/test_python_plugin.py
@@ -28,30 +28,6 @@ import integration_tests
 
 class PythonPluginTestCase(integration_tests.TestCase):
 
-    def test_snap_with_pypi_config(self):
-        project_dir = 'pypi-config'
-        self.run_snapcraft('snap', project_dir)
-
-        expected_files = (
-            os.path.join('stage', 'usr', 'bin', 'config.py'),
-            os.path.join('stage', 'usr', 'bin', 'python3'),
-            os.path.join('snap', 'usr', 'bin', 'config.py'),
-            os.path.join('snap', 'usr', 'bin', 'python3'),
-            os.path.join('snap', 'meta', 'hooks', 'config'),
-        )
-        for expected_file in expected_files:
-            self.assertThat(
-                os.path.join(project_dir, expected_file),
-                FileExists())
-
-        expected_config = (
-            'exec "$SNAP/usr/bin/python3" '
-            '"$SNAP/usr/bin/config.py" $*\n')
-        with open(os.path.join(
-                project_dir, 'snap', 'meta', 'hooks', 'config')) as config:
-            config_contents = config.read()
-        self.assertThat(config_contents, EndsWith(expected_config))
-
     def test_pull_with_pip_requirements_file(self):
         project_dir = 'pip-requirements-file'
         self.run_snapcraft('pull', project_dir)
