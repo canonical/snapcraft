82d560cea5bdeb3c07d8a79c52fbe17e9efe63c8
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-04-13 10:51:42 -0300

    Encapsulate meta creation
    This helps reduing the use of common.get_stagedir()
    
    LP: #1551849
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- snapcraft/meta.py
+++ snapcraft/meta.py
@@ -60,7 +60,7 @@ class CommandError(Exception):
     pass
 
 
-def create(config_data):
+def create_snap_packaging(config_data):
     """Create snap.yaml and related assets in meta.
     Create  the meta directory and provision it with snap.yaml
     in the snap dir using information from config_data.
@@ -68,169 +68,178 @@ def create(config_data):
     :param dict config_data: project values defined in snapcraft.yaml.
     :return: meta_dir.
     """
-    meta_dir = os.path.join(common.get_snapdir(), 'meta')
-    os.makedirs(meta_dir, exist_ok=True)
+    packaging = _SnapPackaging(config_data)
+    packaging.write_snap_yaml()
+    packaging.setup_assets()
 
-    _write_snap_yaml(meta_dir, config_data)
-    _setup_assets(meta_dir, config_data)
-
-    return meta_dir
+    return packaging.meta_dir
 
 
-def _write_snap_yaml(meta_dir, config_data):
-    package_snap_path = os.path.join(meta_dir, 'snap.yaml')
-    snap_yaml = _compose_snap_yaml(meta_dir, config_data)
+class _SnapPackaging:
 
-    with open(package_snap_path, 'w') as f:
-        yaml.dump(snap_yaml, stream=f, default_flow_style=False)
+    @property
+    def meta_dir(self):
+        return self._meta_dir
 
-    return snap_yaml
+    def __init__(self, config_data):
+        self._snap_dir = common.get_snapdir()
+        self._meta_dir = os.path.join(self._meta_dir, 'meta')
+        self._config_data = config_data
 
+        os.makedirs(self._meta_dir, exist_ok=True)
 
-def _setup_assets(meta_dir, config_data):
-    if 'license' in config_data:
-        logger.warning("DEPRECATED: 'license' defined in snapcraft.yaml")
-        license_path = os.path.join(meta_dir, 'license.txt')
-        os.link(config_data['license'], license_path)
+    def write_snap_yaml(self):
+        package_snap_path = os.path.join(self.meta_dir, 'snap.yaml')
+        snap_yaml = self._compose_snap_yaml()
 
-    if 'icon' in config_data:
-        # TODO: use developer.ubuntu.com once it has updated documentation.
-        logger.warning(
-            "DEPRECATED: 'icon' defined in snapcraft.yaml. Look at "
-            "https://github.com/ubuntu-core/snapcraft/blob/master/docs/"
-            "metadata.md#snap-icon for more information")
-        icon_ext = config_data['icon'].split(os.path.extsep)[1]
-        icon_dir = os.path.join(meta_dir, 'gui')
-        icon_path = os.path.join(icon_dir, 'icon.{}'.format(icon_ext))
-        if not os.path.exists(icon_dir):
-            os.mkdir(icon_dir)
-        if os.path.exists(icon_path):
-            os.unlink(icon_path)
-        os.link(config_data['icon'], icon_path)
+        with open(package_snap_path, 'w') as f:
+            yaml.dump(snap_yaml, stream=f, default_flow_style=False)
 
-    _setup_from_setup(meta_dir)
+        return snap_yaml
 
+    def _setup_assets(self):
+        if 'license' in self._config_data:
+            logger.warning("DEPRECATED: 'license' defined in snapcraft.yaml")
+            license_path = os.path.join(self.meta_dir, 'license.txt')
+            os.link(self._config_data['license'], license_path)
 
-def _setup_from_setup(meta_dir):
-    setup_dir = 'setup'
-    if not os.path.exists(setup_dir):
-        return
+        if 'icon' in self._config_data:
+            # TODO: use developer.ubuntu.com once it has updated documentation.
+            logger.warning(
+                "DEPRECATED: 'icon' defined in snapcraft.yaml. Look at "
+                "https://github.com/ubuntu-core/snapcraft/blob/master/docs/"
+                "metadata.md#snap-icon for more information")
+            icon_ext = self._config_data['icon'].split(os.path.extsep)[1]
+            icon_dir = os.path.join(self.meta_dir, 'gui')
+            icon_path = os.path.join(icon_dir, 'icon.{}'.format(icon_ext))
+            if not os.path.exists(icon_dir):
+                os.mkdir(icon_dir)
+            if os.path.exists(icon_path):
+                os.unlink(icon_path)
+            os.link(self._config_data['icon'], icon_path)
 
-    gui_src = os.path.join(setup_dir, 'gui')
-    gui_dst = os.path.join(meta_dir, 'gui')
-    if os.path.exists(gui_dst) and os.path.exists(gui_src):
-        shutil.rmtree(gui_dst)
-    if os.path.exists(gui_src):
-        shutil.copytree(gui_src, gui_dst)
+        self._setup_from_setup(self._meta_dir)
 
-    license_src = os.path.join(setup_dir, 'license.txt')
-    license_dst = os.path.join(meta_dir, 'license.txt')
-    if os.path.exists(license_dst) and os.path.exists(license_src):
-        os.unlink(license_dst)
-    if os.path.exists(license_src):
-        os.link(license_src, license_dst)
+    def _setup_from_setup(self):
+        setup_dir = 'setup'
+        if not os.path.exists(setup_dir):
+            return
 
+        gui_src = os.path.join(setup_dir, 'gui')
+        gui_dst = os.path.join(self.meta_dir, 'gui')
+        if os.path.exists(gui_dst) and os.path.exists(gui_src):
+            shutil.rmtree(gui_dst)
+        if os.path.exists(gui_src):
+            shutil.copytree(gui_src, gui_dst)
 
-def _compose_snap_yaml(meta_dir, config_data):
-    """Creates a new dictionary from config_data to obtain snap.yaml.
+        license_src = os.path.join(setup_dir, 'license.txt')
+        license_dst = os.path.join(self.meta_dir, 'license.txt')
+        if os.path.exists(license_dst) and os.path.exists(license_src):
+            os.unlink(license_dst)
+        if os.path.exists(license_src):
+            os.link(license_src, license_dst)
 
-    Missing key exceptions will be raised if config_data does not contain all
-    the _MANDATORY_PACKAGE_KEYS, config_data can be validated against the
-    snapcraft schema.
+    def _compose_snap_yaml(self):
+        """Create a new dictionary from config_data to obtain snap.yaml.
 
-    Keys that are in _OPTIONAL_PACKAGE_KEYS are ignored if not there.
-    """
-    snap_yaml = {}
+        Missing key exceptions will be raised if config_data does not contain
+        all the _MANDATORY_PACKAGE_KEYS, config_data can be validated against
+        the snapcraft schema.
 
-    for key_name in _MANDATORY_PACKAGE_KEYS:
-        snap_yaml[key_name] = config_data[key_name]
+        Keys that are in _OPTIONAL_PACKAGE_KEYS are ignored if not there.
+        """
+        snap_yaml = {}
 
-    for key_name in _OPTIONAL_PACKAGE_KEYS:
-        if key_name in config_data:
-            snap_yaml[key_name] = config_data[key_name]
+        for key_name in _MANDATORY_PACKAGE_KEYS:
+            snap_yaml[key_name] = self._config_data[key_name]
 
-    if 'apps' in config_data:
-        snap_yaml['apps'] = _wrap_apps(config_data['apps'])
+        for key_name in _OPTIONAL_PACKAGE_KEYS:
+            if key_name in self._config_data:
+                snap_yaml[key_name] = self._config_data[key_name]
 
-    if config_data.get('type', '') == 'kernel':
-        snap_yaml.update(_get_kernel_keys())
+        if 'apps' in self._config_data:
+            snap_yaml['apps'] = self._wrap_apps(self._config_data['apps'])
 
-    return snap_yaml
+        if self._config_data.get('type', '') == 'kernel':
+            snap_yaml.update(self._get_kernel_keys())
 
+        return snap_yaml
 
-def _get_kernel_keys():
-    kernel_keys = _KERNEL_KEYS.copy()
-    version = os.listdir(os.path.join(common.get_snapdir(), 'lib', 'modules'))
-    kernel_keys['modules'] = kernel_keys['modules'].format(version[0])
+    def _get_kernel_keys(self):
+        kernel_keys = _KERNEL_KEYS.copy()
+        version = os.listdir(os.path.join(self._snap_dir, 'lib', 'modules'))
+        kernel_keys['modules'] = kernel_keys['modules'].format(version[0])
 
-    return kernel_keys
+        return kernel_keys
 
+    def _write_wrap_exe(self, wrapexec, wrappath,
+                        shebang=None, args=None, cwd=None):
+        args = ' '.join(args) + ' $*' if args else '$*'
+        cwd = 'cd {}'.format(cwd) if cwd else ''
 
-def _copy(meta_dir, relpath, new_relpath=None):
-    new_base = new_relpath or os.path.basename(relpath)
-    target_path = os.path.join(meta_dir, new_base)
+        assembled_env = common.assemble_env().replace(self._snap_dir, '$SNAP')
+        replace_path = r'{}/[a-z0-9][a-z0-9+-]*/install'.format(
+            common.get_partsdir())
+        assembled_env = re.sub(replace_path, '$SNAP', assembled_env)
+        executable = '"{}"'.format(wrapexec)
+        if shebang is not None:
+            new_shebang = re.sub(replace_path, '$SNAP', shebang)
+            if new_shebang != shebang:
+                # If the shebang was pointing to and executable within the
+                # local 'parts' dir, have the wrapper script execute it
+                # directly, since we can't use $SNAP in the shebang itself.
+                executable = '"{}" "{}"'.format(new_shebang, wrapexec)
+        script = ('#!/bin/sh\n' +
+                  '{}\n'.format(assembled_env) +
+                  '{}\n'.format(cwd) +
+                  'exec {} {}\n'.format(executable, args))
 
-    shutil.copyfile(relpath, target_path)
+        with open(wrappath, 'w+') as f:
+            f.write(script)
 
-    return os.path.join('meta', os.path.basename(relpath))
+        os.chmod(wrappath, 0o755)
 
+    def _wrap_exe(self, command, basename=None):
+        execparts = shlex.split(command)
+        exepath = os.path.join(self._snap_dir, execparts[0])
+        if basename:
+            wrappath = os.path.join(self._snap_dir, basename) + '.wrapper'
+        else:
+            wrappath = exepath + '.wrapper'
+        shebang = None
 
-def _write_wrap_exe(wrapexec, wrappath, shebang=None, args=None, cwd=None):
-    args = ' '.join(args) + ' $*' if args else '$*'
-    cwd = 'cd {}'.format(cwd) if cwd else ''
+        if os.path.exists(wrappath):
+            os.remove(wrappath)
 
-    snap_dir = common.get_snapdir()
-    assembled_env = common.assemble_env().replace(snap_dir, '$SNAP')
-    replace_path = r'{}/[a-z0-9][a-z0-9+-]*/install'.format(
-        common.get_partsdir())
-    assembled_env = re.sub(replace_path, '$SNAP', assembled_env)
-    executable = '"{}"'.format(wrapexec)
-    if shebang is not None:
-        new_shebang = re.sub(replace_path, '$SNAP', shebang)
-        if new_shebang != shebang:
-            # If the shebang was pointing to and executable within the
-            # local 'parts' dir, have the wrapper script execute it
-            # directly, since we can't use $SNAP in the shebang itself.
-            executable = '"{}" "{}"'.format(new_shebang, wrapexec)
-    script = ('#!/bin/sh\n' +
-              '{}\n'.format(assembled_env) +
-              '{}\n'.format(cwd) +
-              'exec {} {}\n'.format(executable, args))
+        wrapexec = '$SNAP/{}'.format(execparts[0])
+        if not os.path.exists(exepath) and '/' not in execparts[0]:
+            _find_bin(execparts[0], self._snap_dir)
+            wrapexec = execparts[0]
+        else:
+            with open(exepath, 'rb') as exefile:
+                # If the file has a she-bang, the path might be pointing to
+                # the local 'parts' dir. Extract it so that _write_wrap_exe
+                # will have a chance to rewrite it.
+                if exefile.read(2) == b'#!':
+                    shebang = exefile.readline().strip().decode('utf-8')
 
-    with open(wrappath, 'w+') as f:
-        f.write(script)
+        self._write_wrap_exe(wrapexec, wrappath,
+                             shebang=shebang, args=execparts[1:])
 
-    os.chmod(wrappath, 0o755)
+        return os.path.relpath(wrappath, self._snap_dir)
 
-
-def _wrap_exe(command, basename=None):
-    execparts = shlex.split(command)
-    snap_dir = common.get_snapdir()
-    exepath = os.path.join(snap_dir, execparts[0])
-    if basename:
-        wrappath = os.path.join(snap_dir, basename) + '.wrapper'
-    else:
-        wrappath = exepath + '.wrapper'
-    shebang = None
-
-    if os.path.exists(wrappath):
-        os.remove(wrappath)
-
-    wrapexec = '$SNAP/{}'.format(execparts[0])
-    if not os.path.exists(exepath) and '/' not in execparts[0]:
-        _find_bin(execparts[0], snap_dir)
-        wrapexec = execparts[0]
-    else:
-        with open(exepath, 'rb') as exefile:
-            # If the file has a she-bang, the path might be pointing to
-            # the local 'parts' dir. Extract it so that _write_wrap_exe
-            # will have a chance to rewrite it.
-            if exefile.read(2) == b'#!':
-                shebang = exefile.readline().strip().decode('utf-8')
-
-    _write_wrap_exe(wrapexec, wrappath, shebang=shebang, args=execparts[1:])
-
-    return os.path.relpath(wrappath, snap_dir)
+    def _wrap_apps(self, apps):
+        for app in apps:
+            cmds = (k for k in ('command', 'stop-command') if k in apps[app])
+            for k in cmds:
+                try:
+                    apps[app][k] = self._wrap_exe(
+                        apps[app][k], '{}-{}'.format(k, app))
+                except CommandError as e:
+                    raise EnvironmentError(
+                        'The specified command {!r} defined in {!r} does '
+                        'not exist or is not executable'.format(str(e), app))
+        return apps
 
 
 def _find_bin(binary, basedir):
@@ -247,15 +256,3 @@ def _find_bin(binary, basedir):
                        stdout=subprocess.DEVNULL)
         except subprocess.CalledProcessError:
             raise CommandError(binary)
-
-
-def _wrap_apps(apps):
-    for app in apps:
-        for k in [k for k in ('command', 'stop-command') if k in apps[app]]:
-            try:
-                apps[app][k] = _wrap_exe(apps[app][k], '{}-{}'.format(k, app))
-            except CommandError as e:
-                raise EnvironmentError(
-                    'The specified command {!r} defined in {!r} does '
-                    'not exist or is not executable'.format(str(e), app))
-    return apps
