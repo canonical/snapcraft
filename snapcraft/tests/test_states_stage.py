676c23eebb9bf20f9cbdbe504737e7b1bfe70148
Kyle Fazzari <kyrofa@users.noreply.github.com>
2016-04-13 13:05:24 -0400

    Automatically re-stage if dirty. (#443)
    Currently Snapcraft only notices YAML changes for the strip step. For
    all other steps, if the step has already run it simply says so and
    bails. This commit changes that for the stage step, where now if one
    changes the `stage` keyword in the YAML, stage will notice that it's out
    of date and automatically clean itself (along with the strip step since
    it depends upon stage) and re-run.
    
    LP: #1477904
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

---
+++ snapcraft/tests/test_states_stage.py
@@ -0,0 +1,62 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import snapcraft.internal
+from snapcraft import tests
+
+
+class StageStateTestCase(tests.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.files = {'foo'}
+        self.directories = {'bar'}
+
+        class Options:
+            def __init__(self):
+                self.stage = ['baz']
+
+        self.options = Options()
+
+        self.state = snapcraft.internal.states.StageState(
+            self.files, self.directories, self.options)
+
+    def test_representation(self):
+        expected = (
+            'StageState(directories: {}, files: {}, properties: {})').format(
+                self.directories, self.files, self.options.__dict__)
+        self.assertEqual(expected, repr(self.state))
+
+    def test_comparison(self):
+        other = snapcraft.internal.states.StageState(
+            self.files, self.directories, self.options)
+
+        self.assertTrue(self.state == other, 'Expected states to be identical')
+
+    def test_comparison_not_equal(self):
+        others = [
+            snapcraft.internal.states.StageState(set(), self.directories,
+                                                 self.options),
+            snapcraft.internal.states.StageState(self.files, set(),
+                                                 self.options),
+            snapcraft.internal.states.StageState(self.files, self.directories,
+                                                 None),
+        ]
+
+        for index, other in enumerate(others):
+            with self.subTest('other #{}'.format(index+1)):
+                self.assertFalse(self.state == other,
+                                 'Expected states to be different')
