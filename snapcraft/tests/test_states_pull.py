6af50cc5cc8244b1c5aed13b28a90d5595c5b1d2
Kyle Fazzari <kyle@canonical.com>
2016-04-14 03:36:50 +0000

    Forgot the pull state test in da1411df
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

---
+++ snapcraft/tests/test_states_pull.py
@@ -0,0 +1,56 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import snapcraft.internal
+from snapcraft import tests
+
+
+class PullStateTestCase(tests.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.schema_properties = ['foo']
+
+        class Options:
+            def __init__(self):
+                self.foo = ['bar']
+
+        self.options = Options()
+
+        self.state = snapcraft.internal.states.PullState(
+            self.schema_properties, self.options)
+
+    def test_representation(self):
+        expected = 'PullState(properties: {}, schema_properties: {})'.format(
+            self.options.__dict__, self.schema_properties)
+        self.assertEqual(expected, repr(self.state))
+
+    def test_comparison(self):
+        other = snapcraft.internal.states.PullState(
+            self.schema_properties, self.options)
+
+        self.assertTrue(self.state == other, 'Expected states to be identical')
+
+    def test_comparison_not_equal(self):
+        others = [
+            snapcraft.internal.states.PullState([], self.options),
+            snapcraft.internal.states.PullState(self.schema_properties, None)
+        ]
+
+        for index, other in enumerate(others):
+            with self.subTest('other #{}'.format(index+1)):
+                self.assertFalse(self.state == other,
+                                 'Expected states to be different')
