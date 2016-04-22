58bc36c00218136d3bf9b2f21a013a080bee9fa9
Vincent Ladeuil <vila+ols@canonical.com>
2016-04-22 10:36:02 +0000

    The infamous W503 line break before binary operator.
    It it said that D. Knuth managed to convince GvD that this check was wrong...
    
    Hopefully pep8 will be fixed soon.

--- snapcraft/tests/store_tests/test_upload.py
+++ snapcraft/tests/store_tests/test_upload.py
@@ -82,8 +82,8 @@ class UploadTestCase(store_tests.TestCase):
         # FIXME: Each test user need his own registered snap names for the test
         # to succeed -- vila 2016-04-12
         registered = (
-            os.environ.get('TEST_USER_EMAIL')
-            == 'u1test+snapcraft@canonical.com')
+            os.environ.get('TEST_USER_EMAIL') ==
+            'u1test+snapcraft@canonical.com')
         if not registered:
             self.expectFailure('basic is registered by someone else',
                                self.assertTrue, resp['success'])
