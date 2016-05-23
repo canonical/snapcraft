6b58c169e3d6e2e251971b2a5d86a3dbc22a018f
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-05-23 18:29:11 -0300

    Use the in ubuntu-core python3
    LP: #1582513
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- snapcraft/tests/test_plugin_python3.py
+++ snapcraft/tests/test_plugin_python3.py
@@ -14,47 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 
-from unittest import mock
-
-import snapcraft
 from snapcraft import tests
-from snapcraft.plugins import python3
 
 
 class Python3PluginTestCase(tests.TestCase):
-
-    def setUp(self):
-        super().setUp()
-
-        class Options:
-            requirements = ''
-            python_packages = []
-
-        self.options = Options()
-        self.project_options = snapcraft.ProjectOptions()
-
-    @mock.patch.object(python3.Python3Plugin, 'run')
-    @mock.patch.object(python3.Python3Plugin, 'run_output',
-                       return_value='python3.4')
-    def test_pip_relative_site_packages_symlink(self, run_output_mock,
-                                                run_mock):
-        plugin = python3.Python3Plugin('test-part', self.options,
-                                       self.project_options)
-        os.makedirs(plugin.sourcedir)
-        os.makedirs(os.path.join(plugin.installdir, 'usr', 'lib', 'python3.4'))
-        os.makedirs(os.path.join(plugin.installdir, 'usr', 'lib', 'python3',
-                                 'dist-packages'))
-
-        open(os.path.join(plugin.sourcedir, 'setup.py'), 'w').close()
-
-        plugin._pip()
-
-        link = os.readlink(os.path.join(plugin.installdir, 'usr', 'lib',
-                                        'python3.4', 'site-packages'))
-        expected = os.path.join('..', 'python3', 'dist-packages')
-        self.assertEqual(link, expected,
-                         'Expected site-packages to be a relative link to '
-                         '"{}", but it was a link to "{}"'.format(expected,
-                                                                  link))
+    pass
