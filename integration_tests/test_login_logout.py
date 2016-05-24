fec5df0c90254992149bfb674908e32590138d6b
Leo Arias <elopio@localhost.localdomain>
2016-05-24 03:05:46 +0000

    Run the integration tests against a local fake server when the user password is not in the environment.
    LP: #1585023

--- integration_tests/test_login_logout.py
+++ integration_tests/test_login_logout.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 import integration_tests
 from snapcraft.tests import fixture_setup
 
@@ -22,7 +24,10 @@ class LoginLogoutTestCase(integration_tests.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.useFixture(fixture_setup.StagingStore())
+        if os.getenv('TEST_USER_PASSWORD', None) is None:
+            self.useFixture(fixture_setup.FakeStore())
+        else:
+            self.useFixture(fixture_setup.StagingStore())
 
     def test_successful_login(self):
         self.addCleanup(self.logout)
