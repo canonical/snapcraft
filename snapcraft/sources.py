09243667db14e3e563bb87b61740bc70fab61557
Sergio Schvezov <sergiusens@gmail.com>
2016-05-26 18:11:16 -0300

    Support zip files as source (#523)
    LP: #1577062
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- snapcraft/sources.py
+++ snapcraft/sources.py
@@ -19,4 +19,5 @@ from snapcraft.internal.sources import Git         # noqa
 from snapcraft.internal.sources import Mercurial   # noqa
 from snapcraft.internal.sources import Tar         # noqa
 from snapcraft.internal.sources import Local       # noqa
+from snapcraft.internal.sources import Zip         # noqa
 from snapcraft.internal.sources import get         # noqa
