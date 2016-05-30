5c2024f99d7ffea90bdd589aed697b7f806f285d
Didier Roche <didrocks@ubuntu.com>
2016-05-30 10:33:50 +0200

    Move to "snapcraft examples"
    Remove tweaked snapcraft-examples command to an independent snapcraft examples
    argument.

--- snapcraft/main.py
+++ snapcraft/main.py
@@ -32,6 +32,7 @@ Usage:
   snapcraft [options] logout
   snapcraft [options] upload <snap-file>
   snapcraft [options] list-plugins
+  snapcraft [options] examples [<directory>]
   snapcraft [options] help (topics | <plugin> | <topic>) [--devel]
   snapcraft (-h | --help)
   snapcraft --version
@@ -64,6 +65,8 @@ Options specific to snapping:
                                         snap.
 
 The available commands are:
+  examples     Setup the snapcraft examples in the specified directory,
+               or ./snapcraft-tour/.
   help         Obtain help for a certain plugin or topic
   init         Initialize a snapcraft project.
   list-plugins List the available plugins that handle different types of part.
@@ -96,8 +99,10 @@ http://developer.ubuntu.com/snappy/snapcraft
 
 from contextlib import suppress
 import logging
+import os
 import pkg_resources
 import pkgutil
+import shutil
 import sys
 import subprocess
 import textwrap
@@ -106,11 +111,12 @@ from docopt import docopt
 
 import snapcraft
 from snapcraft.internal import lifecycle, log
-from snapcraft.internal.common import (format_output_in_columns,
-                                       MAX_CHARACTERS_WRAP)
+from snapcraft.internal.common import (
+    format_output_in_columns, MAX_CHARACTERS_WRAP, get_examplesdir)
 
 
 logger = logging.getLogger(__name__)
+_SNAPCRAFT_TOUR_DIR = "snapcraft-tour"
 
 
 def _get_version():
@@ -118,6 +124,27 @@ def _get_version():
         return pkg_resources.require('snapcraft')[0].version
     except pkg_resources.DistributionNotFound:
         return 'devel'
+
+
+def _scaffold_examples(directory):
+    logger.debug("Copying examples to {}".format(directory))
+    dest_dir = os.path.abspath(directory)
+
+    # If dest_dir doesn't exist, we dump all examples in it.
+    # If it does exist, we dump them into a subdirectory
+    try:
+        shutil.copytree(get_examplesdir(), dest_dir)
+    except FileExistsError:
+        # don't event try to copy if the dest exists already
+        if not os.path.isdir(dest_dir):
+            raise NotADirectoryError("{} is a file, can't be used as a "
+                                     "destination".format(dest_dir))
+        dest_dir = os.path.join(dest_dir, _SNAPCRAFT_TOUR_DIR)
+        shutil.copytree(get_examplesdir(), dest_dir)
+
+    print("Snapcraft tour initialized in {}.\n"
+          "Instructions are in the README, or "
+          "https://snapcraft.io/create/#begin".format(directory))
 
 
 def _list_plugins():
@@ -205,6 +232,8 @@ def run(args, project_options):
         snapcraft.upload(args['<snap-file>'])
     elif args['cleanbuild']:
         lifecycle.cleanbuild(project_options),
+    elif args['examples']:
+        _scaffold_examples(args['<directory>'] or _SNAPCRAFT_TOUR_DIR)
     elif args['help']:
         snapcraft.topic_help(args['<topic>'] or args['<plugin>'],
                              args['--devel'], args['topics'])
