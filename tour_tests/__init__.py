f1ed04e35bb8460ecc583a3c8a1801dc4e644d7e
Didier Roche <didrocks@ubuntu.com>
2016-05-30 14:35:25 +0200

    Support running tests in subdirectories
    Structure of the test runner should follow the tour structure. Then, we run
    and build from the correct subdirectory the tests.

--- tour_tests/__init__.py
+++ tour_tests/__init__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import inspect
 import os
 import sys
 
@@ -23,6 +24,12 @@ sys.path = [topdir] + sys.path
 import demos_tests
 
 
-class ExampleTestCase(demos_tests.ExampleTestCase):
+class TourTestCase(demos_tests.ExampleTestCase):
 
-    src_dir = "examples"
+    def __init__(self, *args, **kwargs):
+        relative_path = os.path.relpath(
+            os.path.dirname(inspect.getfile(self.__class__)),
+            os.path.dirname(__file__))
+        self.src_dir = os.path.join("tour", relative_path)
+        super().__init__(*args, **kwargs)
+
