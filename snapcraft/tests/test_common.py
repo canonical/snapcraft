81f0dfe47bce0d4dad3bc6ffbc990886d54374e6
Didier Roche <didrocks@ubuntu.com>
2016-06-06 10:43:04 +0200

    Ensure that default paths are reset
    We won't depend on unit test running order which may fail the build.

--- snapcraft/tests/test_common.py
+++ snapcraft/tests/test_common.py
@@ -23,6 +23,14 @@ from snapcraft import tests
 
 class CommonTestCase(tests.TestCase):
 
+    def setUp(self):
+        super().setUp()
+        # reset all default paths
+        common._plugindir = common._DEFAULT_PLUGINDIR
+        common._schemadir = common._DEFAULT_SCHEMADIR
+        common._librariesdir = common._DEFAULT_LIBRARIESDIR
+        common._tourdir = common._DEFAULT_TOURDIR
+
     def test_get_default_plugindir(self):
         self.assertEqual(
             '/usr/share/snapcraft/plugins', common.get_plugindir())
