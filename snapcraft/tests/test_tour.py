46bbfced38782de9573188240a65b26723123178
Didier Roche <didrocks@ubuntu.com>
2016-06-09 17:49:53 +0200

    Remove useless imports now

--- snapcraft/tests/test_tour.py
+++ snapcraft/tests/test_tour.py
@@ -14,11 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from contextlib import suppress
 import os
-import shutil
 import sys
-from tempfile import mktemp, TemporaryDirectory, NamedTemporaryFile
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 from unittest import mock
 
 import snapcraft.main
