54ff4541a3fcc7c80c7f86ff4795666799f258f8
Didier Roche <didrocks@ubuntu.com>
2016-06-09 08:49:03 +0200

    Add snap tests for new tour snaps

---
+++ snaps_tests/tour_tests/10-SNAPS/test_02_service_confined.py
@@ -0,0 +1,28 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import snaps_tests
+
+
+class ServiceConfinedTestCase(snaps_tests.SnapsTestCase):
+
+    snap_content_dir = '02-service-confined'
+
+    def test_hello(self):
+        self.build_snap(self.snap_content_dir)
+        self.install_snap(self.snap_content_dir, 'hello-world-service', '0.1')
+        self.assert_service_running('hello-world-service', 'hello-service')
