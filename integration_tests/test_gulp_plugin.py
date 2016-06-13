e5560992333cfd3b609047ee7318ef950645af6d
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-06-13 17:26:41 -0300

    integration test

---
+++ integration_tests/test_gulp_plugin.py
@@ -0,0 +1,36 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015, 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from testtools.matchers import (
+    DirExists,
+    Not
+)
+
+import integration_tests
+
+
+class GulpPluginTestCase(integration_tests.TestCase):
+
+    def test_stage_make_plugin(self):
+        project_dir = 'simple-gulp'
+        self.run_snapcraft('stage', project_dir)
+
+        binary_output = self.get_output_ignoring_non_zero_exit(
+            os.path.join('stage', 'hello-world'),
+            cwd=project_dir)
+        self.assertEqual('I was installed with gulp\n', binary_output)
