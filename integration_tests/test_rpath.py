00e37e57d00c93649602d41e9a601716641a183b
Kyle Fazzari <kyrofa@users.noreply.github.com>
2016-06-24 16:01:40 -0400

    Don't copy libraries that are already in prime. (#580)
    Currently snapcraft is fooled by relative rpaths and copies linked
    libraries, even if they've already been primed. This commit stops that
    by checking each library path to ensure it hasn't already been primed
    before doing so.
    
    LP: #1570895
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

---
+++ integration_tests/test_rpath.py
@@ -0,0 +1,43 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+import integration_tests
+
+from testtools.matchers import (
+    DirExists,
+    FileExists,
+    Not,
+)
+
+
+class RpathTestCase(integration_tests.TestCase):
+
+    def test_origin_rpath(self):
+        project_dir = 'rpath-test'
+        self.run_snapcraft('prime', project_dir)
+
+        primedir = os.path.join(project_dir, 'prime')
+        self.assertThat(os.path.join(primedir, 'binary'), FileExists())
+        self.assertThat(
+            os.path.join(primedir, 'lib', 'libfoo.so'), FileExists())
+
+        # Assert that the $ORIGIN rpath did not result in the library being pulled
+        # in twice.
+        self.assertThat(
+            os.path.join(primedir, os.path.abspath(primedir).lstrip('/')),
+            Not(DirExists()))
