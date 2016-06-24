0f3e5e0ffbb70be8b34b06a3c9b4fdc6fff6c0ac
Sergio Schvezov <sergiusens@gmail.com>
2016-06-24 17:00:17 -0300

    Integrate with new remote parts (#590)
    These replace the wiki.
    
    LP: #1594976
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- snapcraft/internal/yaml.py
+++ snapcraft/internal/yaml.py
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import codecs
-import contextlib
 import logging
 import os
 import os.path
@@ -29,9 +28,9 @@ import snapcraft
 from snapcraft.internal import (
     common,
     libraries,
+    parts,
     pluginhandler,
     sources,
-    wiki,
 )
 from snapcraft._schema import Validator, SnapcraftSchemaError
 
@@ -134,7 +133,7 @@ class Config:
         self.build_tools = self.data.get('build-packages', [])
         self.build_tools.extend(project_options.additional_build_packages)
 
-        self._wiki = wiki.Wiki()
+        self._remote_parts = parts.get_remote_parts()
         self._process_parts()
 
     def _process_parts(self):
@@ -145,17 +144,18 @@ class Config:
             plugin_name = properties.pop('plugin', None)
             if not plugin_name:
                 logger.info(
-                    'Searching the wiki to compose part "{}"'.format(
-                        part_name))
-                with contextlib.suppress(KeyError):
-                    properties = self._wiki.compose(part_name, properties)
+                    'Searching in the remote parts cache for part '
+                    '{!r}'.format(part_name))
+                try:
+                    properties = self._remote_parts.compose(
+                        part_name, properties)
                     plugin_name = properties.pop('plugin', None)
-                    # The wiki still supports using 'type' for snapcraft 1.x
-                    if 'type' in properties:
-                        del properties['type']
-
-            if not plugin_name:
-                raise PluginNotDefinedError(part_name)
+                except KeyError as e:
+                    raise SnapcraftLogicError(
+                        '{!r} is missing the `plugin` entry and is not '
+                        'defined in the current remote parts cache, try to '
+                        'run `snapcraft update` to '
+                        'refresh'.format(part_name)) from e
 
             if 'after' in properties:
                 self.after_requests[part_name] = properties.pop('after')
@@ -187,16 +187,18 @@ class Config:
                         found = True
                         break
                 if not found:
-                    wiki_part = self._wiki.get_part(dep)
-                    found = True if wiki_part else False
-                    if found:
-                        plugin_name = wiki_part.pop('plugin')
-                        part.deps.append(self.load_plugin(
-                            dep, plugin_name, wiki_part))
-                        self._part_names.append(dep)
-                if not found:
-                    raise SnapcraftLogicError(
-                        'part name missing {}'.format(dep))
+                    try:
+                        remote_part = self._remote_parts.get_part(dep)
+                    except KeyError as e:
+                        raise SnapcraftLogicError(
+                            'Cannot find definition for part {!r}. '
+                            'It may be a remote part, run `snapcraft update` '
+                            'to refresh the remote parts '
+                            'cache'.format(dep)) from e
+                    plugin_name = remote_part.pop('plugin')
+                    part.deps.append(self.load_plugin(
+                        dep, plugin_name, remote_part))
+                    self._part_names.append(dep)
 
     def _sort_parts(self):
         '''Performs an inneficient but easy to follow sorting of parts.'''
