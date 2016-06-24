9497f7bb64f0ce5fce51221a3c51082267479c57
Marius Gripsgard <mariogrip@ubuntu.com>
2016-06-24 21:04:41 +0200

    [rust-plugin] correct expected output in test_list_plugins

--- integration_tests/test_list_plugins.py
+++ integration_tests/test_list_plugins.py
@@ -21,9 +21,8 @@ class ListPluginsTestCase(integration_tests.TestCase):
 
     def test_list_plugins(self):
         output = self.run_snapcraft('list-plugins')
-        expected = (
-            'ant        catkin  copy  gulp  kbuild  make   nil     python2  '
-            'scons      \n'
-            'autotools  cmake   go    jdk   kernel  maven  nodejs  python3  '
-            'tar-content\n')
+        expected = ('ant        cmake  gulp    kernel  nil      python3  tar-content\n'
+                    'autotools  copy   jdk     make    nodejs   rust   \n'
+                    'catkin     go     kbuild  maven   python2  scons  \n')
+
         self.assertEqual(expected, output)
