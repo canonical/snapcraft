770e178568276884b92601504d31ad2d188d3bf3
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-07-01 14:36:28 -0300

    use a context manager for the fileinput

--- snapcraft/internal/repo.py
+++ snapcraft/internal/repo.py
@@ -292,16 +292,18 @@ def fix_pkg_config(root, pkg_config_file, prefix_trim=None):
         pattern_trim = re.compile(
             '^prefix={}(?P<prefix>.*)'.format(prefix_trim))
     pattern = re.compile('^prefix=(?P<prefix>.*)')
-    for line in fileinput.input(pkg_config_file, inplace=True):
-        match = pattern.search(line)
-        if prefix_trim:
-            match_trim = pattern_trim.search(line)
-        if prefix_trim and match_trim:
-            print('prefix={}{}'.format(root, match_trim.group('prefix')))
-        elif match:
-            print('prefix={}{}'.format(root, match.group('prefix')))
-        else:
-            print(line, end='')
+
+    with fileinput.input(pkg_config_file, inplace=True) as input_file:
+        for line in input_file: 
+            match = pattern.search(line)
+            if prefix_trim:
+                match_trim = pattern_trim.search(line)
+            if prefix_trim and match_trim:
+                print('prefix={}{}'.format(root, match_trim.group('prefix')))
+            elif match:
+                print('prefix={}{}'.format(root, match.group('prefix')))
+            else:
+                print(line, end='')
 
 
 def _fix_artifacts(debdir):
