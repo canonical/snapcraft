b252a17b2b661e89a1b57e2543cb33fa93903bda
Leo Arias <yo@elopio.net>
2016-07-04 13:08:17 -0600

    Add a travis suite to run the staging store tests (#602)
    In order to do this, the store tests need to handle an empty password.
    As travis only sets the encrypted environment variable for the team branches,
    the personal PRs will run against the fake store.
    
    LP: #1596072

--- integration_tests/test_store_register.py
+++ integration_tests/test_store_register.py
@@ -26,7 +26,7 @@ class RegisterTestCase(integration_tests.TestCase):
 
     def setUp(self):
         super().setUp()
-        if os.getenv('TEST_USER_PASSWORD', None) is None:
+        if not os.getenv('TEST_USER_PASSWORD', None):
             self.useFixture(fixture_setup.FakeStore())
         else:
             self.useFixture(fixture_setup.StagingStore())
@@ -34,7 +34,7 @@ class RegisterTestCase(integration_tests.TestCase):
     def test_successful_registration(self):
         self.login(expect_success=True)
         snap_name = 'u1test{}'.format(uuid.uuid4().int)
-        self.run_snapcraft(['register', snap_name])
+        self.register(snap_name)
 
     def test_failed_registration(self):
         self.login(expect_success=True)
