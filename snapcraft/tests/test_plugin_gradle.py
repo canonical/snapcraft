87521fc28a4494ce42bf617dcf865190ed9870ae
Matthew Aguirre <matt.aguirre@gmail.com>
2016-07-09 11:41:43 -0400

    Added gradle plugin.

---
+++ snapcraft/tests/test_plugin_gradle.py
@@ -0,0 +1,71 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from unittest import mock
+
+import snapcraft
+from snapcraft import tests
+from snapcraft.plugins import gradle
+
+
+class GradlePluginTestCase(tests.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        class Options:
+            gradle_options = []
+        self.options = Options()
+
+        self.project_options = snapcraft.ProjectOptions()
+
+        patcher = mock.patch('snapcraft.repo.Ubuntu')
+        self.ubuntu_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    @mock.patch.object(gradle.GradlePlugin, 'run')
+    def test_build(self, run_mock):
+        plugin = gradle.GradlePlugin('test-part', self.options,
+                                     self.project_options)
+
+        def side(l):
+            os.makedirs(os.path.join(plugin.builddir,
+                        'build', 'libs'))
+            open(os.path.join(plugin.builddir,
+                 'build', 'libs', 'dummy.jar'), 'w').close()
+
+        run_mock.side_effect = side
+        os.makedirs(plugin.sourcedir)
+
+        plugin.build()
+
+        run_mock.assert_has_calls([
+            mock.call(['./gradlew', 'jar']),
+        ])
+
+    @mock.patch.object(gradle.GradlePlugin, 'run')
+    def test_build_fail(self, run_mock):
+        plugin = gradle.GradlePlugin('test-part', self.options,
+                                     self.project_options)
+
+        os.makedirs(plugin.sourcedir)
+        with self.assertRaises(RuntimeError):
+            plugin.build()
+
+        run_mock.assert_has_calls([
+            mock.call(['./gradlew', 'jar']),
+        ])
