dcbb8c1b3d9356dc8bb1f736179d00506b9c746f
Matthew Aguirre <matt.aguirre@gmail.com>
2016-07-13 15:54:18 -0400

    Updated to work w/ options passed in via snapcraft.yaml file.

--- snapcraft/plugins/gradle.py
+++ snapcraft/plugins/gradle.py
@@ -69,9 +69,9 @@ class GradlePlugin(snapcraft.plugins.jdk.JdkPlugin):
     def build(self):
         super().build()
 
-        gradle_cmd = ['./gradlew']
+        gradle_cmd = ['./gradlew', 'jar']
 
-        self.run(gradle_cmd + self.options.gradle_options + '[jar]')
+        self.run(gradle_cmd + self.options.gradle_options)
 
         src = os.path.join(self.builddir, 'build', 'libs')
         jarfiles = glob.glob(os.path.join(src, '*.jar'))
