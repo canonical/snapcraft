88b36e8ade682bc40dcf25d8e422e986777e3cd6
Leo Arias <yo@elopio.net>
2016-07-13 02:48:11 +0000

    Fixed pep8.

--- snaps_tests/demos_tests/test_plainbox_test_tool.py
+++ snaps_tests/demos_tests/test_plainbox_test_tool.py
@@ -23,7 +23,7 @@ class PlainboxTestCase(snaps_tests.SnapsTestCase):
 
     def test_plainbox(self):
         # build and install the example
-        snap_path= self.build_snap(self.snap_content_dir)
+        snap_path = self.build_snap(self.snap_content_dir)
         self.install_snap(snap_path, 'plainbox-test-tool', '0.1')
         # check that can run plainbox and list the tests available
         expected = """2013.com.canonical.plainbox::collect-manifest
