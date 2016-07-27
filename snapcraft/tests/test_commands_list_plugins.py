9eb5bcf3541e3d89441eff4148dda3790dbd713b
Marius Gripsgard <mariogrip@ubuntu.com>
2016-07-27 10:25:57 +0200

    Add new plugin: rust (#579)
    LP: #1606812

--- snapcraft/tests/test_commands_list_plugins.py
+++ snapcraft/tests/test_commands_list_plugins.py
@@ -24,9 +24,9 @@ class ListPluginsCommandTestCase(tests.TestCase):
     # plugin list when wrapper at MAX_CHARACTERS_WRAP
     default_plugin_output = (
         'ant        catkin  copy  gradle  jdk     kernel  maven  '
-        'nodejs             python2  qmake  tar-content\n'
+        'nodejs             python2  qmake  scons      \n'
         'autotools  cmake   go    gulp    kbuild  make    nil    '
-        'plainbox-provider  python3  scons\n')
+        'plainbox-provider  python3  rust   tar-content\n')
 
     def test_list_plugins_non_tty(self):
         fake_terminal = fixture_setup.FakeTerminal(isatty=False)
@@ -47,11 +47,11 @@ class ListPluginsCommandTestCase(tests.TestCase):
         self.useFixture(fake_terminal)
 
         expected_output = (
-            'ant        go      kernel  plainbox-provider  tar-content\n'
-            'autotools  gradle  make    python2          \n'
+            'ant        go      kernel  plainbox-provider  scons      \n'
+            'autotools  gradle  make    python2            tar-content\n'
             'catkin     gulp    maven   python3          \n'
             'cmake      jdk     nil     qmake            \n'
-            'copy       kbuild  nodejs  scons            \n')
+            'copy       kbuild  nodejs  rust             \n')
 
         main(['list-plugins'])
         self.assertEqual(fake_terminal.getvalue(), expected_output)
