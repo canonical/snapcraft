8a7b4fcc494e635d241f892ee2f9004aeb31ada4
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-08-05 19:33:30 +0000

    New plugin: dump
    A simple plugin that just dumps content from the specified `source`
    
    LP: #1597600
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

---
+++ snapcraft/tests/test_plugin_dump.py
@@ -0,0 +1,56 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+import snapcraft
+from snapcraft.plugins.dump import DumpPlugin
+from snapcraft.tests import TestCase
+
+
+class DumpPluginTestCase(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.project_options = snapcraft.ProjectOptions()
+
+    def test_dumping_nothing(self):
+        class Options:
+            source = '.'
+
+        plugin = DumpPlugin('dump', Options(), self.project_options)
+        plugin.pull()
+        plugin.build()
+
+        self.assertEqual(os.listdir(plugin.installdir), [])
+
+    def test_dumping_with_contents(self):
+        class Options:
+            source = '.'
+
+        open('file1', 'w').close()
+        open('file2', 'w').close()
+        os.mkdir('dir1')
+        open(os.path.join('dir1', 'subfile1'), 'w').close()
+
+        plugin = DumpPlugin('dump', Options(), self.project_options)
+        plugin.pull()
+        plugin.build()
+
+        self.assertEqual(os.listdir(plugin.installdir),
+                         ['file1', 'file2', 'dir1'])
+        self.assertEqual(os.listdir(os.path.join(plugin.installdir, 'dir1')),
+                         ['subfile1'])
