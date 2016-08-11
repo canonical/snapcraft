116b67a4004b127aa7a73ddb208b1d83af39b36d
Joe Talbott <joetalbott@gmail.com>
2016-08-11 15:31:51 -0400

    Refactor directory creation into a common method.

--- snapcraft/internal/pluginhandler.py
+++ snapcraft/internal/pluginhandler.py
@@ -650,6 +650,15 @@ def _migratable_filesets(fileset, srcdir):
     return snap_files, snap_dirs
 
 
+def _create_dirs(srcdir, dstdir, follow_symlinks=False):
+    dir_stat = os.stat(srcdir, follow_symlinks=follow_symlinks)
+    uid = dir_stat.st_uid
+    gid = dir_stat.st_gid
+    os.makedirs(dstdir, exist_ok=True)
+    os.chown(dstdir, uid, gid, follow_symlinks=follow_symlinks)
+    shutil.copystat(srcdir, dstdir, follow_symlinks=follow_symlinks)
+
+
 def _migrate_files(snap_files, snap_dirs, srcdir, dstdir, missing_ok=False,
                    follow_symlinks=False, fixup_func=lambda *args: None):
 
@@ -657,26 +666,14 @@ def _migrate_files(snap_files, snap_dirs, srcdir, dstdir, missing_ok=False,
         src = os.path.join(srcdir, directory)
         dst = os.path.join(dstdir, directory)
 
-        dir_stat = os.stat(src, follow_symlinks=follow_symlinks)
-        uid = dir_stat.st_uid
-        gid = dir_stat.st_gid
-        os.makedirs(dst, exist_ok=True)
-        os.chown(dst, uid, gid, follow_symlinks=follow_symlinks)
-        shutil.copystat(src, dst, follow_symlinks=follow_symlinks)
+        _create_dirs(src, dst, follow_symlinks=follow_symlinks)
 
     for snap_file in snap_files:
         src = os.path.join(srcdir, snap_file)
         dst = os.path.join(dstdir, snap_file)
 
-        dir_stat = os.stat(os.path.dirname(src),
-                           follow_symlinks=follow_symlinks)
-        uid = dir_stat.st_uid
-        gid = dir_stat.st_gid
-        os.makedirs(os.path.dirname(dst), exist_ok=True)
-        shutil.copystat(os.path.dirname(src), os.path.dirname(dst),
-                        follow_symlinks=follow_symlinks)
-        os.chown(os.path.dirname(dst), uid, gid,
-                 follow_symlinks=follow_symlinks)
+        _create_dirs(os.path.dirname(src), os.path.dirname(dst),
+                     follow_symlinks=follow_symlinks)
 
         if missing_ok and not os.path.exists(src):
             continue
