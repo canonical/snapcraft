653c716d0a5f2b70ce9c4b2b1b1cb35bd84e1b7d
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-09-01 18:10:45 +0000

    remove rogue print

--- snapcraft/_baseplugin.py
+++ snapcraft/_baseplugin.py
@@ -240,7 +240,6 @@ class BasePlugin:
         if not cwd:
             cwd = self.builddir
         print(' '.join(cmd))
-        print('working directory', cwd)
         os.makedirs(cwd, exist_ok=True)
         return common.run(cmd, cwd=cwd, **kwargs)
 
