49af5ec48a2bfa066f5c6c9ab236ba61e360473a
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-09-22 19:36:20 +0000

    integration regression test

---
+++ integration_tests/snaps/python-and-plainbox-do-not-conflict/2016.com.example:simple/manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+
+setup(
+    name='plainbox-provider-simple',
+    namespace='2016.com.example',
+    version="1.0",
+    description=N_("A really simple Plainbox provider"),
+    gettext_domain="2016_com_example_simple",
+)
