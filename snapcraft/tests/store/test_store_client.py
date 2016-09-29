b67a60d585bbd3000e1d95b546de0cb54e033e4f
Celso Providelo <celso.providelo@canonical.com>
2016-09-29 11:10:35 -0300

    Fixing 'sign-build' integration tests. (#839)
    Even for building the local assertion, the command actually checks if the
    account logged in has upload permission on the targeted snap (resolving
    name to snap-id via account-info).
    
    The test now builds an random snap, register the name in the store and
    only then signs the snap. The fake server was extended to record registered
    names and return them in the account-info endpoint.

--- snapcraft/tests/store/test_store_client.py
+++ snapcraft/tests/store/test_store_client.py
@@ -185,13 +185,14 @@ class PushSnapBuildTestCase(tests.TestCase):
 
     def test_push_snap_build_not_implemented(self):
         # If the "enable_snap_build" feature switch is off in the store, we
-        # will get a 501 Not Implemented response.
+        # will get a descriptive error message.
         self.client.login('dummy', 'test correct password')
         with self.assertRaises(errors.StoreSnapBuildError) as raised:
             self.client.push_snap_build('snap-id', 'test-not-implemented')
         self.assertEqual(
             str(raised.exception),
-            'Could not assert build: 501 Not Implemented')
+            'Could not assert build: The snap-build assertions are '
+            'currently disabled.')
 
     def test_push_snap_build_invalid_data(self):
         self.client.login('dummy', 'test correct password')
@@ -200,6 +201,16 @@ class PushSnapBuildTestCase(tests.TestCase):
         self.assertEqual(
             str(raised.exception),
             'Could not assert build: The snap-build assertion is not valid.')
+
+    def test_push_snap_build_unexpected_data(self):
+        # The endpoint in SCA would never return plain/text, however anything
+        # might happen in the internet, so we are a little defensive.
+        self.client.login('dummy', 'test correct password')
+        with self.assertRaises(errors.StoreSnapBuildError) as raised:
+            self.client.push_snap_build('snap-id', 'test-unexpected-data')
+        self.assertEqual(
+            str(raised.exception),
+            'Could not assert build: 500 Internal Server Error')
 
     def test_push_snap_build_successfully(self):
         self.client.login('dummy', 'test correct password')
