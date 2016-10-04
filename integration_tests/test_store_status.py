2d9e10d29725e0332c6a59996b38ad20126bd36d
Maximiliano Bertacchini <maxiberta@gmail.com>
2016-10-04 12:05:00 -0300

    Improve status integration tests to work with staging store.

--- integration_tests/test_store_status.py
+++ integration_tests/test_store_status.py
@@ -14,9 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import subprocess
+import unittest
+import uuid
 
-from testtools.matchers import Contains
+from testtools.matchers import Contains, FileExists
 
 import integration_tests
 
@@ -61,7 +64,9 @@ class StatusTestCase(integration_tests.StoreTestCase):
             "Snap 'mysnap' for 'i386' was not found in '16' series.",
             str(error.output))
 
-    def test_status_with_login_good_snap(self):
+    @unittest.skipUnless(
+        os.getenv('TEST_STORE', 'fake') == 'fake', 'Skip fake store.')
+    def test_status_fake_store(self):
         self.addCleanup(self.logout)
         self.login()
 
@@ -74,4 +79,33 @@ class StatusTestCase(integration_tests.StoreTestCase):
             'i386    stable     -          -',
             '        beta       -          -',
             '        edge       1.0-i386   3'))
+        self.assertThat(output, Contains(expected))
+
+    @unittest.skipUnless(
+        os.getenv('TEST_STORE', 'fake') == 'staging', 'Skip staging store.')
+    def test_status_staging_store(self):
+        self.addCleanup(self.logout)
+        self.login()
+
+        # Build a random snap, register, push and release it.
+        project_dir = 'basic'
+        unique_id = uuid.uuid4().int
+        name = 'u1test-{}'.format(unique_id)
+        version = str(unique_id)[:32]
+        project_dir = self.update_name_and_version(project_dir, name, version)
+        os.chdir(project_dir)
+        self.run_snapcraft('snap', project_dir)
+        snap_path = '{}_{}_{}.snap'.format(name, version, 'all')
+        self.assertThat(snap_path, FileExists())
+        self.register(name)
+        self.assertEqual(0, self.push(snap_path, release='candidate,beta'))
+
+        output = self.run_snapcraft(['status', name])
+        expected = '\n'.join((
+            'Arch    Channel    Version                           Revision',
+            'all     stable     -                                 -',
+            '        candidate  {version}  1',
+            '        beta       {version}  1',
+            '        edge       ^                                 ^'
+        )).format(version=version)
         self.assertThat(output, Contains(expected))
