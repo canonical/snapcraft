374ec59d7c03247c7768c085f5a75ff778f6d42b
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-10-05 11:18:49 +0200

    Add Waf plugin (#716)
    Adding the waf build system https://github.com/waf-project/waf 
    
    LP: #1611335
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

---
+++ snapcraft/plugins/waf.py
@@ -0,0 +1,72 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The WAF plugin is useful to build waf based parts
+
+waf bases projects are projects that drive configuration and build via
+a local waf python helper - see https://github.com/waf-project/waf for more
+details.
+
+This plugin uses the common plugin keywords as well as those for "sources".
+For more information check the 'plugins' topic for the former and the
+'sources' topic for the latter.
+
+In addition, this plugin uses the following plugin-specific keywords:
+
+    - configflags:
+      (list of strings)
+      configure flags to pass to the build such as those shown by running
+      ./waf --help
+"""
+
+import snapcraft
+
+
+class WafPlugin(snapcraft.BasePlugin):
+    """plugin to build via waf build system"""
+
+    @classmethod
+    def schema(cls):
+        schema = super().schema()
+        schema['properties']['configflags'] = {
+            'type': 'array',
+            'minitems': 1,
+            'uniqueItems': True,
+            'items': {
+                'type': 'string',
+            },
+            'default': [],
+        }
+
+        # Inform Snapcraft of the properties associated with building. If these
+        # change in the YAML Snapcraft will consider the build step dirty.
+        schema['build-properties'].extend(['configflags'])
+
+        return schema
+
+    def __init__(self, name, options, project):
+        super().__init__(name, options, project)
+        self.build_packages.extend([
+            'python-dev',
+        ])
+
+    def build(self):
+        super().build()
+        self.run(['./waf', 'distclean'])
+        self.run(['./waf', 'configure'] + self.options.configflags)
+        self.run(['./waf', 'build'])
+        self.run(['./waf', 'install',
+                  '--destdir=' + self.installdir])  # target from snappy env
