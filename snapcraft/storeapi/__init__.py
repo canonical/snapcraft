fbc7bbd648fd6b11d81ed6ca430d80cac9cacfd5
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-11-14 14:22:27 +0000

    Fix tests and need for auth

--- snapcraft/storeapi/__init__.py
+++ snapcraft/storeapi/__init__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
 import hashlib
 import itertools
 import json
@@ -373,7 +374,8 @@ class SnapIndexClient(Client):
     def get(self, url, headers=None, params=None, stream=False):
         if headers is None:
             headers = {}
-        headers.update({'Authorization': _macaroon_auth(self.conf)})
+        with contextlib.suppress(errors.InvalidCredentialsError):
+            headers.update({'Authorization': _macaroon_auth(self.conf)})
         response = self.request('GET', url, stream=stream,
                                 headers=headers, params=params)
         return response
