54b1a87e4ef092ca94c77b5be37c80bf4be3b420
Leo Arias <yo@elopio.net>
2016-11-15 18:06:34 +0000

    Improve a little the comment

--- tools/retry_autopkgtest.sh
+++ tools/retry_autopkgtest.sh
@@ -3,11 +3,22 @@
 # Retry the autopkgtest run in a pull request.
 # Arguments:
 #   pr: The identifier of the pull request to test.
-#   [release[:architecture] ...]: A list of the name of the Ubuntu release and
-#     architecture to test. By default, it will launch the tests in xenial:amd64. If only the release name is passed as an argument, amd64 will be used as the architecture.
+#   [release[:architecture] ...]: A list of the names of the Ubuntu releases and
+#     architectures to test. By default, it will launch the tests in
+#     xenial:amd64. If only the release name is passed as an argument, amd64
+#     will be used as the architecture.
 #
 # Environment variables:
 #   SNAPCRAFT_AUTOPKGTEST_SECRET: The secret to authenticate the test execution.
+#
+# Examples:
+#   Run the tests for pull request #123 in xenial amd64:
+#     ./tools/retry_autopkgtest.sh 123
+#   Run the tests for pull request #123 in xenial armhf:
+#     ./tools/retry_autopkgtest.sh 123 xenial:armhf
+#   Run the tests for pull request #123 in xenial arm64, yakkety armhf and
+#   zesty amd64:
+#     ./tools/retry_autopkgtest.sh 123 xenial:arm64 yakkety:armhf zesty
 
 if [ -z "${SNAPCRAFT_AUTOPKGTEST_SECRET}" ]; then
     echo 'Set the secret to the environment variable SNAPCRAFT_AUTOPKGTEST_SECRET.'
