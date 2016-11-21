20a47971994f48aa59b9ac76e19f2274933c7852
Leo Arias <yo@elopio.net>
2016-11-21 01:40:05 +0000

    Skip for now in armhf

--- snaps_tests/__main__.py
+++ snaps_tests/__main__.py
@@ -35,6 +35,7 @@ Options:
 
 import logging
 import os
+import platform
 import sys
 
 import docopt
@@ -47,7 +48,7 @@ def main():
 
     arguments = docopt.docopt(__doc__)
 
-    if os.environ.get('container') == 'lxc':
+    if platform.machine() == 'armv7l':
         snaps_tests.config['skip-install'] = True
     else:
         snaps_tests.config['skip-install'] = arguments['--skip-install']
