f52d00d337460e17a8324fb57a5a96f50db15444
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-12-01 14:38:00 +0100

    test_parser: use scenarios for tests and remove duplications

--- integration_tests/test_parser.py
+++ integration_tests/test_parser.py
@@ -18,10 +18,31 @@ import os
 import subprocess
 
 import integration_tests
+import testscenarios
 from snapcraft.tests import fixture_setup
 
 
-class TestParser(integration_tests.TestCase):
+class ParserTestCase(integration_tests.TestCase):
+    """Test bin/snapcraft-parser"""
+
+    def call_parser(self, wiki_path, expect_valid):
+        args = [self.snapcraft_parser_command, '--index', wiki_path,
+                '--debug',
+                '--output', 'parts.yaml']
+
+        if expect_valid:
+            subprocess.check_call(args, stderr=subprocess.DEVNULL,
+                                  stdout=subprocess.DEVNULL)
+        else:
+            self.assertRaises(
+                subprocess.CalledProcessError,
+                subprocess.check_call, args, stderr=subprocess.DEVNULL,
+                stdout=subprocess.DEVNULL)
+
+        self.assertTrue(os.path.exists('parts.yaml'))
+
+
+class TestParser(ParserTestCase):
     """Test bin/snapcraft-parser"""
 
     def test_parser_basic(self):
@@ -29,50 +50,22 @@ class TestParser(integration_tests.TestCase):
         fixture = fixture_setup.FakePartsWiki()
         self.useFixture(fixture)
 
-        args = [self.snapcraft_parser_command, '--index',
-                fixture.fake_parts_wiki_fixture.url,
-                '--debug', '--output', 'parts.yaml']
-        subprocess.check_call(args, stderr=subprocess.DEVNULL,
-                              stdout=subprocess.DEVNULL)
+        self.call_parser(
+            fixture.fake_parts_wiki_fixture.url, expect_valid=True)
 
-        self.assertTrue(os.path.exists('parts.yaml'))
 
-    def test_hidden_snapcraft_yaml(self):
-        """Test hidden .snapcraft.yaml file."""
+class TestParserWikis(testscenarios.WithScenarios, ParserTestCase):
+    """Test bin/snapcraft-parser"""
 
-        args = [self.snapcraft_parser_command, '--index',
-                os.path.join(os.path.dirname(__file__), 'hidden_parts_wiki'),
-                '--debug',
-                '--output', 'parts.yaml']
-        subprocess.check_call(args, stderr=subprocess.DEVNULL,
-                              stdout=subprocess.DEVNULL)
+    scenarios = [
+        ('Hidden .snapcraft.yaml file',
+            {'wiki_file': 'hidden_parts_wiki', 'expect_valid': True}),
+        ('Both snapcraft.yaml and .snapcraft.yaml files',
+            {'wiki_file': 'both_parts_wiki', 'expect_valid': False}),
+        ('Missing .snapcraft.yaml file',
+            {'wiki_file': 'missing_parts_wiki', 'expect_valid': False}),
+    ]
 
-        self.assertTrue(os.path.exists('parts.yaml'))
-
-    def test_both_snapcraft_yaml(self):
-        """Test hidden .snapcraft.yaml file."""
-
-        args = [self.snapcraft_parser_command, '--index',
-                os.path.join(os.path.dirname(__file__), 'both_parts_wiki'),
-                '--debug',
-                '--output', 'parts.yaml']
-        self.assertRaises(
-            subprocess.CalledProcessError,
-            subprocess.check_call, args, stderr=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL)
-
-        self.assertTrue(os.path.exists('parts.yaml'))
-
-    def test_missing_snapcraft_yaml(self):
-        """Test missing .snapcraft.yaml file."""
-
-        args = [self.snapcraft_parser_command, '--index',
-                os.path.join(os.path.dirname(__file__), 'missing_parts_wiki'),
-                '--debug',
-                '--output', 'parts.yaml']
-        self.assertRaises(
-            subprocess.CalledProcessError,
-            subprocess.check_call, args, stderr=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL)
-
-        self.assertTrue(os.path.exists('parts.yaml'))
+    def test_parse_wiki(self):
+        wiki_file = os.path.join(os.path.dirname(__file__), self.wiki_file)
+        self.call_parser(wiki_file, self.expect_valid)
