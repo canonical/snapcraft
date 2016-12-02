149c2bb0735fff93e6cc76e996a1f54fd6c50a8a
Sergio Schvezov <sergiusens@gmail.com>
2016-12-02 07:38:18 -0300

    store: return specific error when already owned (#942)
    On `snapcraft register` of a snap we already own we can just say
    it is already owned by using the specific error code returned by
    the store for this.
    
    LP: #1602091
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- integration_tests/test_store_register.py
+++ integration_tests/test_store_register.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import re
 import subprocess
 import time
@@ -50,6 +51,23 @@ class RegisterTestCase(integration_tests.StoreTestCase):
             'expectations of most users. If you are the publisher most '
             'users expect for \'test-already-registered-snap-name\' then '
             'claim the name at')
+        self.assertThat(str(error.output), Contains(expected))
+        self.assertThat(str(error.output), Contains('register-name'))
+
+    def test_registration_of_already_owned_name(self):
+        self.login()
+        self.addCleanup(self.logout)
+        if os.getenv('TEST_STORE', 'fake') != 'fake':
+            unique_id = uuid.uuid4().int
+            snap_name = 'u1test-{}'.format(unique_id)
+            self.register(snap_name)
+        else:
+            snap_name = self.test_store.already_owned_snap_name
+
+        # The snap name is already registered and you are the owner.
+        error = self.assertRaises(
+            subprocess.CalledProcessError, self.register, snap_name)
+        expected = 'You already own the name {0!r}'.format(snap_name)
         self.assertThat(str(error.output), Contains(expected))
         self.assertThat(str(error.output), Contains('register-name'))
 
