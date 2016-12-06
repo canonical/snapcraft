a87decf40c702524f0ebe5663ae6fbf3902db962
Chris MacNaughton <chmacnaughton@gmail.com>
2016-12-06 16:12:13 -0500

    Let Rust plugin fetch dependencies in pull (#908)
    Pulling the dependencies in the pull stage allows
    building Rust snaps on Launchpad builders,
    which are offline during the build stage
    
    LP: #1647847

--- snapcraft/tests/test_plugin_rust.py
+++ snapcraft/tests/test_plugin_rust.py
@@ -86,14 +86,10 @@ class RustPluginTestCase(tests.TestCase):
 
         plugin.pull()
 
-        self.assertEqual(1, run_mock.call_count)
-        run_mock.assert_has_calls([
-            mock.call(["%s" % plugin._rustup,
-                       "--prefix=%s" % plugin._rustpath,
-                       "--disable-sudo", "--save"])
-        ])
+        self.assertEqual(2, run_mock.call_count)
 
         rustdir = os.path.join(plugin.partdir, 'rust')
-        run_mock.assert_called_once_with([
+        run_mock.assert_has_calls([mock.call([
             os.path.join(rustdir, 'rustup.sh'), '--prefix={}'.format(rustdir),
-            '--disable-sudo', '--save'])
+            '--disable-sudo', '--save']),
+            mock.call([plugin._cargo, "fetch"])])
