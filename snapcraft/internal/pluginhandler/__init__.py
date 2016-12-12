e38976d01aa053e413aa8ab9acb29725f95372c1
Sergio Schvezov <sergio.schvezov@ubuntu.com>
2016-12-12 20:07:30 +0000

    pluginhandler: part scriplet to install
    The `install` property for a part is a script to be run after the `build` step for a plugin is complete.
    
    LP: #1649683
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

--- snapcraft/internal/pluginhandler/__init__.py
+++ snapcraft/internal/pluginhandler/__init__.py
@@ -42,6 +42,7 @@ from snapcraft.internal import (
     sources,
     states,
 )
+from ._scriptlets import ScriptRunner
 
 logger = logging.getLogger(__name__)
 
@@ -386,7 +387,10 @@ class PluginHandler:
         shutil.copytree(self.code.sourcedir, self.code.build_basedir,
                         symlinks=True, ignore=ignore)
 
+        script_runner = ScriptRunner(builddir=self.code.build_basedir)
+
         self.code.build()
+        script_runner.install(scriptlet=self._part_properties.get('install'))
 
         self.mark_build_done()
 
