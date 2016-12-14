a2bf08709c3019e6b6ddd1f6252099a92e671e57
Leo Arias <leo.arias@canonical.com>
2016-12-14 04:59:36 +0000

    Fix integration tests in armhf

--- integration_tests/test_hardlink_src_old_tree.py
+++ integration_tests/test_hardlink_src_old_tree.py
@@ -14,13 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import fileinput
 import os
+import platform
 
 from testtools.matchers import (
     FileExists,
     Not
 )
 
+from snapcraft import _options
 import integration_tests
 
 
@@ -28,6 +31,14 @@ class HardlinkSrcOldTreeTestCase(integration_tests.TestCase):
 
     def test_build_old_tree_still_filters(self):
         project_dir = 'old-part-src'
+        work_dir = self.copy_project_to_tmp(project_dir)
+        arch = _options._ARCH_TRANSLATIONS[platform.machine()]['deb']
+        with fileinput.FileInput(
+                os.path.join(work_dir, 'parts', 'part-name', 'state', 'pull'),
+                inplace=True) as pull_state:
+            for line in pull_state:
+                print(line.replace('$arch', arch), end='')
+
         self.run_snapcraft('build', project_dir)
 
         # Assert that the file we wanted to snap made it into the builddir
