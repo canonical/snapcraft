04fa355b3ce942c3b16511427266552a42f173d9
Kyle Fazzari <kyrofa@users.noreply.github.com>
2016-12-15 03:57:18 -0800

    sources: refactor zip source into module. (#970)
    Extract the Zip source into `sources/_zip.py`.
    
    LP: #1648906
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- snapcraft/tests/sources/test_sources.py
+++ snapcraft/tests/sources/test_sources.py
@@ -15,44 +15,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import unittest.mock
 
 from snapcraft.internal import sources
 from snapcraft import tests
-
-
-class TestZip(tests.FakeFileHTTPServerBasedTestCase):
-
-    @unittest.mock.patch('zipfile.ZipFile')
-    def test_pull_zipfile_must_download_and_extract(self, mock_zip):
-        dest_dir = 'src'
-        os.makedirs(dest_dir)
-        zip_file_name = 'test.zip'
-        source = 'http://{}:{}/{file_name}'.format(
-            *self.server.server_address, file_name=zip_file_name)
-        zip_source = sources.Zip(source, dest_dir)
-
-        zip_source.pull()
-
-        mock_zip.assert_called_once_with(
-            os.path.join(zip_source.source_dir, zip_file_name))
-
-    @unittest.mock.patch('zipfile.ZipFile')
-    def test_extract_and_keep_zipfile(self, mock_zip):
-        zip_file_name = 'test.zip'
-        source = 'http://{}:{}/{file_name}'.format(
-            *self.server.server_address, file_name=zip_file_name)
-        dest_dir = os.path.abspath(os.curdir)
-        zip_source = sources.Zip(source, dest_dir)
-
-        zip_source.download()
-        zip_source.provision(dst=dest_dir, keep_zip=True)
-
-        zip_download = os.path.join(zip_source.source_dir, zip_file_name)
-        mock_zip.assert_called_once_with(zip_download)
-
-        with open(zip_download, 'r') as zip_file:
-            self.assertEqual('Test fake compressed file', zip_file.read())
 
 
 class TestUri(tests.TestCase):
