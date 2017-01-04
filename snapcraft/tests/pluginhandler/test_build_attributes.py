54aa0ec1b0071fd1cc9c826729ad46cfba8505e6
Kyle Fazzari <kyrofa@users.noreply.github.com>
2017-01-04 09:04:29 -0800

    pluginhandler: add support for disabling system library migration (#989)
    Currently snapcraft, during the prime step for a part, checks to see if
    that part needs any libraries from the system. This is not always
    desired, as in some cases the libraries will be provided by another snap
    via the content interface.
    
    This commit introduces a new property to the base part YAML:
    build-attributes. It's simply a list of build attributes, only one of
    which exists right now: `no-system-libraries`. For example:
    
        parts:
          # ...
          example:
            plugin: cmake
            build-attributes: [no-system-libraries]
          # ...
    
    Using the `no-system-libraries` attribute has the effect of not pulling
    any libraries from the system during the prime step for that part.
    
    LP: #1639831
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

---
+++ snapcraft/tests/pluginhandler/test_build_attributes.py
@@ -0,0 +1,28 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from snapcraft.internal.pluginhandler._build_attributes import BuildAttributes
+
+from snapcraft import tests
+
+
+class BuildAttributesTestCase(tests.TestCase):
+    def test_no_system_libraries(self):
+        build_attributes = BuildAttributes([])
+        self.assertFalse(build_attributes.no_system_libraries())
+
+        build_attributes = BuildAttributes(['no-system-libraries'])
+        self.assertTrue(build_attributes.no_system_libraries())
