cf5b6183c967d18b188c8c7759524b753afbd3a6
Joe Talbott <joe.talbott@canonical.com>
2017-01-05 17:51:23 -0500

    review cleanup.
    * remove duplicate snap file
    * move and merge alias integration tests.

--- integration_tests/test_snap.py
+++ integration_tests/test_snap.py
@@ -25,7 +25,6 @@ from testtools.matchers import (
     FileExists,
     Not,
 )
-import yaml
 
 import snapcraft
 import integration_tests
@@ -170,20 +169,3 @@ class SnapTestCase(integration_tests.TestCase):
             "Issues while validating snapcraft.yaml: found character '\\t' "
             "that cannot start any token on line 13 of snapcraft.yaml",
             str(error.output))
-
-    def test_aliases(self):
-        project_dir = 'alias'
-
-        self.run_snapcraft('prime', project_dir)
-        snap_yaml = os.path.join(project_dir, 'prime', 'meta', 'snap.yaml')
-        self.assertThat(
-            snap_yaml,
-            FileExists())
-
-        data = {}
-        with open(snap_yaml) as fp:
-            data = yaml.load(fp)
-
-        expected_aliases = ['hi.sh', 'howdy.sh']
-        self.assertEqual(set(expected_aliases),
-                         set(data['apps']['hello']['aliases']))
