c82d8a0117555e3697122d4ed39c23d9f1a6edeb
Leo Arias <yo@elopio.net>
2017-01-09 23:20:32 -0600

    tests: use rust 1.12 to test the rust-revision in armhf (#1040)
    LP: #1655203

--- integration_tests/test_rust_plugin.py
+++ integration_tests/test_rust_plugin.py
@@ -17,7 +17,6 @@
 import os
 
 import integration_tests
-import snapcraft
 
 
 class RustPluginTestCase(integration_tests.TestCase):
@@ -31,16 +30,13 @@ class RustPluginTestCase(integration_tests.TestCase):
         self.assertEqual('There is rust on snaps!\n', binary_output)
 
     def test_stage_rust_with_revision(self):
-        if snapcraft.ProjectOptions().deb_arch == 'armhf':
-            self.skipTest('The rust revision chosen fails on armhf')
-
         project_dir = 'rust-with-revision'
         self.run_snapcraft('stage', project_dir)
 
         binary_output = self.get_output_ignoring_non_zero_exit(
             os.path.join('stage', 'bin', 'rust-with-revision'),
             cwd=project_dir)
-        self.assertIn('Rust revision: 1.13.0', binary_output)
+        self.assertIn('Rust revision: 1.12.0', binary_output)
 
     def test_stage_rust_plugin_with_conditional_feature(self):
         project_dir = 'rust-with-conditional'
