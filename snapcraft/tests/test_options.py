645707ffd8592a6f291a6f7589ddb3afb6358f56
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-01-20 15:47:27 +0100

    test_options: add tests for 64-bit kernels on 32-bit userspace

--- snapcraft/tests/test_options.py
+++ snapcraft/tests/test_options.py
@@ -29,6 +29,12 @@ class OptionsTestCase(tests.TestCase):
             expected_arch_triplet='x86_64-linux-gnu',
             expected_deb_arch='amd64',
             expected_kernel_arch='x86')),
+        ('amd64-kernel-i686-userspace', dict(
+            machine='x86_64',
+            architecture=('32bit', 'ELF'),
+            expected_arch_triplet='i386-linux-gnu',
+            expected_deb_arch='i386',
+            expected_kernel_arch='x86')),
         ('i686', dict(
             machine='i686',
             architecture=('32bit', 'ELF'),
@@ -47,6 +53,12 @@ class OptionsTestCase(tests.TestCase):
             expected_arch_triplet='aarch64-linux-gnu',
             expected_deb_arch='arm64',
             expected_kernel_arch='arm64')),
+        ('aarch64-kernel-armv7l-userspace', dict(
+            machine='aarch64',
+            architecture=('32bit', 'ELF'),
+            expected_arch_triplet='arm-linux-gnueabihf',
+            expected_deb_arch='armhf',
+            expected_kernel_arch='arm')),
         ('ppc', dict(
             machine='ppc',
             architecture=('32bit', 'ELF'),
@@ -58,6 +70,12 @@ class OptionsTestCase(tests.TestCase):
             architecture=('64bit', 'ELF'),
             expected_arch_triplet='powerpc64le-linux-gnu',
             expected_deb_arch='ppc64el',
+            expected_kernel_arch='powerpc')),
+        ('ppc64le-kernel-ppc-userspace', dict(
+            machine='ppc64le',
+            architecture=('32bit', 'ELF'),
+            expected_arch_triplet='powerpc-linux-gnu',
+            expected_deb_arch='powerpc',
             expected_kernel_arch='powerpc')),
         ('s390x', dict(
             machine='s390x',
