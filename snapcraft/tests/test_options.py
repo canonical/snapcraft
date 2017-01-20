654d29b2a720b98fc5982a24477544eb8c137473
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-01-20 15:42:49 +0100

    test_options: add architecture support

--- snapcraft/tests/test_options.py
+++ snapcraft/tests/test_options.py
@@ -25,44 +25,54 @@ class OptionsTestCase(tests.TestCase):
     scenarios = [
         ('amd64', dict(
             machine='x86_64',
+            architecture=('64bit', 'ELF'),
             expected_arch_triplet='x86_64-linux-gnu',
             expected_deb_arch='amd64',
             expected_kernel_arch='x86')),
         ('i686', dict(
             machine='i686',
+            architecture=('32bit', 'ELF'),
             expected_arch_triplet='i386-linux-gnu',
             expected_deb_arch='i386',
             expected_kernel_arch='x86')),
         ('armv7l', dict(
             machine='armv7l',
+            architecture=('32bit', 'ELF'),
             expected_arch_triplet='arm-linux-gnueabihf',
             expected_deb_arch='armhf',
             expected_kernel_arch='arm')),
         ('aarch64', dict(
             machine='aarch64',
+            architecture=('64bit', 'ELF'),
             expected_arch_triplet='aarch64-linux-gnu',
             expected_deb_arch='arm64',
             expected_kernel_arch='arm64')),
         ('ppc', dict(
             machine='ppc',
+            architecture=('32bit', 'ELF'),
             expected_arch_triplet='powerpc-linux-gnu',
             expected_deb_arch='powerpc',
             expected_kernel_arch='powerpc')),
         ('ppc64le', dict(
             machine='ppc64le',
+            architecture=('64bit', 'ELF'),
             expected_arch_triplet='powerpc64le-linux-gnu',
             expected_deb_arch='ppc64el',
             expected_kernel_arch='powerpc')),
         ('s390x', dict(
             machine='s390x',
+            architecture=('64bit', 'ELF'),
             expected_arch_triplet='s390x-linux-gnu',
             expected_deb_arch='s390x',
             expected_kernel_arch='s390x'))
     ]
 
+    @mock.patch('platform.architecture')
     @mock.patch('platform.machine')
-    def test_architecture_options(self, mock_platform_machine):
+    def test_architecture_options(self,
+            mock_platform_machine, mock_platform_architecture):
         mock_platform_machine.return_value = self.machine
+        mock_platform_architecture.return_value = self.architecture
         options = snapcraft.ProjectOptions()
         self.assertEqual(options.arch_triplet, self.expected_arch_triplet)
         self.assertEqual(options.deb_arch, self.expected_deb_arch)
