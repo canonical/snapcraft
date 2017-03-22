7a9d56b4b196b91b8704bcd9dbd68e30a4c1513c
Joe Talbott <joe.talbott@canonical.com>
2017-03-22 13:53:47 -0400

    asset-tracking: remove dup test and add global versioned package

--- snapcraft/tests/repo/test_deb.py
+++ snapcraft/tests/repo/test_deb.py
@@ -260,22 +260,6 @@ class BuildPackagesTestCase(tests.TestCase):
 
     @patch('snapcraft.repo._deb.is_dumb_terminal')
     @patch('subprocess.check_call')
-    def test_install_versioned_build_package(
-            self, mock_check_call, mock_is_dumb_terminal):
-        mock_is_dumb_terminal.return_value = False
-        self.install_test_packages(self.test_packages)
-
-        installable = self.get_installable_packages(self.test_packages)
-        mock_check_call.assert_has_calls([
-            call('sudo apt-get --no-install-recommends -y '
-                 '-o Dpkg::Progress-Fancy=1 install'.split() +
-                 sorted(set(installable)),
-                 env={'DEBIAN_FRONTEND': 'noninteractive',
-                      'DEBCONF_NONINTERACTIVE_SEEN': 'true'})
-        ])
-
-    @patch('snapcraft.repo._deb.is_dumb_terminal')
-    @patch('subprocess.check_call')
     def test_install_buid_package_in_dumb_terminal(
             self, mock_check_call, mock_is_dumb_terminal):
         mock_is_dumb_terminal.return_value = True
