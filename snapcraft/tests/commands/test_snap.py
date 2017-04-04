7454bb10450855743611702bc82bf45a8fba0f41
Christian Dywan <christian.dywan@canonical.com>
2017-04-04 15:55:17 +0200

    Add unit test for containerized 'build' command

--- snapcraft/tests/commands/test_snap.py
+++ snapcraft/tests/commands/test_snap.py
@@ -18,6 +18,8 @@ import logging
 import os
 import os.path
 import subprocess
+import tarfile
+from testtools.matchers import Contains
 from unittest import mock
 
 import fixtures
@@ -29,6 +31,100 @@ from testtools.matchers import (
 
 from snapcraft.main import main
 from snapcraft import tests
+from snapcraft.tests.test_lxd import check_output_side_effect
+
+
+class SnapCommandContainerized(tests.TestCase):
+
+    yaml_template = """name: snap-test
+version: 1.0
+summary: test containerized snap
+description: if snap is succesful a snap package will be available
+architectures: ['amd64']
+type: {}
+confinement: strict
+grade: stable
+
+parts:
+    part1:
+      plugin: nil
+"""
+
+    def setUp(self):
+        super().setUp()
+        patcher = mock.patch('snapcraft.internal.lxd.check_call')
+        self.check_call_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        patcher = mock.patch('snapcraft.internal.lxd.check_output')
+        self.check_output_mock = patcher.start()
+        self.check_output_mock.side_effect = check_output_side_effect()
+        self.addCleanup(patcher.stop)
+
+        patcher = mock.patch('snapcraft.internal.lxd.sleep', lambda _: None)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def make_snapcraft_yaml(self, n=1):
+        super().make_snapcraft_yaml(self.yaml_template)
+        self.state_dir = os.path.join(self.parts_dir, 'part1', 'state')
+
+    def test_snap_defaults(self):
+        fake_logger = fixtures.FakeLogger(level=logging.INFO)
+        self.useFixture(fake_logger)
+        self.useFixture(fixtures.EnvironmentVariable(
+                'SNAPCRAFT_CONTAINER_BUILDS', '1'))
+
+        self.make_snapcraft_yaml()
+        # simulate build artifacts
+
+        dirs = [
+            os.path.join(self.parts_dir, 'part1', 'src'),
+            self.stage_dir,
+            self.prime_dir,
+            os.path.join(self.parts_dir, 'plugins'),
+        ]
+        files_tar = [
+            os.path.join(self.parts_dir, 'plugins', 'x-plugin.py'),
+            'main.c',
+        ]
+        files_no_tar = [
+            os.path.join(self.stage_dir, 'binary'),
+            os.path.join(self.prime_dir, 'binary'),
+            'snap-test.snap',
+            'snap-test_1.0_source.tar.bz2',
+        ]
+        for d in dirs:
+            os.makedirs(d)
+        for f in files_tar + files_no_tar:
+            open(f, 'w').close()
+
+        main(['snap', '--debug'])
+
+        self.assertIn(
+            'Setting up container with project assets\n'
+            'Waiting for a network connection...\n'
+            'Network connection established\n'
+            'Retrieved snap-test_1.0_amd64.snap\n',
+            fake_logger.output)
+
+        with tarfile.open('snap-test_1.0_source.tar.bz2') as tar:
+            tar_members = tar.getnames()
+
+        for f in files_no_tar:
+            f = os.path.relpath(f)
+            self.assertFalse('./{}'.format(f) in tar_members,
+                             '{} should not be in {}'.format(f, tar_members))
+        for f in files_tar:
+            f = os.path.relpath(f)
+            self.assertTrue('./{}'.format(f) in tar_members,
+                            '{} should be in {}'.format(f, tar_members))
+
+        # Also assert that the snapcraft.yaml made it into the cleanbuild tar
+        self.assertThat(
+            tar_members,
+            Contains(os.path.join('.', 'snap', 'snapcraft.yaml')),
+            'snap/snapcraft unexpectedly excluded from tarball')
 
 
 class SnapCommandTestCase(tests.TestCase):
