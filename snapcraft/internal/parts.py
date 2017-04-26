14512358fadb871e85a4ff3c42c79568715345cf
Leo Arias <leo.arias@canonical.com>
2017-04-26 05:53:33 +0000

    parts: remove the deprecated snap keyword from the internal parts representation

--- snapcraft/internal/parts.py
+++ snapcraft/internal/parts.py
@@ -188,10 +188,11 @@ class PartsConfig:
             if 'filesets' in properties:
                 del properties['filesets']
 
-            # FIXME: snap is deprecated, rewrite it to prime instead.
-            if properties.get('snap'):
+            # Handle the deprecated snap keyword.
+            snap = properties.pop('snap')
+            if snap:
                 deprecations.handle_deprecation_notice('dn1')
-                properties['prime'] = properties.pop('snap')
+                properties['prime'] = snap
 
             self.load_plugin(part_name, plugin_name, properties)
 
