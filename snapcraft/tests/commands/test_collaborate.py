1f97406372efe521737636beb1939242e869dcb2
Leo Arias <leo.arias@canonical.com>
2017-04-28 08:42:13 +0000

    Add the edit tests

--- snapcraft/tests/commands/test_collaborate.py
+++ snapcraft/tests/commands/test_collaborate.py
@@ -29,58 +29,6 @@ from snapcraft import (
 from snapcraft import _store
 
 
-class CollaborateBaseTestCase(tests.TestCase):
-
-    def setUp(self):
-        super().setUp()
-        self.fake_logger = fixtures.FakeLogger(level=logging.INFO)
-        self.useFixture(self.fake_logger)
-        self.fake_store = tests.fixture_setup.FakeStore()
-        self.useFixture(self.fake_store)
-        self.useFixture(fixtures.EnvironmentVariable('EDITOR', 'vi'))
-        self.client = storeapi.StoreClient()
-        patcher = mock.patch('snapcraft._store.Popen')
-        self.popen_mock = patcher.start()
-        process_mock = mock.Mock()
-        process_mock.returncode = 0
-        process_mock.communicate.return_value = [b'foo', b'']
-        self.popen_mock.return_value = process_mock
-        self.addCleanup(patcher.stop)
-        patcher = mock.patch('subprocess.check_call')
-        self.check_call_mock = patcher.start()
-        self.addCleanup(patcher.stop)
-
-    def _generate_mock(self, *, existing_value=None, return_value=None):
-        if not return_value:
-            return_value = {'developers': []}
-
-        def mocked_call(*args):
-            edited_file = args[0][1]
-            with open(edited_file, 'w') as f:
-                yaml.dump(return_value, stream=f)
-        return mocked_call
-
-
-class CollaborateTestCase(CollaborateBaseTestCase):
-
-    def test_collaborate_success(self):
-        self.check_call_mock.side_effect = self._generate_mock(
-            return_value={'developers': [{
-                'developer-id': 'dummy-id',
-                'since': '2015-07-19 19:30:00'}]})
-        self.client.login('dummy', 'test correct password')
-        _store.collaborate('ubuntu-core', 'keyname')
-
-        self.popen_mock.assert_called_with(['snap', 'sign', '-k', 'keyname'],
-                                           stderr=-1, stdin=-1, stdout=-1)
-        self.assertIn('Signing developers assertion for ubuntu-core',
-                      self.fake_logger.output)
-        self.assertNotIn('Error signing developers assertion',
-                         self.fake_logger.output)
-        self.assertNotIn('Invalid response from the server',
-                         self.fake_logger.output)
-
-
 class EditCollaboratorsTestCase(tests.TestCase):
 
     def setUp(self):
@@ -89,13 +37,67 @@ class EditCollaboratorsTestCase(tests.TestCase):
         patcher.start()
         self.addCleanup(patcher.stop)
 
-    def test_edit_collaborators_must_write_collaboration_header(self):
+    def test_edit_collaborators_must_write_header_and_developers(self):
+        developers_from_assertion = [
+            {'developer-id': 'test-dev-id1',
+             'since': '2017-02-10T08:35:00.000000Z',
+             'until': '2018-02-10T08:35:00.000000Z'},
+            {'developer-id': 'test-dev-id2',
+             'since': '2016-02-10T08:35:00.000000Z',
+             'until': '2019-02-10T08:35:00.000000Z'},
+        ]
+
+        existing_developers = ("developers:\n"
+                               "- developer-id: test-dev-id1\n"
+                               "  since: '2017-02-10 08:35:00'\n"
+                               "  until: '2018-02-10 08:35:00'\n"
+                               "- developer-id: test-dev-id2\n"
+                               "  since: '2016-02-10 08:35:00'\n"
+                               "  until: '2019-02-10 08:35:00'\n")
+        expected_written = (_store._COLLABORATION_HEADER + '\n' +
+                            existing_developers)
+
         with mock.patch(
                 'builtins.open',
-                mock.mock_open(read_data='{developers: []}')) as mock_open:
-            _store._edit_collaborators({})
+                new_callable=mock.mock_open,
+                read_data=expected_written) as mock_open:
+            developers_for_assertion = _store._edit_collaborators(
+                developers_from_assertion)
 
-        mock_open().write.assert_called_with([_store._COLLABORATION_HEADER], mock.ANY)
+        written = ''
+        for call in mock_open().write.call_args_list:
+            written += str(call.call_list()[0][0][0])
+        self.assertEqual(expected_written, written)
+        self.assertEqual(developers_for_assertion, developers_from_assertion)
+
+    def test_edit_collaborators_must_return_new_developers(self):
+        developers_from_assertion = [
+            {'developer-id': 'test-dev-id1',
+             'since': '2017-02-10T08:35:00.000000Z',
+             'until': '2018-02-10T08:35:00.000000Z'},
+        ]
+
+        new_developers = ("developers:\n"
+                          "- developer-id: test-dev-id1\n"
+                          "  since: '2017-02-10 08:35:00'\n"
+                          "  until: '2018-02-10 08:35:00'\n"
+                          "- developer-id: test-dev-id2\n"
+                          "  since: '2016-02-10 08:35:00'\n"
+                          "  until: '2019-02-10 08:35:00'\n")
+
+        with mock.patch(
+                'builtins.open',
+                new_callable=mock.mock_open,
+                read_data=new_developers) as mock_open:
+            developers_for_assertion = _store._edit_collaborators(
+                developers_from_assertion)
+
+        expected_developers = developers_from_assertion + [{
+            'developer-id': 'test-dev-id2',
+            'since': '2016-02-10T08:35:00.000000Z',
+            'until': '2019-02-10T08:35:00.000000Z',
+        }]
+        self.assertEqual(developers_for_assertion, expected_developers)
 
 
 class EditCollaboratorsOpenEditorTestCase(tests.TestCase):
@@ -117,14 +119,57 @@ class EditCollaboratorsOpenEditorTestCase(tests.TestCase):
         self.check_call_mock.assert_called_with([self.expected, mock.ANY])
 
 
+class CollaborateBaseTestCase(tests.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.fake_logger = fixtures.FakeLogger(level=logging.INFO)
+        self.useFixture(self.fake_logger)
+        self.fake_store = tests.fixture_setup.FakeStore()
+        self.useFixture(self.fake_store)
+        self.client = storeapi.StoreClient()
+        patcher = mock.patch('snapcraft._store.Popen')
+        self.popen_mock = patcher.start()
+        process_mock = mock.Mock()
+        process_mock.returncode = 0
+        process_mock.communicate.return_value = [b'foo', b'']
+        self.popen_mock.return_value = process_mock
+        self.addCleanup(patcher.stop)
+        patcher = mock.patch('snapcraft._store._edit_collaborators')
+        self.edit_collaborators_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
+
+class CollaborateTestCase(CollaborateBaseTestCase):
+
+    def test_collaborate_success(self):
+        self.edit_collaborators_mock.return_value = {
+            'developers': [{
+                'developer-id': 'dummy-id',
+                'since': '2015-07-19 19:30:00'}]
+        }
+        self.client.login('dummy', 'test correct password')
+        _store.collaborate('ubuntu-core', 'keyname')
+
+        self.popen_mock.assert_called_with(['snap', 'sign', '-k', 'keyname'],
+                                           stderr=-1, stdin=-1, stdout=-1)
+        self.assertIn('Signing developers assertion for ubuntu-core',
+                      self.fake_logger.output)
+        self.assertNotIn('Error signing developers assertion',
+                         self.fake_logger.output)
+        self.assertNotIn('Invalid response from the server',
+                         self.fake_logger.output)
+
+
 class CollaborateErrorsTestCase(CollaborateBaseTestCase):
 
     def setUp(self):
         super().setUp()
-        self.check_call_mock.side_effect = self._generate_mock(
-            return_value={'developers': [{
+        self.edit_collaborators_mock.return_value = {
+            'developers': [{
                 'developer-id': 'dummy-id',
-                'since': '2015-07-19 19:30:00'}]})
+                'since': '2015-07-19 19:30:00'}]
+        }
 
     def test_collaborate_snap_not_found(self):
         self.client.login('dummy', 'test correct password')
