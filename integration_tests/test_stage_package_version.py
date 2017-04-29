3241a9da68d2ea4d4898b10292ed4831dbb585a9
Leo Arias <leo.arias@canonical.com>
2017-04-28 18:32:55 -0600

    tests: refactor tests with build and stage packages (#1290)

--- integration_tests/test_stage_package_version.py
+++ integration_tests/test_stage_package_version.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import subprocess
 
 import integration_tests
@@ -21,12 +22,16 @@ import integration_tests
 
 class StagePackageVersionTestCase(integration_tests.TestCase):
 
-    def test_stage_package_gets_version(self):
+    def test_stage_package_with_invalid_version_must_fail(self):
+        self.copy_project_to_cwd('stage-package')
+        self.set_stage_package_version(
+            os.path.join('snap', 'snapcraft.yaml'),
+            part='hello', package='hello', version='invalid')
         error = self.assertRaises(
             subprocess.CalledProcessError,
-            self.run_snapcraft, 'pull', 'stage-package-version')
+            self.run_snapcraft, 'pull')
         self.assertIn(
             "Error downloading stage packages for part 'hello': "
-            "The package 'hello=x.y-z' was not found.",
+            "The package 'hello=invalid' was not found.",
             str(error.output)
         )
