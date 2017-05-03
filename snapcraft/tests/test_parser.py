3cc70322886b5794cbf5447d8587252bf6075ca5
edvega <edvegavalerio@gmail.com>
2017-05-03 09:30:45 -0600

    Get proper KeyError for API users, when source-type is unknown

--- snapcraft/tests/test_parser.py
+++ snapcraft/tests/test_parser.py
@@ -1189,7 +1189,7 @@ class MissingAssetsTestCase(TestCase):
     @mock.patch('snapcraft.internal.sources._get_source_type_from_uri')
     def test_filenotfound_for_non_repos(self, mock_type, mock_pull):
         mock_pull.side_effect = FileNotFoundError()
-        mock_type.return_value = None
+        mock_type.return_value = ''
         fake_logger = fixtures.FakeLogger(level=logging.ERROR)
         self.useFixture(fake_logger)
 
