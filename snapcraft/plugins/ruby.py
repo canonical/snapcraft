0c58c4483c6fb7c3a74ec7d80f0edf0fa6a3bc46
jamesbeedy <jamesbeedy@gmail.com>
2017-06-15 02:25:46 +0000

    add copyright header

--- snapcraft/plugins/ruby.py
+++ snapcraft/plugins/ruby.py
@@ -1,3 +1,19 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2017 James Beedy <jamesbeedy@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """The ruby plugin is useful for ruby based parts.
 The plugin uses gem to install dependencies from a `Gemfile`.
 This plugin uses the common plugin keywords as well as those for "sources".
