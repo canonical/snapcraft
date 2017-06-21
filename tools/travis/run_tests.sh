97a2b2c85f11ccbb3fe1bb19262f902fbeb51f19
Leo Arias <leo.arias@canonical.com>
2017-06-21 19:07:16 +0000

    tests: allow to filter tests in docker

--- tools/travis/run_tests.sh
+++ tools/travis/run_tests.sh
@@ -27,6 +27,10 @@ fi
 
 test="$1"
 
+if [ "$#" -gt 1 ]; then
+    pattern="$2"
+fi
+
 if [ "$test" = "static" ]; then
     dependencies="apt install -y python3-pip && python3 -m pip install -r requirements-devel.txt"
 elif [ "$test" = "unit" ]; then
@@ -41,7 +45,7 @@ fi
 script_path="$(dirname "$0")"
 "$script_path/run_docker_container.sh" test-runner
 docker exec -i test-runner sh -c "$dependencies"
-docker exec -i test-runner ./runtests.sh $test
+docker exec -i test-runner ./runtests.sh $test $pattern
 
 if [ "$test" = "unit" ]; then
     # Report code coverage.
