a8c33e6cba04cf94f371fbd8d45bd71901c42378
jamesbeedy <jamesbeedy@gmail.com>
2017-07-12 18:54:33 -0400

    s/'/"/

--- snapcraft/plugins/ruby.py
+++ snapcraft/plugins/ruby.py
@@ -16,7 +16,7 @@
 
 """The ruby plugin is useful for ruby based parts.
 The plugin uses gem to install dependencies from a `Gemfile`.
-This plugin uses the common plugin keywords as well as those for "sources".
+This plugin uses the common plugin keywords as well as those for 'sources'.
 For more information check the 'plugins' topic for the former and the
 'sources' topic for the latter.
 Additionally, this plugin uses the following plugin-specific keywords:
@@ -28,7 +28,6 @@ Additionally, this plugin uses the following plugin-specific keywords:
       The version of ruby you want this snap to run.
 """
 import os
-import re
 import platform
 import glob
 from contextlib import contextmanager
@@ -51,18 +50,18 @@ class RubyPlugin(BasePlugin):
     def schema(cls):
         schema = super().schema()
 
-        schema['properties']['ruby-version'] = {
-            'type': 'string',
-            'default': '2.3.1'
+        schema["properties"]["ruby-version"] = {
+            "type": "string",
+            "default": "2.3.1"
         }
-        schema['properties']['gems'] = {
-            'type': 'array',
-            'minitems': 1,
-            'uniqueItems': True,
-            'items': {
-                'type': 'string'
+        schema["properties"]["gems"] = {
+            "type": "array",
+            "minitems": 1,
+            "uniqueItems": True,
+            "items": {
+                "type": "string"
             },
-            'default': []
+            "default": []
         }
         return schema
 
@@ -70,22 +69,22 @@ class RubyPlugin(BasePlugin):
     def get_pull_properties(cls):
         # Inform Snapcraft of the properties associated with pulling. If these
         # change in the YAML Snapcraft will consider the build step dirty.
-        return ['ruby-version', 'gems']
+        return ["ruby-version", "gems"]
 
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
 
         self._ruby_version = self.options.ruby_version
-        self._ruby_part_dir = os.path.join(self.partdir, 'ruby')
+        self._ruby_part_dir = os.path.join(self.partdir, "ruby")
         self._ruby_download_url = \
-            'https://cache.ruby-lang.org/pub/ruby/ruby-{}.tar.gz'.format(
+            "https://cache.ruby-lang.org/pub/ruby/ruby-{}.tar.gz".format(
                 self._ruby_version)
         self._ruby_tar = Tar(self._ruby_download_url, self._ruby_part_dir)
         self._gems = self.options.gems
         self._install_bundler = False
 
-        self.build_packages.extend(['gcc', 'g++', 'make', 'zlib1g-dev',
-                                    'libssl-dev', 'libreadline-dev'])
+        self.build_packages.extend(["gcc", "g++", "make", "zlib1g-dev",
+                                    "libssl-dev", "libreadline-dev"])
 
     def pull(self):
         super().pull()
@@ -99,49 +98,49 @@ class RubyPlugin(BasePlugin):
     def env(self, root):
         env = super().env(root)
 
-        env.append('PATH={}:{}'.format(
-            os.path.join(root, 'bin'), os.environ['PATH']))
+        env.append("PATH={}:{}".format(
+            os.path.join(root, "bin"), os.environ["PATH"]))
 
-        env.append('RUBYPATH={}'.format(os.path.join(root, 'bin')))
+        env.append("RUBYPATH={}".format(os.path.join(root, "bin")))
 
-        rubydir = os.path.join(root, 'lib', 'ruby')
+        rubydir = os.path.join(root, "lib", "ruby")
 
         with cd(rubydir):
-            ruby_version_dir = glob.glob('[0-9].[0-9].[0-9]')[0]
-        
+            ruby_version_dir = glob.glob("[0-9].[0-9].[0-9]")[0]
+
         rubylib = os.path.join(rubydir, ruby_version_dir)
 
-        env.append('RUBYLIB={}:{}'.format(
+        env.append("RUBYLIB={}:{}".format(
             rubylib, os.path.join(
-                rubylib, '{}-linux'.format(platform.machine()))))
+                rubylib, "{}-linux".format(platform.machine()))))
 
-        env.append('GEM_HOME={}'.format(
-            os.path.join(rubydir, 'gems', ruby_version_dir)))
+        env.append("GEM_HOME={}".format(
+            os.path.join(rubydir, "gems", ruby_version_dir)))
 
-        env.append('GEM_PATH={}'.format(
-            os.path.join(rubydir, 'gems', ruby_version_dir)))
+        env.append("GEM_PATH={}".format(
+            os.path.join(rubydir, "gems", ruby_version_dir)))
 
         return env
 
     def _ruby_install(self, builddir):
         self._ruby_tar.provision(
             builddir, clean_target=False, keep_tarball=True)
-        self.run(['./configure', '--disable-install-rdoc', '--prefix=/'],
+        self.run(["./configure", "--disable-install-rdoc", "--prefix=/"],
                  cwd=builddir)
-        self.run(['make', '-j{}'.format(self.parallel_build_count)],
+        self.run(["make", "-j{}".format(self.parallel_build_count)],
                  cwd=builddir)
-        self.run(['make', 'install', 'DESTDIR={}'.format(self.installdir)],
+        self.run(["make", "install", "DESTDIR={}".format(self.installdir)],
                  cwd=builddir)
 
     def _gem_install(self):
-        if os.path.exists(os.path.join(self.builddir, 'Gemfile')):
+        if os.path.exists(os.path.join(self.builddir, "Gemfile")):
             self._install_bundler = True
-            self._gems = self._gems + ['bundler']
+            self._gems = self._gems + ["bundler"]
         if self._gems:
-            self.run([os.path.join(self.installdir, 'bin', 'ruby'),
-                      os.path.join(self.installdir, 'bin', 'gem'),
-                      'install'] + self._gems)
+            self.run([os.path.join(self.installdir, "bin", "ruby"),
+                      os.path.join(self.installdir, "bin", "gem"),
+                      "install"] + self._gems)
 
     def _bundle_install(self):
-        self.run([os.path.join(self.installdir, 'bin', 'ruby'),
-                  os.path.join(self.installdir, 'bin', 'bundle'), 'install'])
+        self.run([os.path.join(self.installdir, "bin", "ruby"),
+                  os.path.join(self.installdir, "bin", "bundle"), "install"])
