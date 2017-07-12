3324e4e8ee33409ac941ec87015410bcb59cd490
jamesbeedy <jamesbeedy@gmail.com>
2017-07-12 19:19:21 -0400

    add app to test

--- integration_tests/plugins/test_ruby_plugin.py
+++ integration_tests/plugins/test_ruby_plugin.py
@@ -15,28 +15,46 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-
+import glob
 import platform
-
 import integration_tests
+from contextlib import contextmanager
+
+
+@contextmanager
+def cd(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
 
 
 class RubyPluginTestCase(integration_tests.TestCase):
 
-    def env(self, root, ruby_version_dir):
+    def env(self, root):
         env = []
-        env.append('PATH={}:{}'.format(
-            os.path.join(root, 'bin'), os.environ['PATH']))
-        env.append('RUBYPATH={}'.format(os.path.join(root, 'bin')))
-        rubydir = os.path.join(root, 'lib', 'ruby')
+        env.append("PATH={}:{}".format(
+            os.path.join(root, "bin"), os.environ["PATH"]))
+
+        env.append("RUBYPATH={}".format(os.path.join(root, "bin")))
+
+        rubydir = os.path.join(root, "lib", "ruby")
+
+        with cd(rubydir):
+            ruby_version_dir = glob.glob("[0-9].[0-9].[0-9]")[0]
+
         rubylib = os.path.join(rubydir, ruby_version_dir)
-        env.append('RUBYLIB={}:{}'.format(
+
+        env.append("RUBYLIB={}:{}".format(
             rubylib, os.path.join(
-                rubylib, '{}-linux'.format(platform.machine()))))
-        env.append('GEM_HOME={}'.format(
-            os.path.join(rubydir, 'gems', ruby_version_dir)))
-        env.append('GEM_PATH={}'.format(
-            os.path.join(rubydir, 'gems', ruby_version_dir)))
+                rubylib, "{}-linux".format(platform.machine()))))
+
+        env.append("GEM_HOME={}".format(
+            os.path.join(rubydir, "gems", ruby_version_dir)))
+
+        env.append("GEM_PATH={}".format(
+            os.path.join(rubydir, "gems", ruby_version_dir)))
+
         return env
 
     def test_bins_exist(self):
@@ -52,9 +70,8 @@ class RubyPluginTestCase(integration_tests.TestCase):
         self.assertTrue(os.path.exists(rack_path))
 
     def test_ruby_hello(self):
-        self.run_snapcraft('stage', 'ruby-hello',
-                           env=self.env(self.stage_dir, "2.3.0"))
+        self.run_snapcraft('stage', 'ruby-hello', env=self.env(self.stage_dir))
         binary_output = self.get_output_ignoring_non_zero_exit(
-            os.path.join(self.stage_dir, 'bin', 'ruby'),
+            os.path.join(self.stage_dir, 'bin', os.path.basename(self.path)),
             os.path.join(self.stage_dir, 'ruby-hello.rb'))
         self.assertEqual("Ruby says, Hello snapcraft.\n", binary_output)
