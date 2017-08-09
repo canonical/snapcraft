ea3670dc5e4f33bce7274ddc28b5b3ca81be2993
Christian Dywan <christian@twotoasts.de>
2017-08-10 00:44:04 +0200

    kbuild plugin: move over the cross-compiling logic from the kernel plugin (#1417)
    Move the cross-compiling bits from the `kernel` plugin to the `kbuild` plugin, taking advantage of the fact that kbuild's being used as a superclass.

--- snapcraft/tests/plugins/test_kbuild.py
+++ snapcraft/tests/plugins/test_kbuild.py
@@ -254,28 +254,56 @@ ACCEPT=n
 """
         self.assertEqual(config_contents, expected_config)
 
+
+class KBuildCrossCompilePluginTestCase(tests.TestCase):
+
+    scenarios = [
+        ('armv7l', dict(deb_arch='armhf')),
+        ('aarch64', dict(deb_arch='arm64')),
+        ('ppc64le', dict(deb_arch='ppc64el')),
+    ]
+
+    def setUp(self):
+        super().setUp()
+
+        class Options:
+            build_parameters = []
+            kconfigfile = None
+            kconfigflavour = None
+            kdefconfig = []
+            kconfigs = []
+            build_attributes = []
+
+        self.options = Options()
+        self.project_options = snapcraft.ProjectOptions(
+            target_deb_arch=self.deb_arch)
+
+        patcher = mock.patch('snapcraft.internal.common.run')
+        self.run_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        patcher = mock.patch('snapcraft.ProjectOptions.is_cross_compiling')
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+        patcher = mock.patch.dict(os.environ, {})
+        self.env_mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
     @mock.patch('subprocess.check_call')
     @mock.patch.object(kbuild.KBuildPlugin, 'run')
-    def test_build_without_install_target(self, run_mock, check_call_mock):
-        self.options.kconfigfile = 'config'
-        self.options.build_attributes = ['no-install']
-        with open(self.options.kconfigfile, 'w') as f:
-            f.write('ACCEPT=y\n')
-
+    def test_cross_compile(self, run_mock, check_call_mock):
         plugin = kbuild.KBuildPlugin('test-part', self.options,
                                      self.project_options)
-
-        os.makedirs(plugin.builddir)
+        plugin.enable_cross_compilation()
 
         plugin.build()
-
-        self.assertEqual(1, check_call_mock.call_count)
-        check_call_mock.assert_has_calls([
-            mock.call('yes "" | make -j2 oldconfig', shell=True,
-                      cwd=plugin.builddir),
-        ])
-
-        self.assertEqual(1, run_mock.call_count)
         run_mock.assert_has_calls([
-            mock.call(['make', '-j2']),
+            mock.call(['make', '-j1', 'ARCH={}'.format(
+                           self.project_options.kernel_arch),
+                       'CROSS_COMPILE={}'.format(
+                           self.project_options.cross_compiler_prefix),
+                       'PATH={}:/usr/{}/bin'.format(
+                           os.environ.copy().get('PATH', ''),
+                           self.project_options.arch_triplet)]),
         ])
