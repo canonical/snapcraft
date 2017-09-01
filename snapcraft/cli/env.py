fde85f86c338f4c33096f64164b5c7f9035a9a95
Christian <christian.dywan@canonical.com>
2017-09-01 17:18:09 +0200

    Use docstring in ContainerConfig.__init__

--- snapcraft/cli/env.py
+++ snapcraft/cli/env.py
@@ -21,10 +21,15 @@ from snapcraft.internal import errors, lxd
 class ContainerConfig:
 
     def __init__(self):
-        # The environment variable can be used in 3 ways
-        # 1. SNAPCRAFT_CONTAINER_BUILDS=1 enables local containers
-        # 2. SNAPCRAFT_CONTAINER_BUILDS=foobar uses the value as a remote
-        # 3. SNAPCRAFT_CONTAINER_BUILDS=0 or unset, no container is used
+        """
+        Determines if a container should be used and which remote to choose
+
+        Checks the environment variable SNAPCRAFT_CONTAINER_BUILDS:
+          1. SNAPCRAFT_CONTAINER_BUILDS=1 enables local containers
+          2. SNAPCRAFT_CONTAINER_BUILDS=foobar uses the value as a remote
+          3. SNAPCRAFT_CONTAINER_BUILDS=0 or unset, no container is used
+        """
+
         container_builds = os.environ.get('SNAPCRAFT_CONTAINER_BUILDS', '0')
         # Default remote if it's a truthy value - otherwise it's a remote name
         try:
