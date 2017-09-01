2496f1754a2e68d030db6a864b3c9a185284518f
Christian <christian.dywan@canonical.com>
2017-09-01 20:42:24 +0200

    Swap order of conditionals in FakeLXD side effect

--- snapcraft/tests/fixture_setup.py
+++ snapcraft/tests/fixture_setup.py
@@ -491,6 +491,20 @@ class FakeLXD(fixtures.Fixture):
                             'DEVICES': self.devices,
                             }).encode('utf-8')
                 return '[]'.encode('utf-8')
+            elif args[0][:2] == ['lxc', 'init']:
+                self.name = args[0][3]
+                self.status = 'Stopped'
+            elif args[0][:2] == ['lxc', 'launch']:
+                self.name = args[0][4]
+                self.status = 'Running'
+            elif args[0][:2] == ['lxc', 'stop'] and not self.status:
+                # error: not found
+                raise CalledProcessError(returncode=1, cmd=args[0])
+            # Fail on an actual snapcraft command and not the command
+            # for the installation of it.
+            elif ('snapcraft snap' in ' '.join(args[0])
+                  and self.fail_on_snapcraft_run):
+                raise CalledProcessError(returncode=255, cmd=args[0])
             elif args[0][:2] == ['lxc', 'exec']:
                 if self.status and args[0][2] == self.name:
                     if 'sshfs' in args[0]:
@@ -513,20 +527,8 @@ class FakeLXD(fixtures.Fixture):
                     def terminate(self):
                         pass
                 return Popen(args[0])
-            elif args[0][:2] == ['lxc', 'init']:
-                self.name = args[0][3]
-                self.status = 'Stopped'
-            elif args[0][:2] == ['lxc', 'launch']:
-                self.name = args[0][4]
-                self.status = 'Running'
-            elif args[0][:2] == ['lxc', 'stop'] and not self.status:
-                # error: not found
-                raise CalledProcessError(returncode=1, cmd=args[0])
-            # Fail on an actual snapcraft command and not the command
-            # for the installation of it.
-            elif ('snapcraft snap' in ' '.join(args[0])
-                  and self.fail_on_snapcraft_run):
-                raise CalledProcessError(returncode=255, cmd=args[0])
+            else:
+                return ''.encode('utf-8')
         return call_effect
 
 
