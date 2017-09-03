549fb7532e6972d698c0949dde85f7fadceab2fa
Leo Arias <leo.arias@canonical.com>
2017-09-03 22:00:21 +0000

    Fix pep8.

--- integration_tests/containers/test_cleanbuild.py
+++ integration_tests/containers/test_cleanbuild.py
@@ -28,17 +28,17 @@ class CleanbuildTestCase(integration_tests.TestCase):
             self.copy_project_to_cwd(project_dir)
 
         command = [sudo, self.snapcraft_command, '-d', 'cleanbuild']
-        popen = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
-        for stdout_line in iter(popen.stdout.readline, ''):
-            yield stdout_line
+        popen = subprocess.Popen(
+            command, stdout=subprocess.PIPE, universal_newlines=True)
+        for line in iter(popen.stdout.readline, ''):
+            print(line)
         popen.stdout.close()
         return_code = popen.wait()
         if return_code:
             raise subprocess.CalledProcessError(return_code, command)
 
     def test_cleanbuild(self):
-        for line in self.run_snapcraft_cleanbuild('assemble'):
-            print(line)
+        self.run_snapcraft_cleanbuild('assemble'):
 
         snap_source_path = 'assemble_1.0_source.tar.bz2'
         self.assertThat(snap_source_path, FileExists())
