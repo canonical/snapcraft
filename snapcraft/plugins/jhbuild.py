08c080d1f5b7f8f752217b14a661d00851ee96f2
Daniel Llewellyn <daniel@bowlhat.net>
2017-09-07 00:50:54 +0100

    switch format from percent style to `.format()` in jhbuild plugin
    as requested by code review from @elopio

--- snapcraft/plugins/jhbuild.py
+++ snapcraft/plugins/jhbuild.py
@@ -284,7 +284,8 @@ class JHBuildPlugin(snapcraft.BasePlugin):
 
         if jhbuild_user != os.getuid():
             chmod_path = os.path.dirname(os.path.dirname(self.partdir))
-            for filename in glob.iglob('%s/**' % chmod_path, recursive=True):
+            for filename in glob.iglob('{path!s}/**'.format(path=chmod_path),
+                                       recursive=True):
                 if not os.path.exists(filename):
                     continue
                 os.chown(filename, jhbuild_user, jhbuild_group)
