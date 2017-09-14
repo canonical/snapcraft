3935f09f7bda0429af37e12d6c9701faec9cd675
Kyle Fazzari <kyrofa@ubuntu.com>
2017-09-14 15:20:01 -0700

    dirs: set plugin, schema, and library dir for snap
    The default plugin, schema, and library dir are relative to
    `sys.prefix`, which works nicely for Snapcraft as a deb or in a venv.
    However, Snapcraft's Python plugin installs packages in `$SNAP/` as a
    prefix, while Python itself is contained in `$SNAP/usr/`. As a result,
    using `sys.prefix` (which is `/usr`) to find these files when Snapcraft
    is a snap ends up looking in the wrong place.
    
    Treat running Snapcraft as a snap as a special case, similar to how
    running from git is supported, and set the directories appropriately.
    
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- snapcraft/__init__.py
+++ snapcraft/__init__.py
@@ -456,8 +456,3 @@ def str_presenter(dumper, data):
 yaml.add_representer(str, str_presenter)
 yaml.add_representer(OrderedDict, dict_representer)
 yaml.add_constructor(_mapping_tag, dict_constructor)
-
-from snapcraft.internal import common as _common # noqa
-if _common.is_snap():
-    snap = _os.environ.get('SNAP')
-    _common.set_schemadir(_os.path.join(snap, 'share', 'snapcraft', 'schema'))
