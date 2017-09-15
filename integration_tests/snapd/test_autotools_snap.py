efa8927e81385e132d10946d850df03f81a3c970
Leo Arias <yo@elopio.net>
2017-09-14 22:47:04 -0600

    tests: replace the first batch of demo tests with snapd integration tests
    revert

--- integration_tests/snapd/test_autotools_snap.py
+++ integration_tests/snapd/test_autotools_snap.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2015-2016 Canonical Ltd
+# Copyright (C) 2017 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -14,19 +14,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import snaps_tests
+import subprocess
+
+from testtools.matchers import Equals
+
+import integration_tests
+from snapcraft.tests import fixture_setup
 
 
-class LibPipelineTestCase(snaps_tests.SnapsTestCase):
+class AutotoolsTestCase(integration_tests.SnapdIntegrationTestCase):
 
-    snap_content_dir = 'libpipeline'
-
-    def test_libpipeline(self):
-        snap_path = self.build_snap(self.snap_content_dir)
-        self.install_snap(snap_path, 'pipelinetest', '1.0')
-        expected = (
-            'running echo test | grep s | grep t\n'
-            'custom libpipeline called\n'
-            'test\n')
-        self.assert_command_in_snappy_testbed(
-            '/snap/bin/pipelinetest', expected)
+    def test_install_and_execution(self):
+        with fixture_setup.WithoutSnapInstalled('autotools-hello'):
+            self.run_snapcraft(project_dir='autotools-hello')
+            self.install_snap()
+            self.assertThat(
+                subprocess.check_output(
+                    ['autotools-hello'], universal_newlines=True),
+                Equals("Hello world\n"))
