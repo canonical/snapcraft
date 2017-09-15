0c0f753dbfc73c098b71c412f4868637903293ce
Leo Arias <yo@elopio.net>
2017-09-14 22:47:04 -0600

    tests: replace the first batch of demo tests with snapd integration tests
    revert

--- integration_tests/snapd/test_python_snap.py
+++ integration_tests/snapd/test_python_snap.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2015-2016 Canonical Ltd
+# Copyright (C) 2017 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -14,15 +14,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import snaps_tests
+import subprocess
+
+from testtools.matchers import Equals
+
+import integration_tests
+from snapcraft.tests import fixture_setup
 
 
-class WebchatTestCase(snaps_tests.SnapsTestCase):
+class PythonTestCase(integration_tests.SnapdIntegrationTestCase):
 
-    snap_content_dir = 'webchat'
-
-    def test_webchat(self):
-        snap_path = self.build_snap(self.snap_content_dir)
-        snap_name = 'webchat'
-        self.install_snap(snap_path, snap_name, '0.0.1')
-        self.assert_service_running(snap_name, 'webchat')
+    def test_install_and_execution(self):
+        with fixture_setup.WithoutSnapInstalled('python-hello'):
+            self.run_snapcraft(project_dir='python-hello')
+            self.install_snap()
+            self.assertThat(
+                subprocess.check_output(
+                    ['python-hello'], universal_newlines=True),
+                Equals('Hello world!\n'))
