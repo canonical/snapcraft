ca30a3db31039c8a416ddcb916dd8aaa62cbeebb
Sergio Schvezov <sergio.schvezov@canonical.com>
2017-09-20 12:17:14 -0300

    added docstrings

--- snapcraft/storeapi/assertions.py
+++ snapcraft/storeapi/assertions.py
@@ -24,15 +24,33 @@ from . import constants
 
 
 class _BaseAssertion:
+    """Private Base class to handle assertions.
+
+    Implementations are supposed to define a class attribute to determine
+    the assertion type.
+
+    :cvar str _assertion_type: the assertion type, also treated as the endpoint
+                               for the assertion on the store and the payload
+                               header key for the returned data.
+    """
 
     @property
     def publisher_id(self):
+        """Return the publisher-id of a snap.
+        This entry is also known as accound-id or developer-id.
+
+        The value is lazily fetched from the store.
+        """
         if not self._account_info:
             self._account_info = self._store_client.get_account_information()
         return self._account_info['account_id']
 
     @property
     def snap_id(self):
+        """Return the snap-id of the given snap_name.
+
+        The value is lazily fetched from the store.
+        """
         if not self._account_info:
             self._account_info = self._store_client.get_account_information()
         snaps = self._account_info['snaps'][self._release]
@@ -41,7 +59,15 @@ class _BaseAssertion:
         except KeyError:
             raise errors.SnapNotFoundError(self._snap_name)
 
-    def __init__(self, *, snap_name, signing_key):
+    def __init__(self, *, snap_name, signing_key=None):
+        """Create an instance to handle an assertion.
+
+        :param str snap_name: snap name to handle assertion for.
+        :param str signing_key: the name of the key to use, if not
+                                provided, the default key is used.
+        :ivar dict assertion: holds the actual assertion.
+        :ivar dict signed_assertion: holds a signed version of assertion.
+        """
         self._store_client = StoreClient()
         self._snap_name = snap_name
         self._signing_key = signing_key
@@ -51,6 +77,11 @@ class _BaseAssertion:
         self.signed_assertion = None
 
     def get(self):
+        """Get the current assertion from the store.
+
+        :returns: the assertion corresponding to the snap_name.
+        :rtype: dict
+        """
         # The store adds a header key which is not consistent with the endpoint
         # which we need to pop as it is not understood by snap sign.
         if self.assertion:
@@ -61,6 +92,10 @@ class _BaseAssertion:
         return self.assertion
 
     def sign(self):
+        """Create a signed version of the obtained assertion.
+
+        :returns: signed assertion document.
+        """
         cmdline = ['snap', 'sign']
         if self._signing_key:
             cmdline += ['-k', self._signing_key]
@@ -79,7 +114,10 @@ class _BaseAssertion:
         return assertion
 
     def push(self, force=False):
-        """Push an assertion to the store, signing if necessary."""
+        """Push the assertion to the store, signing if necessary.
+
+        :returns: None
+        """
         if not self.signed_assertion:
             self.sign()
         self._store_client.push_assertion(
@@ -88,11 +126,42 @@ class _BaseAssertion:
 
 
 class DeveloperAssertion(_BaseAssertion):
+    """Implementation of a developer assertion.
+    The assertion is used to enable collaboration for a given snap_name by
+    means of operating agains the snap-id's developer assertion store endpoint.
+
+    The assertion content has the following structure
+    {
+        'type': 'snap-developer',
+        'authority-id': '<account_id of the publisher>',
+        'publisher-id': '<account_id of the publisher>',
+        'snap-id': '<snap-id>',
+        'developers': [{
+            'developer-id': '<account-id>',
+            'since': '2017-02-10T08:35:00.390258Z'
+           },{
+            'developer-id': '<account-id>',
+            'since': '2017-02-10T08:35:00.390258Z',
+            'until': '2018-02-10T08:35:00.390258Z'
+           }],
+        }
+
+    """
 
     _assertion_type = 'developers'
 
     def new_assertion(self, *, developers):
         """Create a new assertion with developers based out of the current one.
+
+        The new assertion has its assertion's authority-id normalized to the
+        assertion's publisher_id and the assertion's revision increaded by 1.
+
+        :param list developers: a list of a dictionary developers holding the
+                                keys: developer_id (mandatory), since
+                                (mandatory) and until (optional).
+        :returns: a new assertion based out of the current assertion with the
+                  provided developers list.
+        :rtype: DeveloperAssertion
         """
         new_assertion = deepcopy(self.assertion)
         new_assertion['developers'] = developers
@@ -138,6 +207,9 @@ class DeveloperAssertion(_BaseAssertion):
         }
 
         The assertion is saved without the snap_developer header.
+
+        :returns: the latest developer assertion corresponding to snap_name.
+        :rtype: dict
         """
         try:
             self.assertion = super().get()
@@ -157,12 +229,24 @@ class DeveloperAssertion(_BaseAssertion):
         return self.assertion
 
     def get_developers(self):
-        """Return a copy of the current developers listed in the assertion."""
+        """Return a copy of the current developers listed in the assertion.
+
+        :returns: a list of developers.
+        :rtype: list
+        """
         return self.get()['developers'].copy()
 
     def is_equal(self, other_assertion):
-        # We need to compare without milliseconds, so drop them from the
-        # assertion.
+        """Determines equality of developer lists in each assertion.
+
+        Returns True if the list of developers in this instances assertion list
+        is equal to the list from other_assertion.
+
+        During the comparison, differences in milliseconds are not considered.
+
+        :returns: True if equal, else False.
+        :rtype: bool
+        """
         this_devs = self._normalize_time(
             self.assertion['developers'].copy())
         other_devs = self._normalize_time(
