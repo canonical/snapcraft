50fa5fc151339d5d85e120d48f69efc1f26da320
Christian <christian.dywan@canonical.com>
2017-10-17 20:47:03 +0200

    lxd: split container classes into different files

---
+++ snapcraft/internal/lxd/__init__.py
@@ -0,0 +1,21 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2017 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ._containerbuild import Containerbuild            # noqa
+from ._cleanbuilder import Cleanbuilder                # noqa
+from ._project import Project                          # noqa
+
+from ._containerbuild import _remote_is_valid          # noqa
