79592d6afb9d753fc0a78740a33b834c006640ed
Leo Arias <leo.arias@canonical.com>
2017-10-27 05:40:44 +0000

    lxd: fix the push in container builds

--- snapcraft/internal/lxd/_containerbuild.py
+++ snapcraft/internal/lxd/_containerbuild.py
@@ -256,6 +256,10 @@ class Containerbuild:
                            is_dangerous=rev.startswith('x'),
                            is_classic=is_classic)
 
+    def _push_file(self, src, dst):
+        subprocess.check_call(['lxc', 'file', 'push',
+                              src, '{}{}'.format(self._container_name, dst)])
+
     def _install_snap(self, name, channel=None,
                       is_dangerous=False,
                       is_classic=False):
