9c4e4cd1d39257a9b10ab178d6c8eac35efa8e99
Daniel Llewellyn <daniel@bowlhat.net>
2017-11-01 12:41:13 +0000

    Add stage/usr/local/include to CMAKE_INCLUDE_PATH
    Similar to previous commit adding `stage/usr/local/lib` to CMAKE_LIBRARY_PATH
    
    - Add `stage/usr/local/include` and `stage/usr/local/include/$ARCH_TRIPLET` to `CMAKE_INCLUDE_PATH`

--- snapcraft/plugins/cmake.py
+++ snapcraft/plugins/cmake.py
@@ -90,7 +90,8 @@ class CMakePlugin(snapcraft.plugins.make.MakePlugin):
             self.project.stage_dir)
         env['CMAKE_INCLUDE_PATH'] = '$CMAKE_INCLUDE_PATH:' + ':'.join(
             ['{0}/include', '{0}/usr/include', '{0}/include/{1}',
-             '{0}/usr/include/{1}']).format(
+             '{0}/usr/include/{1}', '{0}/usr/local/include',
+             '{0}/usr/local/include/{1}']).format(
                  self.project.stage_dir, self.project.arch_triplet)
         env['CMAKE_LIBRARY_PATH'] = '$CMAKE_LIBRARY_PATH:' + ':'.join(
             ['{0}/lib', '{0}/usr/lib', '{0}/lib/{1}',
