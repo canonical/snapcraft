57689a753b9f4c736f18ee37a7af0dda6c86277f
Leo Arias <leo.arias@canonical.com>
2017-11-22 05:28:05 +0000

    move the workaround comment and duplicate it

--- tools/travis/run_autopkgtests.sh
+++ tools/travis/run_autopkgtests.sh
@@ -36,12 +36,14 @@ project_path="$(readlink -f "$script_path/../..")"
 "$script_path/setup_lxd.sh"
 "$script_path/run_lxd_container.sh" test-runner
 $lxc file push --recursive $project_path test-runner/root/
-# Workaround for
-# - Setup snap "core" (2462) security profiles (cannot reload udev rules: exit status 2
 $lxc exec test-runner -- sh -c "apt remove --yes lxd lxd-client"
+# Ignore the core install error as a workaround for
+# - Setup snap "core" (2462) security profiles (cannot reload udev rules: exit status 2
 $lxc exec test-runner -- sh -c "snap install core" || echo "ignored error"
 $lxc exec test-runner -- sh -c "cd snapcraft && ./tools/travis/setup_lxd.sh"
 $lxc exec test-runner -- sh -c "apt install --yes autopkgtest"
+# Ignore the core install error as a workaround for
+# - Setup snap "core" (2462) security profiles (cannot reload udev rules: exit status 2
 $lxc exec test-runner -- sh -c "cd snapcraft && adt-run --testname $test snapcraft --setup-commands $script_path/setup_autopkgtests_ppa.sh -U --setup-commands \"apt install squashfuse && (snap install core || echo 'ignored error')\" --- lxd $distro
 
 $lxc stop test-runner
