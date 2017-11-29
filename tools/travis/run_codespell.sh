e2198452076fe128c43d6512ac08b6b032cd6629
daniellim2000 <daniellimweesoong2000@gmail.com>
2017-11-29 22:26:12 +0800

    Remove -w from codespell

--- tools/travis/run_codespell.sh
+++ tools/travis/run_codespell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 sudo pip install codespell
-codespell -S "*.xz,*.zip,*.bz2,*.7z,*.gz,*.deb,*.rpm,*.snap,*.gpg,*.pyc,*.png,*.ico,*.jar,./.git" -w
+codespell -S "*.xz,*.zip,*.bz2,*.7z,*.gz,*.deb,*.rpm,*.snap,*.gpg,*.pyc,*.png,*.ico,*.jar,./.git"
