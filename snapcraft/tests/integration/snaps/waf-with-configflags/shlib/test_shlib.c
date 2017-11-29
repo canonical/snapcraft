4eafb986d7e61548dad67251e241a971275a9b88
daniellim2000 <daniellimweesoong2000@gmail.com>
2017-11-29 22:42:46 +0800

    Add -q4 to codespell

--- snapcraft/tests/integration/snaps/waf-with-configflags/shlib/test_shlib.c
+++ snapcraft/tests/integration/snaps/waf-with-configflags/shlib/test_shlib.c
@@ -2,4 +2,4 @@
 static const int truc=5;
 
 void foo() { }
-void _internal() {} /* this one is not gonna be exported by *.def */
+void _internal() {} /* this one is not going to be exported by *.def */
