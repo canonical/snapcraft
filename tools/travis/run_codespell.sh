547c24f27361c3826100aa8f2599a57edfba0190
daniellim2000 <daniellimweesoong2000@gmail.com>
2017-11-29 22:48:55 +0800

    Exclude changelog from codespell

--- tools/travis/run_codespell.sh
+++ tools/travis/run_codespell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 sudo pip install codespell
-codespell -S "*.xz,*.zip,*.bz2,*.7z,*.gz,*.deb,*.rpm,*.snap,*.gpg,*.pyc,*.png,*.ico,*.jar,./.git" -q4
+codespell -S "*.xz,*.zip,*.bz2,*.7z,*.gz,*.deb,*.rpm,*.snap,*.gpg,*.pyc,*.png,*.ico,*.jar,./.git,changelog" -q4
