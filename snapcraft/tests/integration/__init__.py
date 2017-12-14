1dc40feb4c13543ae54f385c9d9bb88168c064f8
Sergio Schvezov <sergio.schvezov@canonical.com>
2017-12-13 21:15:47 -0300

    round 2

--- snapcraft/tests/integration/__init__.py
+++ snapcraft/tests/integration/__init__.py
@@ -140,6 +140,7 @@ class TestCase(testtools.TestCase):
                 stderr=subprocess.STDOUT, universal_newlines=True,
                 env=env)
         except subprocess.CalledProcessError as e:
+            self.addDetail('command', content.text(self.snapcraft_command))
             self.addDetail('output', content.text_content(e.output))
             raise
 
