c053321a2076f6c2c47a9709262aece3337ed6b0
Leo Arias <leo.arias@canonical.com>
2017-12-15 00:08:09 +0000

    inherit from SnapcraftError

--- snapcraft/internal/meta/_errors.py
+++ snapcraft/internal/meta/_errors.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from snapcraft.internal import errors
 
-class CommandError(Exception):
+
+class CommandError(errors.SnapcraftError):
     pass
