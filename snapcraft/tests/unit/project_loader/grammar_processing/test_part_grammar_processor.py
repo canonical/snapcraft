e55dd19a53251bb5f26bfe63cb7d5878208a4868
Christian <christian.dywan@canonical.com>
2017-12-20 13:56:36 +0100

    Unit tests for strings in part grammar

--- snapcraft/tests/unit/project_loader/grammar_processing/test_part_grammar_processor.py
+++ snapcraft/tests/unit/project_loader/grammar_processing/test_part_grammar_processor.py
@@ -14,13 +14,58 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import snapcraft
+from snapcraft.tests import unit
+from testtools.matchers import Equals
+
 from snapcraft.internal.project_loader.grammar_processing import (
+    PartGrammarProcessor,
     _part_grammar_processor as processor
 )
 
 import doctest
+from unittest import mock
 
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(processor))
     return tests
+
+
+class PartGrammarTestCase(unit.TestCase):
+
+    scenarios = [
+        ('plain string', {
+            'properties': {'source': 'foo'},
+            'target_arch': 'amd64',
+            'expected': {'source': 'foo'}
+        }),
+        ('on amd64', {
+            'properties': {'source': [{'on amd64': 'foo'}]},
+            'target_arch': 'amd64',
+            'expected': {'source': 'foo'}
+        }),
+        ('on i386', {
+            'properties': {'source': [{'on i386': 'foo'}, {'else': 'bar'}]},
+            'target_arch': 'amd64',
+            'expected': {'source': 'bar'}
+        }),
+        ('on i386, target_arch=i386', {
+            'properties': {'source': [{'on i386': 'foo'}, {'else': 'bar'}]},
+            'target_arch': 'i386',
+            'expected': {'source': 'foo'}
+        }),
+    ]
+
+    def test_string_grammar(self):
+        repo = mock.Mock()
+        repo.is_valid.return_value = True
+        plugin = mock.Mock()
+        plugin.properties = self.properties
+        self.assertThat(PartGrammarProcessor(
+            plugin=plugin,
+            properties=plugin.properties,
+            project_options=snapcraft.ProjectOptions(
+                target_deb_arch=self.target_arch),
+            repo=repo).get_properties(),
+                        Equals(self.expected))
