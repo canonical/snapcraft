50726643a769fcc4fd07f4a74678e4611a3a9121
Sergio Schvezov <sergio.schvezov@canonical.com>
2017-12-21 17:02:18 -0300

    elf: search for DT_NEEDED in prime and the base. (#1823)
    Closes: #1664
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- snapcraft/internal/pluginhandler/__init__.py
+++ snapcraft/internal/pluginhandler/__init__.py
@@ -443,8 +443,13 @@ class PluginHandler:
 
         elf_files = elf.get_elf_files(self.primedir, snap_files)
         all_dependencies = set()
+        # TODO: base snap support
+        core_path = common.get_core_path()
+
         for elf_file in elf_files:
-            all_dependencies.update(elf_file.load_dependencies())
+            all_dependencies.update(
+                elf_file.load_dependencies(root_path=self.primedir,
+                                           core_base_path=core_path))
 
         # Split the necessary dependencies into their corresponding location.
         # We'll both migrate and track the system dependencies, but we'll only
@@ -470,36 +475,10 @@ class PluginHandler:
                 _migrate_files(system, system_dependency_paths, '/',
                                self.primedir, follow_symlinks=True)
 
-        # TODO: base snap support
-        core_path = common.get_core_path()
-
-        def mangle_library_path(library_path: str, elf_file_path: str) -> str:
-            # If the path is is in the core snap, use the absolute path,
-            # if the path is primed, use $ORIGIN, and last if the dependency
-            # is not anywhere return an empty string.
-            #
-            # Once we move away from the system library grabbing logic
-            # we can move to a smarter library capturing mechanism.
-            library_path = library_path.replace(self.installdir, self.primedir)
-            if library_path.startswith(core_path):
-                return os.path.dirname(library_path)
-            elif (library_path.startswith(self.primedir) and
-                  os.path.exists(library_path)):
-                rel_library_path = os.path.relpath(library_path, elf_file_path)
-                rel_library_path_dir = os.path.dirname(rel_library_path)
-                # return the dirname, with the first .. replace with $ORIGIN
-                return rel_library_path_dir.replace('..', '$ORIGIN', 1)
-            else:
-                return ''
-
         if self._confinement == 'classic':
-            core_rpaths = common.get_library_paths(
-                core_path, self._project_options.arch_triplet,
-                existing_only=False)
             dynamic_linker = self._project_options.get_core_dynamic_linker()
             elf_patcher = elf.Patcher(dynamic_linker=dynamic_linker,
-                                      library_path_func=mangle_library_path,
-                                      base_rpaths=core_rpaths)
+                                      root_path=self.primedir)
             for elf_file in elf_files:
                 elf_patcher.patch(elf_file=elf_file)
 
