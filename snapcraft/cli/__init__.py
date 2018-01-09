1251a0316a268b6a7ab45609758b34353bade356
Christian <christian.dywan@canonical.com>
2018-01-09 17:23:36 +0100

    Import run so bin/snapcraft can work

--- snapcraft/cli/__init__.py
+++ snapcraft/cli/__init__.py
@@ -14,5 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import snapcraft.internal.dirs
+from ._runner import run       # noqa
 
 snapcraft.internal.dirs.setup_dirs()
