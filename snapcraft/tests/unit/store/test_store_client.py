54cf2bd9ef052344a2e2c660992f2dd97e1a5914
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2018-01-10 19:58:10 +0800

    tests: add snap not found tests (#1849)

--- snapcraft/tests/unit/store/test_store_client.py
+++ snapcraft/tests/unit/store/test_store_client.py
@@ -32,7 +32,8 @@ from snapcraft import (
     ProjectOptions,
 )
 from snapcraft.storeapi import (
-    errors
+    errors,
+    constants
 )
 from snapcraft.tests import (
     fixture_setup,
@@ -1506,3 +1507,40 @@ class PushBinaryMetadataTestCase(StoreTestCase):
             # force the update, even on conflicts!
             result = self.client.push_binary_metadata('basic', metadata, True)
         self.assertIsNone(result)
+
+
+class SnapNotFoundTestCase(StoreTestCase):
+
+    scenarios = (
+        ('push_metadata', dict(attribute='push_metadata')),
+        ('push_binary_metadata', dict(attribute='push_binary_metadata')),
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.fake_logger = fixtures.FakeLogger(level=logging.DEBUG)
+        self.useFixture(self.fake_logger)
+
+    def _setup_snap(self):
+        """Login, register and push a snap.
+
+        These are all the previous steps needed to push binary metadata.
+        """
+        self.client.login('dummy', 'test correct password')
+        self.client.register('basic')
+        path = os.path.join(
+            os.path.dirname(tests.__file__), 'data', 'test-snap.snap')
+        tracker = self.client.upload('basic', path)
+        tracker.track()
+
+    def test_snap_not_found(self):
+        self._setup_snap()
+        metadata = {'field_ok': 'dummy'}
+        raised = self.assertRaises(
+            errors.SnapNotFoundError,
+            getattr(self.client, self.attribute),
+            'test-unexistent-snap', metadata, False)
+        self.assertThat(str(raised),
+                        Equals("Snap 'test-unexistent-snap' was not "
+                               "found in '{}' series."
+                               .format(constants.DEFAULT_SERIES)))
