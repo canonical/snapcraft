65399dece0f3cddef5a9613f19cf7697c7928476
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-01-12 12:15:21 -0300

    tests: call file directly for the HasArchitecture checker
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- snapcraft/tests/matchers.py
+++ snapcraft/tests/matchers.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2017 Canonical Ltd
+# Copyright (C) 2017-2018 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -13,8 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import subprocess
 
-import magic
 import testtools
 
 
@@ -23,20 +23,29 @@ class HasArchitecture:
 
     def __init__(self, expected_arch):
         self._expected_arch = expected_arch
-        self._ms = magic.open(magic.NONE)
-        self._ms.load()
 
     def __str__(self):
         return 'HasArchitecture()'
 
     def match(self, file_path):
-        magic = self._ms.file(file_path)
-        # Catch exceptions on splitting the string to provide context
-        # This includes "cannot open `...' (No such file or directory)"
+        cmd = ['file', file_path]
         try:
-            arch = magic.split(',')[1]
+            output = subprocess.check_output(cmd).decode()
+        except subprocess.CalledProcessError as cmd_err:
+            raise ValueError(
+                'Failed to get architecture info: {}', cmd_err) from cmd_err
+
+        try:
+            # output is of the form
+            # ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV), \
+            # dynamically linked, interpreter /lib/ld-linux-aarch64.so.1, \
+            # for GNU/Linux 3.7.0, \
+            # BuildID[sha1]=58b8495bef7a6c9e5f403879e29dc63ad4ed2d11, \
+            # not stripped
+            arch = output.split(',')[1]
         except IndexError as e:
-            raise ValueError('Failed to parse magic {!r}'.format(magic)) from e
+            raise ValueError('Failed to parse magic {!r}'.format(
+                output)) from e
         if self._expected_arch not in arch:
             return testtools.matchers.Mismatch(
                 'Expected {!r} to be in {!r}'.format(
