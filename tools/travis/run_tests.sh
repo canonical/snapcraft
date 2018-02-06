dbde8e76e4c68460f489d9f845e82ffebe076240
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-02-05 16:05:46 -0800

    more deps

--- tools/travis/run_tests.sh
+++ tools/travis/run_tests.sh
@@ -32,7 +32,7 @@ pattern="$2"
 if [ "$test" = "static" ]; then
     dependencies="apt install -y python3-pip && python3 -m pip install -r requirements-devel.txt"
 elif [ "$test" = "snapcraft/tests/unit" ]; then
-    dependencies="apt install -y git bzr subversion mercurial rpm2cpio libnacl-dev libsodium-dev libffi-dev libapt-pkg-dev python3-pip squashfs-tools xdelta3 && python3 -m pip install -r requirements-devel.txt -r requirements.txt codecov && apt install -y python3-coverage"
+    dependencies="apt install -y git bzr subversion mercurial rpm2cpio p7zip-full libnacl-dev libsodium-dev libffi-dev libapt-pkg-dev python3-pip squashfs-tools xdelta3 && python3 -m pip install -r requirements-devel.txt -r requirements.txt codecov && apt install -y python3-coverage"
 elif [[ "$test" = "snapcraft/tests/integration"* || "$test" = "snapcraft.tests.integration"* ]]; then
     # TODO remove the need to install the snapcraft dependencies due to nesting
     #      the tests in the snapcraft package
