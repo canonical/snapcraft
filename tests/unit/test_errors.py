d879b2e3a98eda1cbbbd8d665c585d0975a5afbb
Christian Dywan <christian@twotoasts.de>
2018-04-25 16:32:23 +0200

    repo: catch error updating the package cache (#2079)
    Fixes: #2029
    Fixes: #2030
    LP: #1747715

--- tests/unit/test_errors.py
+++ tests/unit/test_errors.py
@@ -21,6 +21,7 @@ from testtools.matchers import Equals
 
 from snapcraft.internal import errors
 from snapcraft.internal.meta import _errors as meta_errors
+from snapcraft.internal.repo import errors as repo_errors
 from snapcraft.storeapi import errors as store_errors
 from tests import unit
 
@@ -429,6 +430,24 @@ class ErrorFormattingTestCase(unit.TestCase):
                 "Exited with code 2.\n"
                 "Verify that the part is using the correct parameters and try "
                 "again."
+            )
+        }),
+        ('CacheUpdateFailedError', {
+            'exception': repo_errors.CacheUpdateFailedError,
+            'kwargs': {'errors': ''},
+            'expected_message': (
+                "Failed to update the package cache: "
+                "Some files could not be downloaded: "
+                "Check that the sources on your host are configured correctly."
+            )
+        }),
+        ('CacheUpdateFailedError', {
+            'exception': repo_errors.CacheUpdateFailedError,
+            'kwargs': {'errors': 'foo, bar'},
+            'expected_message': (
+                "Failed to update the package cache: "
+                "Some files could not be downloaded:\n\nfoo\nbar\n\n"
+                "Check that the sources on your host are configured correctly."
             )
         }),
         ('StoreNetworkError generic error', {
