bfbd077f4ad409bc60ba34f6ac166103a9b917e4
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-05-04 15:34:12 +0200

    move out get_env and accomodate with using the file descriptor in popen

--- snapcraft/internal/pluginhandler/_runner.py
+++ snapcraft/internal/pluginhandler/_runner.py
@@ -116,20 +116,6 @@ class Runner:
                 'override-prime', self._override_prime_scriptlet,
                 self._primedir)
 
-    def _get_env(self):
-        env = ''
-        if common.is_snap():
-            # Since the snap is classic, $SNAP/bin is not on the $PATH.
-            # Let's set an alias to make sure it's found (but only if it
-            # exists).
-            snapcraftctl_path = os.path.join(
-                os.getenv('SNAP'), 'bin', 'snapcraftctl')
-            if os.path.exists(snapcraftctl_path):
-                env += 'alias snapcraftctl="$SNAP/bin/snapcraftctl"\n'
-        env += common.assemble_env()
-
-        return env
-
     def _run_scriptlet(self, scriptlet_name: str, scriptlet: str,
                        workdir: str) -> None:
         with tempfile.TemporaryDirectory() as tempdir:
@@ -142,23 +128,24 @@ class Runner:
             # interpreter as that used by snapcraft itself, thus the definition
             # of SNAPCRAFT_INTERPRETER.
             script = textwrap.dedent("""\
+                set -e
                 export SNAPCRAFTCTL_CALL_FIFO={call_fifo}
                 export SNAPCRAFTCTL_FEEDBACK_FIFO={feedback_fifo}
                 export SNAPCRAFT_INTERPRETER={interpreter}
+                {env}
                 {scriptlet}""").format(
                     interpreter=sys.executable, call_fifo=call_fifo.path,
-                    feedback_fifo=feedback_fifo.path, scriptlet=scriptlet)
+                    feedback_fifo=feedback_fifo.path, scriptlet=scriptlet,
+                    env=_get_env())
 
             # We write to a file as the script may be to long and hit the
             # argument list limit.
             run_script = os.path.join(tempdir, 'run_script')
             with open(run_script, 'w+') as script_file:
-                print('set -e', file=script_file)
-                print(self._get_env(), file=script_file)
                 print(script, file=script_file)
 
-            process = subprocess.Popen(['/bin/sh', script_file.name],
-                                       cwd=workdir)
+                process = subprocess.Popen(['/bin/sh'], stdin=script_file,
+                                           cwd=workdir)
 
             status = None
             try:
@@ -251,3 +238,18 @@ class _NonBlockingRWFifo:
     def close(self) -> None:
         if self._fd is not None:
             os.close(self._fd)
+
+
+def _get_env():
+    env = ''
+    if common.is_snap():
+        # Since the snap is classic, $SNAP/bin is not on the $PATH.
+        # Let's set an alias to make sure it's found (but only if it
+        # exists).
+        snapcraftctl_path = os.path.join(
+            os.getenv('SNAP'), 'bin', 'snapcraftctl')
+        if os.path.exists(snapcraftctl_path):
+            env += 'alias snapcraftctl="$SNAP/bin/snapcraftctl"\n'
+    env += common.assemble_env()
+
+    return env
