39a328dd404ea77d4cfdd6a2bf126d6ac9b18908
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-05-17 16:43:13 -0300

    Update test_plainbox_provider_plugin.py

--- tests/integration/plugins/test_plainbox_provider_plugin.py
+++ tests/integration/plugins/test_plainbox_provider_plugin.py
@@ -61,6 +61,6 @@ class PlainboxProviderPluginTestCase(integration.TestCase):
 
     def test_python_stage_pkg_mix(self):
         project_dir = 'plainbox-provider-python-stage-pkg-mix'
-        self.run_snapcraft('snap', project_dir)
+        self.run_snapcraft('stage', project_dir)
         # Tests bug where snap would fail to build with error:
         # "Failed building wheel for bitstring"
