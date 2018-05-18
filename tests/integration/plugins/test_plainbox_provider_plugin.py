9ac5d693e3c68aed71ac83327bb2d7056bdd23ae
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-05-18 10:28:09 -0300

    tests: generalize the plainbox-provider tests
    Allow for the same code to work on xenial and bionic.
    Also, only stage as priming is not necessary for this test.
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- tests/integration/plugins/test_plainbox_provider_plugin.py
+++ tests/integration/plugins/test_plainbox_provider_plugin.py
@@ -45,10 +45,8 @@ class PlainboxProviderPluginStageTestCase(testscenarios.WithScenarios,
 class PlainboxProviderPluginTestCase(integration.TestCase):
 
     def test_snap_provider_with_deps(self):
-        if self.deb_arch == 'armhf':
-            self.skipTest("No patchelf deb for armhf")
         project_dir = 'plainbox-provider-with-deps'
-        self.run_snapcraft('prime', project_dir)
+        self.run_snapcraft('stage', project_dir)
         # No assertion required as the project will fail to complete to the
         # prime step if validation fails
 
