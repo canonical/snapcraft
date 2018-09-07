cee8e5c802caa55577ed7e42df4fca0a971d0e20
Kyle Fazzari <kyrofa@ubuntu.com>
2018-09-06 20:01:26 -0700

    Fix integration tests
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- tests/integration/lifecycle/test_snap.py
+++ tests/integration/lifecycle/test_snap.py
@@ -41,6 +41,13 @@ class SnapTestCase(integration.TestCase):
             expected_binary1_wrapper = file_.read()
         self.assertThat(binary1_wrapper_path, FileContains(expected_binary1_wrapper))
 
+        command_chain_path = os.path.join(
+            self.prime_dir, "snap", "command-chain", "snapcraft-runner"
+        )
+        with open("command-chain", "r") as file_:
+            expected_command_chain = file_.read()
+        self.assertThat(command_chain_path, FileContains(expected_command_chain))
+
         self.useFixture(
             fixtures.EnvironmentVariable(
                 "SNAP", os.path.join(os.getcwd(), self.prime_dir)
@@ -54,7 +61,8 @@ class SnapTestCase(integration.TestCase):
         )
         for binary, expected_output in binary_scenarios:
             output = subprocess.check_output(
-                os.path.join(self.prime_dir, binary), universal_newlines=True
+                [command_chain_path, os.path.join(self.prime_dir, binary)],
+                universal_newlines=True,
             )
             self.assertThat(output, Equals(expected_output))
 
@@ -92,15 +100,15 @@ class SnapTestCase(integration.TestCase):
                 grade: stable
                 apps:
                   assemble-bin:
-                    command: command-assemble-bin.wrapper
+                    command: snap/command-chain/snapcraft-runner $SNAP/command-assemble-bin.wrapper
                   assemble-service:
-                    command: command-assemble-service.wrapper
+                    command: snap/command-chain/snapcraft-runner $SNAP/command-assemble-service.wrapper
                     daemon: simple
-                    stop-command: stop-command-assemble-service.wrapper
+                    stop-command: snap/command-chain/snapcraft-runner $SNAP/stop-command-assemble-service.wrapper
                   binary-wrapper-none:
                     command: subdir/binary3
                   binary2:
-                    command: command-binary2.wrapper
+                    command: snap/command-chain/snapcraft-runner $SNAP/command-binary2.wrapper
             """
                 ).format(self.deb_arch)
             ),
@@ -269,7 +277,7 @@ class SnapTestCase(integration.TestCase):
               stub_key: stub-value
             apps:
               stub-app:
-                command: command-stub-app.wrapper
+                command: snap/command-chain/snapcraft-runner $SNAP/command-stub-app.wrapper
         """
         )
         self.assertThat(
