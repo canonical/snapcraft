10b836b77de35fccf9f5c225e3176128eaaa325b
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-09-18 08:05:08 +0200

    build providers: re-exec as root
    When snapcraft is called from managed-host environment, it needs to re-exec itself as root.
    
    LP: #1791977
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- snapcraft/cli/_runner.py
+++ snapcraft/cli/_runner.py
@@ -64,6 +64,19 @@ command_groups = [
 def run(ctx, debug, catch_exceptions=False, **kwargs):
     """Snapcraft is a delightful packaging tool."""
 
+    # Do this early, if we are in managed-host and not root yet we want to re-exec with
+    # sudo keeping the current environment settings.
+    if os.getenv("SNAPCRAFT_BUILD_ENVIRONMENT") == "managed-host" and os.geteuid() != 0:
+        snap_path = os.getenv("SNAP")
+        # Use -E to keep the environment.
+        cmd = ["usr/bin/sudo", "-E"]
+        # Pick the correct interpreter if running from a snap (in managed-host mode, this
+        # is most likely the only scenario).
+        if os.getenv("SNAP_NAME") == "snapcraft":
+            cmd.append(os.path.join(snap_path, "usr", "bin", "python3"))
+        cmd.extend(sys.argv)
+        os.execve("/usr/bin/sudo",  cmd, os.environ)
+
     # Debugging snapcraft itself is not tied to debugging a snapcraft project.
     try:
         is_snapcraft_developer_debug = util.strtobool(
