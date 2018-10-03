5eb674f4d975e67cbe1dc3b52d34272014aab697
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-10-03 12:06:47 -0300

    tests: use mocked plugins for list-plugins (#2315)
    LP: #1795859
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- tests/unit/commands/test_list_plugins.py
+++ tests/unit/commands/test_list_plugins.py
@@ -13,6 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from unittest import mock
+
 from testtools.matchers import Equals, Contains
 
 from tests import fixture_setup
@@ -26,18 +28,53 @@ class ListPluginsCommandTestCase(CommandBaseTestCase):
         ("plugins alias", {"command_name": "plugins"}),
     ]
 
-    # plugin list when wrapper at MAX_CHARACTERS_WRAP
     default_plugin_output = (
-        "ament      catkin        copy    go      gulp     kbuild  maven  "
-        "nodejs             python2  ruby   tar-content\n"
-        "ant        catkin-tools  dotnet  godeps  jdk      kernel  meson  "
-        "plainbox-provider  python3  rust   waf        \n"
-        "autotools  cmake         dump    gradle  jhbuild  make    nil    "
-        "python             qmake    scons\n"
+        "fake      dotruby         kernelz  checkbox-provider  oxide  hornet      dumper  "
+        "jamesbuild  none   macaroon    gmake\nnot-real  godependencies  planton  "
+        "snake              web    fake-tools  cradle  maker       pmake  breaktools\n"
     )
 
-    def test_list_plugins_non_tty(self):
+    # fake plugin list, made up names so it is obvious when it fails that
+    # we are not using real data, but use names to have some sort of simulation
+    # with reality
+    plugin_list = [
+        "fake",
+        "not-real",
+        "dotruby",
+        "godependencies",
+        "kernelz",
+        "planton",
+        "checkbox-provider",
+        "snake",
+        "oxide",
+        "web",
+        "hornet",
+        "fake-tools",
+        "dumper",
+        "cradle",
+        "jamesbuild",
+        "maker",
+        "none",
+        "pmake",
+        "macaroon",
+        "breaktools",
+        "gmake",
+    ]
+
+    def setUp(self):
+        super().setUp()
+
         self.maxDiff = None
+
+        # Generate fake plugin data
+        iter_modules_result = (("", p, "") for p in self.plugin_list)
+
+        patcher = mock.patch("pkgutil.iter_modules")
+        modules_mock = patcher.start()
+        modules_mock.return_value = iter_modules_result
+        self.addCleanup(patcher.stop)
+
+    def test_list_plugins_non_tty(self):
         fake_terminal = fixture_setup.FakeTerminal(isatty=False)
         self.useFixture(fake_terminal)
 
@@ -47,7 +84,6 @@ class ListPluginsCommandTestCase(CommandBaseTestCase):
         self.assertThat(result.output, Contains(self.default_plugin_output))
 
     def test_list_plugins_large_terminal(self):
-        self.maxDiff = None
         fake_terminal = fixture_setup.FakeTerminal(columns=999)
         self.useFixture(fake_terminal)
 
@@ -57,22 +93,20 @@ class ListPluginsCommandTestCase(CommandBaseTestCase):
         self.assertThat(result.output, Equals(self.default_plugin_output))
 
     def test_list_plugins_small_terminal(self):
-        self.maxDiff = None
         fake_terminal = fixture_setup.FakeTerminal(columns=60)
         self.useFixture(fake_terminal)
 
-        expected_output = (
-            "ament         dump     kernel             python2    \n"
-            "ant           go       make               python3    \n"
-            "autotools     godeps   maven              qmake      \n"
-            "catkin        gradle   meson              ruby       \n"
-            "catkin-tools  gulp     nil                rust       \n"
-            "cmake         jdk      nodejs             scons      \n"
-            "copy          jhbuild  plainbox-provider  tar-content\n"
-            "dotnet        kbuild   python             waf        \n"
-        )
+        expected_output = [
+            "fake            checkbox-provider  dumper      macaroon",
+            "not-real        snake              cradle      breaktools",
+            "dotruby         oxide              jamesbuild  gmake",
+            "godependencies  web                maker",
+            "kernelz         hornet             none",
+            "planton         fake-tools         pmake",
+        ]
 
         result = self.run_command([self.command_name])
 
         self.assertThat(result.exit_code, Equals(0))
-        self.assertThat(result.output, Equals(expected_output))
+        output_slice = [o.strip() for o in result.output.splitlines()]
+        self.assertThat(output_slice, Equals(expected_output))
