039f0e0881e96daddfc1c349f8b8b6ef9d4a3b53
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-10-08 17:16:15 -0300

    fix tests

--- tests/spread/tools/restore.sh
+++ tests/spread/tools/restore.sh
@@ -4,8 +4,8 @@
 snaps="$(snap list | awk '{if (NR!=1) {print $1}}')"
 for snap in $snaps; do
 	case "$snap" in
-		"core" | "core18" | "snapcraft")
-			# Don't want to remove these
+		"core" | "core16" | "core18" | "snapcraft")
+			# Do not or cannot remove these
 			;;
 		*)
 			snap remove "$snap"
