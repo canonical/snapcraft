ca815233a3d7f43b11ae1d10c1aeb7a31121b5a1
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-10-08 22:51:51 -0300

    restoration and test fixes

--- tests/spread/tools/restore.sh
+++ tests/spread/tools/restore.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# Remove all auto-installed packages
+apt-get autoremove --purge -y
+
 # Remove all non-critical snaps
 snaps="$(snap list | awk '{if (NR!=1) {print $1}}')"
 for snap in $snaps; do
