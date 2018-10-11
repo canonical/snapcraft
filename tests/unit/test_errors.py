194989b4ef4a4231efa7bee641cf24717626c96d
Kyle Fazzari <github@status.e4ward.com>
2018-10-11 15:25:27 -0700

    schema, meta: support app command-chain (#2341)
    Add support for specifying command-chain. This is only available in
    snapd v2.36, but more features depend on this one, so we need to add
    support now.
    
    LP: #1790978
    
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- tests/unit/test_errors.py
+++ tests/unit/test_errors.py
@@ -267,12 +267,25 @@ class ErrorFormattingTestCase(unit.TestCase):
             "InvalidAppCommandError",
             {
                 "exception": errors.InvalidAppCommandError,
-                "kwargs": {"command": "test-command", "app": "test-app"},
+                "kwargs": {"command": "test-command", "app_name": "test-app"},
                 "expected_message": (
                     "Failed to generate snap metadata: "
                     "The specified command 'test-command' defined in the app "
                     "'test-app' does not exist or is not executable.\n"
                     "Ensure that 'test-command' is relative to the prime directory."
+                ),
+            },
+        ),
+        (
+            "InvalidCommandChainError",
+            {
+                "exception": errors.InvalidCommandChainError,
+                "kwargs": {"item": "test-chain", "app_name": "test-app"},
+                "expected_message": (
+                    "Failed to generate snap metadata: "
+                    "The command-chain item 'test-chain' defined in the app 'test-app' "
+                    "does not exist or is not executable.\n"
+                    "Ensure that 'test-chain' is relative to the prime directory."
                 ),
             },
         ),
