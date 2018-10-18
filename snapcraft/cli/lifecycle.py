e375bcbf442d389d8f32bd3658ddce8db40f4d3f
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-10-18 17:23:06 -0300

    fix import ordering and separation

--- snapcraft/cli/lifecycle.py
+++ snapcraft/cli/lifecycle.py
@@ -14,8 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import typing
 import sys
+import typing
+
 import click
 
 from . import echo
