ede92930d63ffe06434bfafb5768a07778ee481e
Sergio Schvezov <sergio.schvezov@canonical.com>
2018-10-24 17:40:03 -0600

    re-do re-exec

--- snapcraft/cli/__main__.py
+++ snapcraft/cli/__main__.py
@@ -19,10 +19,13 @@ import codecs
 import locale
 import os
 import subprocess
+import sys
 
-from ._runner import run
+from ._runner import run as run_snapcraft
 from .snapcraftctl._runner import run as run_snapcraftctl  # noqa
 from .echo import warning
+from snapcraft import project, yaml_utils
+from snapcraft.internal import common
 
 # If the locale ends up being ascii, Click will barf. Let's try to prevent that
 # here by using C.UTF-8 as a last-resort fallback. This mostly happens in CI,
@@ -38,5 +41,46 @@ if codecs.lookup(locale.getpreferredencoding()).name == "ascii" and os.name == "
             os.environ["LANG"] = "C.UTF-8"
             break
 
+
+def _needs_legacy() -> bool:
+    if not os.path.isdir(common.get_legacy_snapcraft_dir()):
+        return False
+
+    try:
+        # Early bootstrapping does not allow us to use the existing utilities we
+        # have to manage this check.
+        if os.getenv("SNAPCRAFT_BUILD_ENVIRONMENT") == "managed-host":
+            base_dir = os.path.expanduser(os.path.join("~", "project"))
+        else:
+            base_dir = None
+        snapcraft_yaml_path = project.get_snapcraft_yaml(base_dir=base_dir)
+        with open(snapcraft_yaml_path, "r") as f:
+            data = yaml_utils.load(f)
+        print(data.get("base"), data.get("base") is None)
+        return data.get("base") is None
+    except Exception:
+        # If there are issues loading/parsing the YAML, just pass off to the current version
+        # where the error should be properly handled
+        return False
+
+
+def run_legacy_snapcraft() -> None:
+    legacy_python = os.path.join(
+        common.get_legacy_snapcraft_dir(), "usr", "bin", "python3"
+    )
+    legacy_snapcraft = os.path.join(
+        common.get_legacy_snapcraft_dir(), "bin", "snapcraft"
+    )
+
+    os.execv(legacy_python, [legacy_python, legacy_snapcraft] + sys.argv[1:])
+
+
+def run():
+    if _needs_legacy():
+        run_legacy_snapcraft()
+    else:
+        run_snapcraft(prog_name="snapcraft")
+
+
 if __name__ == "__main__":
-    run(prog_name="snapcraft")
+    run()
--- snapcraft/cli/_runner.py
+++ snapcraft/cli/_runner.py
@@ -22,8 +22,7 @@ from distutils import util
 import click
 
 import snapcraft
-from snapcraft import project, yaml_utils
-from snapcraft.internal import common, log
+from snapcraft.internal import log
 from .assertions import assertionscli
 from .containers import containerscli
 from .discovery import discoverycli
@@ -53,38 +52,6 @@ command_groups = [
 ]
 
 
-def _is_legacy_reexec() -> bool:
-    if not os.path.isdir(common.get_legacy_snapcraft_dir()):
-        return False
-
-    try:
-        # Early bootstrapping does not allow us to use the existing utilities we
-        # have to manage this check.
-        if os.getenv("SNAPCRAFT_BUILD_ENVIRONMENT") == "managed-host":
-            base_dir = os.path.expanduser(os.path.join("~", "project"))
-        else:
-            base_dir = None
-        snapcraft_yaml_path = project.get_snapcraft_yaml(base_dir=base_dir)
-        with open(snapcraft_yaml_path, "r") as f:
-            data = yaml_utils.load(f)
-        return data.get("base") is None
-    except Exception:
-        # If there are issues loading/parsing the YAML, just pass off to the current version
-        # where the error should be properly handled
-        return False
-
-
-def _legacy_reexec() -> None:
-    legacy_python = os.path.join(
-        common.get_legacy_snapcraft_dir(), "usr", "bin", "python3"
-    )
-    legacy_snapcraft = os.path.join(
-        common.get_legacy_snapcraft_dir(), "bin", "snapcraft"
-    )
-
-    os.execv(legacy_python, [legacy_python, legacy_snapcraft] + sys.argv[1:])
-
-
 @click.group(cls=SnapcraftGroup, invoke_without_command=True)
 @click.version_option(
     message=SNAPCRAFT_VERSION_TEMPLATE, version=snapcraft.__version__  # type: ignore
@@ -94,9 +61,6 @@ def _legacy_reexec() -> None:
 @click.option("--debug", "-d", is_flag=True)
 def run(ctx, debug, catch_exceptions=False, **kwargs):
     """Snapcraft is a delightful packaging tool."""
-
-    if _is_legacy_reexec():
-        _legacy_reexec()
 
     # Debugging snapcraft itself is not tied to debugging a snapcraft project.
     try:
