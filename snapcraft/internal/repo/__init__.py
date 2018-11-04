20e54e1c6a44ec28d8a15a70100028275b65f70d
Anatoli Babenia <anatoli@rainforce.org>
2018-11-04 10:33:42 +0300

    repo: document package purpose

--- snapcraft/internal/repo/__init__.py
+++ snapcraft/internal/repo/__init__.py
@@ -13,6 +13,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Operations with system-specific package repositories."""
+
 import contextlib
 import shutil
 
