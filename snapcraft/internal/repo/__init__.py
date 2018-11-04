38b16ab9464f6266481edf7110e1ffdd625f206f
Anatoli Babenia <anatoli@rainforce.org>
2018-11-04 10:45:16 +0300

    repo: platform is a better word

--- snapcraft/internal/repo/__init__.py
+++ snapcraft/internal/repo/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Operations with system-specific package repositories."""
+"""Operations with platform specific package repositories."""
 
 import contextlib
 import shutil
