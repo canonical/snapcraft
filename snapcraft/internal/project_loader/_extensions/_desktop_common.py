601d71ff5b2c3fba850f21ff86fa837e57367441
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-07 22:48:59 +0000

    add shared snap mountpoints and update launcher
    * ensure the shared data snap mountpoints exist
    * update launcher scripts with fixes to ensure gsettings schemas are
    correctly compiled.

--- snapcraft/internal/project_loader/_extensions/_desktop_common.py
+++ snapcraft/internal/project_loader/_extensions/_desktop_common.py
@@ -34,13 +34,11 @@ class DesktopCommonExtension(Extension):
         super().__init__(yaml_data)
 
         self.ld_library_paths = [
-            "$SNAP/usr/lib/$SNAP_LAUNCHER_ARCH_TRIPLET/lapack",
-            "$SNAP/usr/lib/$SNAP_LAUNCHER_ARCH_TRIPLET/blas",
-
+            "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/lapack",
+            "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/blas",
+            "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pulseaudio",
             "$LD_LIBRARY_PATH",
-
             "/var/lib/snapd/lib/gl",
-
             # Testability support
             "$SNAP/testability",
             "$SNAP/testability/$SNAPCRAFT_ARCH_TRIPLET",
@@ -49,18 +47,32 @@ class DesktopCommonExtension(Extension):
 
         self.path = ["$PATH"]
 
+        self.plugs = {
+            "gtk-3-themes": {
+                "interface": "content",
+                "target": "$SNAP/data-dir/themes",
+                "default-provider": "gtk-common-themes:gtk-3-themes",
+            },
+            "icon-themes": {
+                "interface": "content",
+                "target": "$SNAP/data-dir/icons",
+                "default-provider": "gtk-common-themes:icon-themes",
+            },
+            "sound-themes": {
+                "interface": "content",
+                "target": "$SNAP/data-dir/sounds",
+                "default-provider": "gtk-common-themes:sound-themes",
+            },
+        }
+
         self.xdg_cache_home = "$SNAP_USER_COMMON/.cache"
         self.xdg_config_home = "$SNAP_USER_DATA/.config"
         self.xdg_data_home = "$SNAP_USER_DATA/.local/share"
-        self.xdg_config_dirs = [
-            "$SNAP/etc/xdg",
-            "$XDG_CONFIG_DIRS",
-        ]
+        self.xdg_config_dirs = ["$SNAP/etc/xdg", "$XDG_CONFIG_DIRS"]
         self.xdg_data_dirs = [
             # Workaround for GLib < 2.53.2 not searching for schemas in $XDG_DATA_HOME:
             #   https://bugzilla.gnome.org/show_bug.cgi?id=741335
             self.xdg_data_home,
-            
             "$SNAP_USER_DATA",
             "$SNAP/data-dir",
             "$SNAP/share",
@@ -71,21 +83,14 @@ class DesktopCommonExtension(Extension):
         self.environment = {
             "FONTCONFIG_FILE": "$SNAP/gnome-platform/etc/fonts/fonts.conf",
             "FONTCONFIG_PATH": "$SNAP/gnome-platform/etc/fonts",
-
             "GST_PLUGIN_PATH": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer-1.0",
             "GST_PLUGIN_SCANNER": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner",
             "GST_PLUGIN_SYSTEM_PATH": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer-1.0",
-
             "IBUS_CONFIG_PATH": "{}/ibus".format(self.xdg_config_home),
-            
             "LD_LIBRARY_PATH": ":".join(self.ld_library_paths),
-            
             "LIBGL_DRIVERS_PATH": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/dri",
-            
             "PATH": ":".join(self.path),
-            
             "XCURSOR_PATH": "$SNAP/data-dir/icons",
-
             "XDG_CACHE_HOME": self.xdg_cache_home,
             "XDG_CONFIG_DIRS": ":".join(self.xdg_config_dirs),
             "XDG_CONFIG_HOME": self.xdg_config_home,
@@ -96,6 +101,7 @@ class DesktopCommonExtension(Extension):
         self.root_snippet = {
             "assumes": ["command-chain"],
             "environment": self.environment,
+            "plugs": self.plugs,
         }
 
         self.command_chain = [
@@ -103,9 +109,7 @@ class DesktopCommonExtension(Extension):
             "snap/command-chain/desktop-mark-and-exec",
         ]
 
-        self.app_snippet = {
-            "command-chain": self.command_chain,
-        }
+        self.app_snippet = {"command-chain": self.command_chain}
 
         self.parts = {
             "desktop-common-extension": {
