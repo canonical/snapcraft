54215e3051ac76dea3516946c4e6c4c511bcbf00
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-07 22:48:59 +0000

    add shared snap mountpoints and update launcher
    * ensure the shared data snap mountpoints exist
    * update launcher scripts with fixes to ensure gsettings schemas are
    correctly compiled.

--- snapcraft/internal/project_loader/_extensions/gnome.py
+++ snapcraft/internal/project_loader/_extensions/gnome.py
@@ -44,16 +44,14 @@ class GnomeExtension(DesktopCommonExtension):
         if yaml_data.get("confinement") == "classic":
             raise GnomeExtensionClassicConfinementError()
 
-        platform_snap = "gnome-3-26-1604" # default
+        platform_snap = "gnome-3-26-1604"  # default
         base = yaml_data.get("base")
         after_dependencies = []
         dependency_part = {}
         if base is not None:
             if base == "core16":
                 platform_snap = "gnome-3-26-1604"
-                after_dependencies = [
-                    "gnome-extension-platform-dependencies",
-                ]
+                after_dependencies = ["gnome-extension-platform-dependencies"]
                 dependency_part = {
                     "gnome-extension-platform-dependencies": {
                         "plugin": None,
@@ -61,7 +59,7 @@ class GnomeExtension(DesktopCommonExtension):
                             add-apt-repository ppa:
                             apt-get update
                             apt-get upgrade -yqq
-                            """
+                            """,
                     }
                 }
             elif base == "core18":
@@ -86,70 +84,46 @@ class GnomeExtension(DesktopCommonExtension):
             "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pulseaudio",
         ] + self.ld_library_paths
 
-        path = self.path + [
-            "$SNAP/gnome-platform/usr/bin",
-        ]
+        path = self.path + ["$SNAP/gnome-platform/usr/bin"]
 
         xdg_config_dirs = ["$SNAP/gnome-platform/etc/xdg"] + self.xdg_config_dirs
         xdg_data_dirs = ["$SNAP/gnome-platform/usr/share"] + self.xdg_data_dirs
 
         self.root_snippet = {
             **self.root_snippet,
-
             "plugs": {
-                "gnome-extension-gnome-platform": {
+                **self.plugs,
+                platform_snap: {
                     "interface": "content",
                     "target": "$SNAP/gnome-platform",
                     "default-provider": "{snap}:{slot}".format(
-                        snap=platform_snap, slot=platform_snap),
-                },
-                "gnome-extension-common-themes": {
-                    "interface": "content",
-                    "target": "$SNAP/data-dir/themes",
-                    "default-provider": "gtk-common-themes:gtk-3-themes",
-                },
-                "gnome-extension-common-icons": {
-                    "interface": "content",
-                    "target": "$SNAP/data-dir/icons",
-                    "default-provider": "gtk-common-themes:icon-themes",
-                },
-                "gnome-extension-common-sounds": {
-                    "interface": "content",
-                    "target": "$SNAP/data-dir/sounds",
-                    "default-provider": "gtk-common-themes:sounds-themes",
+                        snap=platform_snap, slot=platform_snap
+                    ),
                 },
             },
             "environment": {
                 **self.environment,
-
                 "LD_LIBRARY_PATH": ":".join(ld_library_paths),
-
                 "GDK_PIXBUF_MODULE_FILE": "{}/gdk-pixbuf-loaders.cache".format(
-                    self.xdg_cache_home),
+                    self.xdg_cache_home
+                ),
                 "GDK_PIXBUF_MODULEDIR": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gdk-pixbuf-2.0/2.10.0/loaders",
-
                 "GI_TYPELIB_PATH": ":".join(gi_typelib_paths),
                 "GIO_MODULE_DIR": "{}/gio-modules".format(self.xdg_cache_home),
-                "GS_SCHEMA_DIR": "{}/glib-2.0/schemas".format(
-                    self.xdg_data_home),
-
+                "GS_SCHEMA_DIR": "{}/glib-2.0/schemas".format(self.xdg_data_home),
                 "GST_PLUGIN_SCANNER": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner",
                 "GST_PLUGIN_SYSTEM_PATH": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer-1.0",
-
                 "GTK_EXE_PREFIX": "$SNAP/gnome-platform/usr",
-                "GTK_IM_MODULE_DIR": "$XDG_CACHE_HOME/immodules",
-                "GTK_IM_MODULE_FILE": "$GTK_IM_MODULE_DIR/immodules.cache",
-                
+                "GTK_IM_MODULE_DIR": "{}/immodules".format(self.xdg_cache_home),
+                "GTK_IM_MODULE_FILE": "{}/immodules/immodules.cache".format(
+                    self.xdg_cache_home
+                ),
                 "LOCPATH": "$SNAP/gnome-platform/usr/lib/locale",
-                
                 "PATH": ":".join(path),
-                
                 "XCURSOR_PATH": "$SNAP/data-dir/icons:$SNAP/gnome-platform/usr/share/icons",
-                
                 "XDG_CONFIG_DIRS": ":".join(xdg_config_dirs),
                 "XDG_DATA_DIRS": ":".join(xdg_data_dirs),
                 "XKB_CONFIG_ROOT": "$SNAP/gnome-platform/usr/share/X11/xkb",
-                
                 "XLOCALEDIR": "$SNAP/gnome-platform/usr/share/X11/locale",
             },
         }
@@ -170,16 +144,13 @@ class GnomeExtension(DesktopCommonExtension):
 
         self.parts = {
             **self.parts,
-
             **dependency_part,
             "gnome-extension": {
                 "after": after_dependencies,
                 "plugin": "dump",
                 "source": "$SNAPCRAFT_EXTENSIONS_DIR/gnome",
                 "source-type": "local",
-                "organize": {
-                    "desktop-*": "snap/command-chain/",
-                },
+                "organize": {"desktop-*": "snap/command-chain/"},
                 "build-packages": ["build-essential", "libgtk-3-dev"],
             },
         }
