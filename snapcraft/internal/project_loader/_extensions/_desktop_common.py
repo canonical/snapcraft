23595d3f7dea10fe2a9acaf9c256dec5c6b569a5
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-07 14:21:28 +0000

    refactor gnome extension
    Move most of the environment variables into snapcraft.yaml

--- snapcraft/internal/project_loader/_extensions/_desktop_common.py
+++ snapcraft/internal/project_loader/_extensions/_desktop_common.py
@@ -33,14 +33,77 @@ class DesktopCommonExtension(Extension):
 
         super().__init__(yaml_data)
 
-        self.root_snippet = {"assumes": ["command-chain"]}
+        self.ld_library_paths = [
+            "$SNAP/usr/lib/$SNAP_LAUNCHER_ARCH_TRIPLET/lapack",
+            "$SNAP/usr/lib/$SNAP_LAUNCHER_ARCH_TRIPLET/blas",
+
+            "$LD_LIBRARY_PATH",
+
+            "/var/lib/snapd/lib/gl",
+
+            # Testability support
+            "$SNAP/testability",
+            "$SNAP/testability/$SNAPCRAFT_ARCH_TRIPLET",
+            "$SNAP/testability/$SNAPCRAFT_ARCH_TRIPLET/mesa",
+        ]
+
+        self.path = ["$PATH"]
+
+        self.xdg_cache_home = "$SNAP_USER_COMMON/.cache"
+        self.xdg_config_home = "$SNAP_USER_DATA/.config"
+        self.xdg_data_home = "$SNAP_USER_DATA/.local/share"
+        self.xdg_config_dirs = [
+            "$SNAP/etc/xdg",
+            "$XDG_CONFIG_DIRS",
+        ]
+        self.xdg_data_dirs = [
+            # Workaround for GLib < 2.53.2 not searching for schemas in $XDG_DATA_HOME:
+            #   https://bugzilla.gnome.org/show_bug.cgi?id=741335
+            self.xdg_data_home,
+            
+            "$SNAP_USER_DATA",
+            "$SNAP/data-dir",
+            "$SNAP/share",
+            "$SNAP/usr/share",
+            "$XDG_DATA_DIRS",
+        ]
+
+        self.environment = {
+            "FONTCONFIG_FILE": "$SNAP/gnome-platform/etc/fonts/fonts.conf",
+            "FONTCONFIG_PATH": "$SNAP/gnome-platform/etc/fonts",
+
+            "GST_PLUGIN_PATH": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer-1.0",
+            "GST_PLUGIN_SCANNER": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner",
+            "GST_PLUGIN_SYSTEM_PATH": "$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/gstreamer-1.0",
+
+            "IBUS_CONFIG_PATH": "{}/ibus".format(self.xdg_config_home),
+            
+            "LD_LIBRARY_PATH": ":".join(self.ld_library_paths),
+            
+            "LIBGL_DRIVERS_PATH": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/dri",
+            
+            "PATH": ":".join(self.path),
+            
+            "XDG_CACHE_HOME": self.xdg_cache_home,
+            "XDG_CONFIG_DIRS": ":".join(self.xdg_config_dirs),
+            "XDG_CONFIG_HOME": self.xdg_config_home,
+            "XDG_DATA_DIRS": ":".join(self.xdg_data_dirs),
+            "XDG_DATA_HOME": self.xdg_data_home,
+        }
+
+        self.root_snippet = {
+            "assumes": ["command-chain"],
+            "environment": self.environment,
+        }
+
+        self.command_chain = [
+            "snap/command-chain/desktop-init",
+            "snap/command-chain/desktop-exports",
+            "snap/command-chain/desktop-mark-and-exec",
+        ]
 
         self.app_snippet = {
-            "command-chain": [
-                "snap/command-chain/desktop-init",
-                "snap/command-chain/desktop-exports",
-                "snap/command-chain/desktop-mark-and-exec",
-            ]
+            "command-chain": self.command_chain,
         }
 
         self.parts = {
