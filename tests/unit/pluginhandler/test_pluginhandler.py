9fdd3bdc7283308f183bfd241b188d8aadd950f5
Claudio Matsuoka <cmatsuoka@gmail.com>
2019-01-28 11:17:36 -0200

    pluginhandler: handle removal of inconsistent files (#2450)
    When a part is cleaned, the system does its housekeeping by removing
    all the files or directories it prepared for stage or prime. If the
    user manually removes a file (say, in another part's override-prime),
    we should ignore the non-existent file and issue a warning instead
    of halting and catching fire.
    
    LP: #1813033
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- tests/unit/pluginhandler/test_pluginhandler.py
+++ tests/unit/pluginhandler/test_pluginhandler.py
@@ -1807,6 +1807,27 @@ class StateTestCase(StateBaseTestCase):
         self.assertThat(self.handler.next_step(), Equals(steps.PRIME))
         self.assertFalse(os.path.exists(bindir))
 
+    def test_clean_prime_state_inconsistent_files(self):
+        self.assertRaises(errors.NoLatestStepError, self.handler.latest_step)
+        self.assertThat(self.handler.next_step(), Equals(steps.PULL))
+
+        self.handler.mark_done(steps.STAGE)
+
+        self.handler.mark_done(
+            steps.PRIME, states.PrimeState({"bin/1", "bin/2"}, {"bin", "not-there"})
+        )
+
+        # Only create a subset of the files and directories that were marked as primed
+        bindir = os.path.join(self.prime_dir, "bin")
+        os.makedirs(bindir)
+        open(os.path.join(bindir, "1"), "w").close()
+
+        self.handler.clean_prime({})
+
+        self.assertThat(self.handler.latest_step(), Equals(steps.STAGE))
+        self.assertThat(self.handler.next_step(), Equals(steps.PRIME))
+        self.assertFalse(os.path.exists(bindir))
+
     def test_clean_prime_state_multiple_parts(self):
         self.assertRaises(errors.NoLatestStepError, self.handler.latest_step)
         self.assertThat(self.handler.next_step(), Equals(steps.PULL))
