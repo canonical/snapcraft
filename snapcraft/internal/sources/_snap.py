088c950c1111b15b1ca34a016ea4487cb7dd0b58
Sergio Schvezov <sergio.schvezov@canonical.com>
2019-02-13 01:24:17 -0300

    add Ltd

--- snapcraft/internal/sources/_snap.py
+++ snapcraft/internal/sources/_snap.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2019 Canonical
+# Copyright (C) 2019 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
