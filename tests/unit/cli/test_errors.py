9950692f4f266643c1ea879d86f7e3d4321e1a72
Sergio Schvezov <sergio.schvezov@canonical.com>
2019-02-26 10:18:41 -0300

    Update tests/unit/cli/test_errors.py
    Co-Authored-By: cmatsuoka <cmatsuoka@gmail.com>

--- tests/unit/cli/test_errors.py
+++ tests/unit/cli/test_errors.py
@@ -172,7 +172,7 @@ class ProviderErrorTest(ErrorsBaseTestCase):
         )
         self._raise_other_error()
         self.move_mock.assert_not_called()
-        self.assertTrue(self.traceback_mock.call_count == 1)
+        self.assertThat(self.move_mock.call_count, Equals(1))
 
     @mock.patch("os.path.isfile", return_value=False)
     def test_provider_error_inner(self, isfile_function):
