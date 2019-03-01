a59ec61c13686522f91e8666b2a86448850f93ca
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2019-03-01 14:18:34 -0300

    store: handle invalid snap file errors
    Properly handle corrupt or invalid snap files when extracting package
    data for signing or push.
    
    Fixes SNAPCRAFT-GE
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- tests/unit/commands/test_push.py
+++ tests/unit/commands/test_push.py
@@ -19,7 +19,7 @@ from unittest import mock
 from testtools.matchers import Contains, Equals, FileExists, Not
 from xdg import BaseDirectory
 
-from snapcraft import file_utils, storeapi
+from snapcraft import file_utils, storeapi, internal
 from snapcraft.storeapi.errors import (
     StoreDeltaApplicationError,
     StorePushError,
@@ -87,6 +87,19 @@ class PushCommandTestCase(PushCommandBaseTestCase):
     def test_push_nonexisting_snap_must_raise_exception(self):
         result = self.run_command(["push", "test-unexisting-snap"])
         self.assertThat(result.exit_code, Equals(2))
+
+    def test_push_invalid_snap_must_raise_exception(self):
+        snap_path = os.path.join(
+            os.path.dirname(tests.__file__), "data", "invalid.snap"
+        )
+
+        raised = self.assertRaises(
+            internal.errors.SnapDataExtractionError,
+            self.run_command,
+            ["push", snap_path],
+        )
+
+        self.assertThat(str(raised), Contains("Cannot read data from snap"))
 
     def test_push_unregistered_snap_must_raise_exception(self):
         class MockResponse:
