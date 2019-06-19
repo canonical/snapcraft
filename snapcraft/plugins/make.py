257c360a0626083e0133b8b654463604e8b0f184
Dimitri John Ledkov <xnox@ubuntu.com>
2019-06-19 12:31:13 +0100

    make: add "none" and "core20" as supported bases.
    LP: #1833409
    
    Signed-off-by: Dimitri John Ledkov <xnox@ubuntu.com>

--- snapcraft/plugins/make.py
+++ snapcraft/plugins/make.py
@@ -89,7 +89,7 @@ class MakePlugin(snapcraft.BasePlugin):
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
 
-        if project.info.base not in ("core", "core16", "core18"):
+        if project.info.base not in ("none", "core", "core16", "core18", "core20"):
             raise errors.PluginBaseError(part_name=self.name, base=project.info.base)
 
         self.build_packages.append("make")
