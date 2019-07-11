1cc2aa7b6d659cda64ae537e8e976c4b6ae36601
Chris Patterson <chris.patterson@canonical.com>
2019-07-11 11:05:48 -0400

    tests/unit/pluginhandler: introduce tests to repro symlink preservation bug
    _migrate_files() fails to preserve symlinks in some cases, we add a few
    tests here to cover a set of scenarios. Specifically, the
    test_migrate_files_preserves_symlink_nonempty_dir test case will fail
    prior to fix.
    
    LP #1833408
    
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tests/unit/pluginhandler/test_pluginhandler.py
+++ tests/unit/pluginhandler/test_pluginhandler.py
@@ -209,6 +209,105 @@ class PluginTestCase(unit.TestCase):
             "Expected migrated 'bar' to point to 'foo'",
         )
 
+    def test_migrate_files_preserves_symlink_file(self):
+        os.makedirs("install")
+        os.makedirs("stage")
+
+        with open(os.path.join("install", "foo"), "w") as f:
+            f.write("installed")
+
+        os.symlink("foo", os.path.join("install", "bar"))
+
+        files, dirs = pluginhandler._migratable_filesets(["*"], "install")
+        pluginhandler._migrate_files(files, dirs, "install", "stage")
+
+        # Verify that the symlinks were preserved
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "bar")),
+            "Expected migrated 'sym-a' to be a symlink.",
+        )
+
+    def test_migrate_files_preserves_symlink_nested_file(self):
+        os.makedirs(os.path.join("install", "a"))
+        os.makedirs("stage")
+
+        with open(os.path.join("install", "a", "foo"), "w") as f:
+            f.write("installed")
+
+        os.symlink(os.path.join("a", "foo"), os.path.join("install", "bar"))
+        os.symlink(os.path.join("foo"), os.path.join("install", "a", "bar"))
+
+        files, dirs = pluginhandler._migratable_filesets(["*"], "install")
+        pluginhandler._migrate_files(files, dirs, "install", "stage")
+
+        # Verify that the symlinks were preserved
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "bar")),
+            "Expected migrated 'sym-a' to be a symlink.",
+        )
+
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "a", "bar")),
+            "Expected migrated 'sym-a' to be a symlink.",
+        )
+
+    def test_migrate_files_preserves_symlink_empty_dir(self):
+        os.makedirs(os.path.join("install", "foo"))
+        os.makedirs("stage")
+
+        os.symlink("foo", os.path.join("install", "bar"))
+
+        files, dirs = pluginhandler._migratable_filesets(["*"], "install")
+        pluginhandler._migrate_files(files, dirs, "install", "stage")
+
+        # Verify that the symlinks were preserved
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "bar")),
+            "Expected migrated 'sym-a' to be a symlink.",
+        )
+
+    def test_migrate_files_preserves_symlink_nonempty_dir(self):
+        os.makedirs(os.path.join("install", "foo"))
+        os.makedirs("stage")
+
+        os.symlink("foo", os.path.join("install", "bar"))
+
+        with open(os.path.join("install", "foo", "xfile"), "w") as f:
+            f.write("installed")
+
+        files, dirs = pluginhandler._migratable_filesets(["*"], "install")
+        pluginhandler._migrate_files(files, dirs, "install", "stage")
+
+        # Verify that the symlinks were preserved
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "bar")),
+            "Expected migrated 'sym-a' to be a symlink.",
+        )
+
+    def test_migrate_files_preserves_symlink_nested_dir(self):
+        os.makedirs(os.path.join("install", "a", "b"))
+        os.makedirs("stage")
+
+        os.symlink(os.path.join("a", "b"), os.path.join("install", "bar"))
+        os.symlink(os.path.join("b"), os.path.join("install", "a", "bar"))
+
+        with open(os.path.join("install", "a", "b", "xfile"), "w") as f:
+            f.write("installed")
+
+        files, dirs = pluginhandler._migratable_filesets(["*"], "install")
+        pluginhandler._migrate_files(files, dirs, "install", "stage")
+
+        # Verify that the symlinks were preserved
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "bar")),
+            "Expected migrated 'bar' to be a symlink.",
+        )
+
+        self.assertTrue(
+            os.path.islink(os.path.join("stage", "a", "bar")),
+            "Expected migrated 'a/bar' to be a symlink.",
+        )
+
     def test_migrate_files_supports_follow_symlinks(self):
         os.makedirs("install")
         os.makedirs("stage")
