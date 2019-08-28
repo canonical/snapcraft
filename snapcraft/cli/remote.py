canonical/snapcraft

commit 4253354cf3f995da19f1a568ffcaaa3a1c3359d3

remote-build: fully preserve local sources
Fetch all "local" project parts and archive them inside of a git repo
for launchpad to build with.

Packaged sources are compressed with gzip'd tar files to prevent any
potential git-tracking-unfriendly sources from being added to the
created git repository.

- rewrite snapcraft.yaml shipped to launchpad to reflect source handling

- drop Repo class

- drop --git option

- Update "Worktree" class to manage the project's working tree and the
  creation of source repository that may be used by remote builders.
  Support gitification of the packaged source repository for remote
  builders that utilize it, i.e. launchpad.

- Raise error if base is not used, or suppported by remote-build.

- Raise error if `version-script` is used, it's being deprecated in
  favour of snapcraftctl set-version scriptlet.

Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

snapcraft/cli/remote.py
@@ -18,15 +18,9 @@ import click
 import os
 import uuid
 
-from snapcraft.internal.remote_build import (
-    Worktree,
-    LaunchpadClient,
-    Repo,
-    InfoFile,
-    errors,
-)
+from snapcraft.internal.remote_build import WorkTree, LaunchpadClient, InfoFile, errors
 from snapcraft.formatting_utils import humanize_list
-from typing import List, Tuple
+from typing import List
 from xdg import BaseDirectory
 from . import echo
 from ._options import get_project, PromptOption
@@ -73,9 +67,6 @@ def remotecli():
     cls=PromptOption,
 )
 @click.option(
-    "--git", is_flag=True, required=False, help="Build a local git repository."
-)
-@click.option(
     "--user", metavar="<username>", nargs=1, required=False, help="Launchpad username."
 )
 def remote_build(
@@ -83,7 +74,6 @@ def remote_build(
     status: int,
     user: str,
     arch: str,
-    git: bool,
     accept_public_upload: bool,
     echoer=echo,
 ) -> None:
@@ -92,10 +82,6 @@ def remote_build(
     Command remote-build sends the current project to be built remotely. After the build
     is complete, packages for each architecture are retrieved and will be available in
     the local filesystem.
-
-    If the local files are under git revision control, use option --git to build the
-    current branch of this repository. Be aware, however, that untracked files and
-    uncommitted changes will not be used.
 
     If not specified in the snapcraft.yaml file, the list of architectures to build
     can be set using the --arch option. If both are specified, the architectures listed
@@ -110,7 +96,7 @@ def remote_build(
     Examples:
         snapcraft remote-build
         snapcraft remote-build --arch=all
-        snapcraft remote-build --git --arch=arm64,armhf
+        snapcraft remote-build --arch=amd64,armhf
         snapcraft remote-build --recover 47860738
         snapcraft remote-build --status 47860738
     """
@@ -123,6 +109,11 @@ def remote_build(
     )
 
     project = get_project()
+
+    # TODO: use project.is_legacy() when available.
+    base = project.info.get_build_base()
+    if base is None:
+        raise errors.BaseRequiredError()
 
     remote_dir = os.path.join(
         BaseDirectory.save_data_path("snapcraft"),
@@ -144,9 +135,15 @@ def remote_build(
         remote_info["provider"] = provider
         remote_info.save()
 
+    # Pull/update sources for project.
+    worktree_dir = os.path.join(remote_dir, "worktree")
+    wt = WorkTree(worktree_dir, project)
+    repo_dir = wt.prepare_repository()
+
     # TODO: change login strategy after launchpad infrastructure is ready (LP #1827679)
     lp = LaunchpadClient(project, build_id)
     lp.login(user)
+    url = lp.push_source_tree(user, repo_dir)
 
     if status:
         # Show build status
@@ -171,23 +168,10 @@ def remote_build(
         # Sanity check for build architectures
         _check_supported_architectures(archs)
 
-        # The default branch to build
-        branch = "master"
-
-        # Send local data to the remote repository
-        echo.info("Sending data to remote builder...")
-
-        if git:
-            url, branch = _send_current_tree(provider, lp.user, build_id + "-git")
-        else:
-            url = _copy_and_send_tree(
-                provider, lp.user, remote_dir, build_id, project.info.name
-            )
-
         # Create build recipe
         # (delete any existing snap to remove leftovers from previous builds)
         lp.delete_snap()
-        lp.create_snap(url, branch, archs)
+        lp.create_snap(url, archs)
 
         targets = " for {}".format(humanize_list(archs, "and", "{}")) if archs else ""
         echo.info(
@@ -195,7 +179,7 @@ def remote_build(
         )
 
         # Start building
-        req_number = lp.start_build(timeout=5, attempts=5)
+        req_number = lp.start_build()
         echo.info(
             "If interrupted, resume with: 'snapcraft remote-build --recover {}'".format(
                 req_number
@@ -208,39 +192,6 @@ def _monitor_build(lp: LaunchpadClient) -> None:
     lp.monitor_build()
     echo.info("Build complete.")
     lp.delete_snap()
-
-
-def _send_current_tree(provider: str, user: str, build_id: str) -> Tuple[str, str]:
-    if not os.path.exists(".git"):
-        raise errors.NotGitRepositoryError
-    repo = Repo(".")
-    branch = repo.branch_name
-    if repo.is_dirty:
-        echo.warning(
-            "The following files have uncommitted changes that will not be used:"
-        )
-        echo.warning("\n".join(repo.uncommitted_files))
-    url = repo.push_remote(provider, user, branch, build_id)
-    return url, branch
-
-
-def _copy_and_send_tree(
-    provider: str, user: str, remote_dir: str, build_id: str, name: str
-) -> str:
-    if os.path.exists(".git"):
-        echo.warning(
-            "Git repository found, use option --git to build the current branch."
-        )
-    work_dir = os.path.join(remote_dir, build_id)
-    wt = Worktree(
-        ".",
-        work_dir,
-        ignore=[name + "_*.snap", name + "_*.txt.gz*", "parts", "stage", "prime"],
-    )
-    url = wt.add_remote(provider, user, build_id)
-    wt.sync()
-    wt.push()
-    return url
 
 
 def _check_supported_architectures(archs: List[str]) -> None:

snapcraft/internal/remote_build/_launchpad.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
+import importlib
 import logging
 import os
 import shutil
@@ -26,11 +28,11 @@ from lazr import restfulclient
 from launchpadlib.launchpad import Launchpad
 from typing import Any, Dict, List, Tuple
 from xdg import BaseDirectory
-from snapcraft.project import Project
 from . import errors
 
 import snapcraft
 from snapcraft.config import Config
+from snapcraft.project import Project
 
 _LP_POLL_INTERVAL = 30
 _LP_SUCCESS_STATUS = "Successfully built"
@@ -43,6 +45,11 @@ class LaunchpadClient:
     """Launchpad remote builder operations."""
 
     def __init__(self, project: Project, build_id: str) -> None:
+        try:
+            self._git_module = importlib.import_module("git")  # type: Any
+        except ImportError:
+            raise errors.GitNotFoundProviderError(provider="Launchpad")
+
         self._id = build_id
         self._core_channel = "stable"
         self._snapcraft_channel = "edge"
@@ -83,7 +90,7 @@ class LaunchpadClient:
             version="devel",
         )
 
-    def create_snap(self, repository: str, branch: str, archs: List[str]) -> None:
+    def create_snap(self, repository: str, archs: List[str]) -> None:
         """Create a snap recipe."""
         logger.debug("Create snap for {}".format(self._id))
         # TODO: remove this after launchpad infrastructure is ready (LP #1827679)
@@ -92,7 +99,7 @@ class LaunchpadClient:
             "name": self._id,
             "owner": "/~" + self.user,
             "git_repository_url": url,
-            "git_path": branch,
+            "git_path": "master",
             "auto_build": False,
             "auto_build_archive": "/ubuntu/+archive/primary",
             "auto_build_pocket": "Updates",
@@ -130,7 +137,6 @@ class LaunchpadClient:
                 )
             )
 
-            # No entries yet, sleep for specified duration.
             time.sleep(timeout)
 
             # Refresh status.
@@ -276,3 +282,35 @@ class LaunchpadClient:
         logger.debug("Download snap from {!r}".format(url))
         with urllib.request.urlopen(url) as response, open(name, "wb") as snapfile:
             shutil.copyfileobj(response, snapfile)  # type: ignore
+
+    def _gitify_repository(self, repo_dir: str):
+        """Git-ify source repository tree.
+
+        :return: Git repo.
+        """
+        git_repo = self._git_module.Repo.init(repo_dir)
+
+        for f in os.listdir(repo_dir):
+            if f != ".git":
+                git_repo.index.add([f])
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        git_repo.index.commit(
+            "snapcraft commit\n\nversion: {}\ntimestamp: {}\n".format(
+                snapcraft.__version__, timestamp
+            )
+        )
+
+        return git_repo
+
+    def push_source_tree(self, user: str, repo_dir: str) -> str:
+        """Push source tree to launchpad, returning URL."""
+        git_repo = self._gitify_repository(repo_dir)
+
+        url = "git+ssh://{user}@git.launchpad.net/~{user}/+git/{id}/".format(
+            user=user, id=self._id
+        )
+
+        logger.info("Sending data to remote builder... ({})".format(url))
+        git_repo.git.push(url, "HEAD:master", force=True)
+        return url

tests/unit/remote_build/test_launchpad.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import snapcraft
 import textwrap
 
@@ -21,6 +22,7 @@ from snapcraft.internal.remote_build import LaunchpadClient, errors
 from testtools.matchers import Equals, Contains
 from tests import unit
 from unittest import mock
+from . import TestDir
 
 
 class FakeLaunchpadObject:
@@ -165,12 +167,12 @@ class LaunchpadTestCase(unit.TestCase):
         lpc = LaunchpadClient(self._project, "id")
         lpc.user = "user"
         lpc._lp = LaunchpadImpl()
-        lpc.create_snap("git+ssh://repo", "branch", ["arch1", "arch2"])
+        lpc.create_snap("git+ssh://repo", ["arch1", "arch2"])
         lpc._lp.snaps.new.assert_called_with(
             auto_build=False,
             auto_build_archive="/ubuntu/+archive/primary",
             auto_build_pocket="Updates",
-            git_path="branch",
+            git_path="master",
             git_repository_url="https://repo",
             name="id",
             owner="/~user",
@@ -300,3 +302,14 @@ class LaunchpadTestCase(unit.TestCase):
             snapcraft_yaml_file_path=snapcraft_yaml_file_path
         )
         return project
+
+    @mock.patch("launchpadlib.launchpad.Launchpad")
+    def test_git_repository_creation(self, mock_lp):
+        source_testdir = self.useFixture(TestDir())
+        source_testdir.create_file("foo")
+        repo_dir = source_testdir.path
+        lpc = LaunchpadClient(self._project, "id")
+        self.assertFalse(os.path.exists(os.path.join(repo_dir, ".git")))
+        git_repo = lpc._gitify_repository(repo_dir)
+        self.assertTrue(os.path.exists(os.path.join(repo_dir, ".git")))
+        self.assertThat(len(git_repo.head.object.hexsha), Equals(40))

snapcraft/internal/remote_build/__init__.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from ._worktree import Worktree  # noqa: F401
+from ._worktree import WorkTree  # noqa: F401
 from ._launchpad import LaunchpadClient  # noqa: F401
-from ._repo import Repo  # noqa: F401
 from ._info_file import InfoFile  # noqa: F401

snapcraft/internal/remote_build/_repo.py
@@ -1,109 +1,0 @@
-# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
-#
-# Copyright (C) 2019 Canonical Ltd
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import importlib
-
-from typing import List
-from typing import Any  # noqa: F401
-from . import errors
-
-import snapcraft
-
-
-class Repo:
-    """Project repository management operations."""
-
-    def __init__(self, path: str) -> None:
-        self._root = path
-
-        try:
-            git = importlib.import_module("git")  # type: Any
-        except ImportError:
-            raise errors.GitNotFoundError
-
-        try:
-            self._repo = git.Repo(path)
-        except git.exc.InvalidGitRepositoryError:
-            self._repo = git.Repo.init(path)
-
-    def add(self, entry: str) -> None:
-        self._repo.index.add([str(entry)])
-
-    def remove(self, entry: str) -> None:
-        self._repo.index.remove([str(entry)], working_tree=True)
-
-    def commit(self) -> None:
-        if self._repo.is_dirty():
-            self._repo.index.commit(
-                "snapcraft commit\n\nversion: {}".format(snapcraft.__version__)
-            )
-
-    def push_remote(self, provider: str, user: str, branch: str, build_id: str) -> str:
-        """Push without adding a remote to the local repository."""
-
-        if provider == "launchpad":
-            url = self._remote_url(user, build_id)
-            self._repo.git.push(url, branch + ":refs/head/master", force=True)
-            self._repo.git.push(url, "--tags")
-        else:
-            raise errors.RemoteBuilderNotSupportedError(provider=provider)
-        return url
-
-    def add_remote(self, provider: str, user: str, build_id: str) -> str:
-        if provider == "launchpad":
-            url = self._remote_url(user, build_id)
-        else:
-            raise errors.RemoteBuilderNotSupportedError(provider=provider)
-
-        for remote in self._repo.remotes:
-            if remote.name == provider:
-                self._remote = remote
-                return url
-
-        self._remote = self._repo.create_remote(provider, url=url)
-        return url
-
-    def push(self):
-        self._remote.push(refspec="master:master")
-
-    def reset(self) -> None:
-        self._repo.git.reset("--hard")
-
-    @property
-    def is_dirty(self) -> bool:
-        return self._repo.is_dirty()
-
-    @property
-    def uncommitted_files(self) -> List[str]:
-        files = []
-        for item in self._repo.index.diff(None):
-            files.append(item.a_path)
-        return files
-
-    @property
-    def branch_name(self) -> str:
-        # Check whether we are in a detached HEAD state
-        if self._repo.git.rev_parse("--symbolic-full-name", "HEAD") == "HEAD":
-            return "HEAD"
-        return self._repo.active_branch.name
-
-    @staticmethod
-    def _remote_url(user: str, build_id: str) -> str:
-        # TODO: change this after launchpad infrastructure is ready
-        url = "git+ssh://{user}@git.launchpad.net/~{user}/+git/{id}/".format(
-            user=user, id=build_id
-        )
-        return url

commit d9695dacff7bb145d771b3a3f5480415e1028955

remote-build: error if --user is required
When initializing LaunchpadClient, lookup user information from
snapcraft config.  If not present, and user not passed in, raise
error to inform user --user is required. I effectively broke
this behavior in 4253354cf3f995da19f1a568ffcaaa3a1c3359d3 because
this was done in login(), but not consistent with push_source_tree().

- Rename self.user -> self._user for consistency.

- Modify login() and push_source_tree() to use saved username.

- Move launchpad init up before worktree preparation so it fails
  soonest if --user was required.

- Shuffle config handling bits into _load_snapcraft_config() and
  _update_snapcraft_config().

- Update unit tests.

Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

snapcraft/cli/remote.py
@@ -141,15 +141,15 @@ def remote_build(
         remote_info["provider"] = provider
         remote_info.save()
 
+    # TODO: change login strategy after launchpad infrastructure is ready (LP #1827679)
+    lp = LaunchpadClient(project=project, build_id=build_id, user=user)
+    lp.login()
+
     # Pull/update sources for project.
     worktree_dir = os.path.join(remote_dir, "worktree")
     wt = WorkTree(worktree_dir, project, package_all_sources=package_all_sources)
     repo_dir = wt.prepare_repository()
-
-    # TODO: change login strategy after launchpad infrastructure is ready (LP #1827679)
-    lp = LaunchpadClient(project, build_id)
-    lp.login(user)
-    url = lp.push_source_tree(user, repo_dir)
+    url = lp.push_source_tree(repo_dir)
 
     if status:
         # Show build status

snapcraft/internal/remote_build/_launchpad.py
@@ -25,7 +25,7 @@ import urllib.parse
 
 from lazr import restfulclient
 from launchpadlib.launchpad import Launchpad
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Optional
 from xdg import BaseDirectory
 from . import errors
 
@@ -44,7 +44,7 @@ logger = logging.getLogger(__name__)
 class LaunchpadClient:
     """Launchpad remote builder operations."""
 
-    def __init__(self, project: Project, build_id: str) -> None:
+    def __init__(self, *, project: Project, build_id: str, user: Optional[str]) -> None:
         if not Git.check_command_installed():
             raise errors.GitNotFoundProviderError(provider="Launchpad")
 
@@ -62,24 +62,32 @@ class LaunchpadClient:
 
         os.makedirs(self._data_dir, mode=0o700, exist_ok=True)
         self._credentials = os.path.join(self._data_dir, "credentials")
+        self._load_snapcraft_config()
 
-    def login(self, user: str) -> None:
-        conf = Config()
-        conf.load()
+        # Save user to snapcraft config, if specified.
+        if user is not None:
+            self._user = user
+            self._update_snapcraft_config()
 
-        if user:
-            escaped_user = urllib.parse.quote(user).replace("%", "%%")
-            conf.set("username", escaped_user, section_name="Launchpad")
-            conf.save()
-        else:
-            escaped_user = conf.get("username", section_name="Launchpad")
-            if escaped_user:
-                user = urllib.parse.unquote(escaped_user)
-
-        if not user:
+        if self._user is None:
             raise errors.NoLaunchpadUsernameError
 
-        self.user = user
+    def _load_snapcraft_config(self):
+        self._config = Config()
+        self._config.load()
+        escaped_user = self._config.get("username", section_name="Launchpad")
+        if escaped_user:
+            self._user = urllib.parse.unquote(escaped_user)
+        else:
+            self._user = None
+
+    def _update_snapcraft_config(self):
+        self._config.load()
+        escaped_user = urllib.parse.quote(self._user).replace("%", "%%")
+        self._config.set("username", escaped_user, section_name="Launchpad")
+        self._config.save()
+
+    def login(self) -> None:
         self._lp = Launchpad.login_with(
             "snapcraft remote-build {}".format(snapcraft.__version__),
             "production",
@@ -95,7 +103,7 @@ class LaunchpadClient:
         url = repository.replace("git+ssh://", "https://")
         snap = {
             "name": self._id,
-            "owner": "/~" + self.user,
+            "owner": "/~" + self._user,
             "git_repository_url": url,
             "git_path": "master",
             "auto_build": False,
@@ -111,14 +119,14 @@ class LaunchpadClient:
     def delete_snap(self) -> None:
         """Remove a snap recipe and all associated files."""
         try:
-            snap = self._lp.snaps.getByName(name=self._id, owner="/~" + self.user)
+            snap = self._lp.snaps.getByName(name=self._id, owner="/~" + self._user)
             snap.lp_delete()
         except restfulclient.errors.NotFound:
             pass
 
     def start_build(self, timeout: int = 5, attempts: int = 5) -> int:
         """Initiate a new snap build."""
-        owner = self._lp.people[self.user]
+        owner = self._lp.people[self._user]
         dist = self._lp.distributions["ubuntu"]
         snap = self._lp.snaps.getByName(name=self._id, owner=owner)
         snap_build_request = snap.requestBuilds(
@@ -179,7 +187,7 @@ class LaunchpadClient:
     def recover_build(self, req_number: int) -> None:
         """Prepare internal state to monitor an existing build."""
         url = "https://api.launchpad.net/devel/~{}/+snap/{}/+build-request/{}".format(
-            self.user, self._id, req_number
+            self._user, self._id, req_number
         )
 
         try:
@@ -306,12 +314,12 @@ class LaunchpadClient:
 
         return git_handler
 
-    def push_source_tree(self, user: str, repo_dir: str) -> str:
+    def push_source_tree(self, repo_dir: str) -> str:
         """Push source tree to launchpad, returning URL."""
         git_handler = self._gitify_repository(repo_dir)
 
         url = "git+ssh://{user}@git.launchpad.net/~{user}/+git/{id}/".format(
-            user=user, id=self._id
+            user=self._user, id=self._id
         )
 
         logger.info("Sending data to remote builder... ({})".format(url))

tests/unit/remote_build/test_launchpad.py
@@ -148,12 +148,12 @@ class LaunchpadTestCase(unit.TestCase):
     def setUp(self):
         super().setUp()
         self._project = self._make_snapcraft_project()
+        self.lpc = LaunchpadClient(project=self._project, build_id="id", user="user")
 
     @mock.patch("launchpadlib.launchpad.Launchpad.login_with")
     def test_login(self, mock_login):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.login("user")
-        self.assertThat(lpc.user, Equals("user"))
+        self.lpc.login()
+        self.assertThat(self.lpc._user, Equals("user"))
         mock_login.assert_called_with(
             "snapcraft remote-build {}".format(snapcraft.__version__),
             "production",
@@ -164,11 +164,9 @@ class LaunchpadTestCase(unit.TestCase):
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     def test_create_snap(self, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.create_snap("git+ssh://repo", ["arch1", "arch2"])
-        lpc._lp.snaps.new.assert_called_with(
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.create_snap("git+ssh://repo", ["arch1", "arch2"])
+        self.lpc._lp.snaps.new.assert_called_with(
             auto_build=False,
             auto_build_archive="/ubuntu/+archive/primary",
             auto_build_pocket="Updates",
@@ -181,18 +179,14 @@ class LaunchpadTestCase(unit.TestCase):
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     def test_delete_snap(self, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.delete_snap()
-        lpc._lp.snaps.getByName_mock.assert_called_with(name="id", owner="/~user")
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.delete_snap()
+        self.lpc._lp.snaps.getByName_mock.assert_called_with(name="id", owner="/~user")
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     def test_start_build(self, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        num = lpc.start_build()
+        self.lpc._lp = LaunchpadImpl()
+        num = self.lpc.start_build()
         self.assertThat(num, Equals("1234"))
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
@@ -203,12 +197,10 @@ class LaunchpadTestCase(unit.TestCase):
         ),
     )
     def test_start_build_error(self, mock_rb, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
+        self.lpc._lp = LaunchpadImpl()
 
         raised = self.assertRaises(
-            errors.RemoteBuilderError, lpc.start_build, timeout=0, attempts=1
+            errors.RemoteBuilderError, self.lpc.start_build, timeout=0, attempts=1
         )
         self.assertThat(str(raised), Contains("snapcraft.yaml not found..."))
 
@@ -218,32 +210,29 @@ class LaunchpadTestCase(unit.TestCase):
         return_value=SnapBuildReqImpl(status="Pending", error_message=""),
     )
     def test_start_build_error_timeout(self, mock_rb, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
+        self.lpc._lp = LaunchpadImpl()
         raised = self.assertRaises(
-            errors.RemoteBuilderNotReadyError, lpc.start_build, timeout=0, attempts=1
+            errors.RemoteBuilderNotReadyError,
+            self.lpc.start_build,
+            timeout=0,
+            attempts=1,
         )
         self.assertThat(str(raised), Contains("is not ready"))
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     def test_recover_build(self, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.recover_build(1234)
-        self.assertThat(lpc._lp.load_mock.call_count, Equals(2))
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.recover_build(1234)
+        self.assertThat(self.lpc._lp.load_mock.call_count, Equals(2))
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     @mock.patch("snapcraft.internal.remote_build.LaunchpadClient._download_file")
     def test_monitor_build(self, mock_download_file, mock_lp):
         open("test_i386.txt.gz", "w").close()
         open("test_i386.txt.gz.1", "w").close()
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.start_build()
-        lpc.monitor_build(interval=0)
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.start_build()
+        self.lpc.monitor_build(interval=0)
         mock_download_file.assert_has_calls(
             [
                 mock.call("url_for/build_log_file_1", "test_i386.txt.gz.2"),
@@ -261,11 +250,9 @@ class LaunchpadTestCase(unit.TestCase):
     def test_monitor_build_error(
         self, mock_log, mock_urls, mock_download_file, mock_lp
     ):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.start_build()
-        lpc.monitor_build(interval=0)
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.start_build()
+        self.lpc.monitor_build(interval=0)
         mock_download_file.assert_has_calls(
             [mock.call("url_for/build_log_file_2", "test_amd64.txt.gz")]
         )
@@ -275,11 +262,9 @@ class LaunchpadTestCase(unit.TestCase):
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
     def test_get_build_status(self, mock_lp):
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.user = "user"
-        lpc._lp = LaunchpadImpl()
-        lpc.start_build()
-        build_status = lpc.get_build_status()
+        self.lpc._lp = LaunchpadImpl()
+        self.lpc.start_build()
+        build_status = self.lpc.get_build_status()
         self.assertThat(
             build_status,
             Equals([("i386", "Successfully built"), ("amd64", "Failed to build")]),
@@ -308,9 +293,8 @@ class LaunchpadTestCase(unit.TestCase):
         source_testdir = self.useFixture(TestDir())
         source_testdir.create_file("foo")
         repo_dir = source_testdir.path
-        lpc = LaunchpadClient(self._project, "id")
         self.assertFalse(os.path.exists(os.path.join(repo_dir, ".git")))
-        lpc._gitify_repository(repo_dir)
+        self.lpc._gitify_repository(repo_dir)
         self.assertTrue(os.path.exists(os.path.join(repo_dir, ".git")))
 
     @mock.patch("launchpadlib.launchpad.Launchpad")
@@ -319,8 +303,7 @@ class LaunchpadTestCase(unit.TestCase):
         source_testdir = self.useFixture(TestDir())
         source_testdir.create_file("foo")
         repo_dir = source_testdir.path
-        lpc = LaunchpadClient(self._project, "id")
-        lpc.push_source_tree("user", repo_dir)
+        self.lpc.push_source_tree(repo_dir)
         mock_push.assert_called_with(
             "git+ssh://user@git.launchpad.net/~user/+git/id/", "HEAD:master", force=True
         )