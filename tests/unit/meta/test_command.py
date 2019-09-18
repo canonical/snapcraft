548e081a92d73abc721c687c69fe25f051a8ae31
Chris Patterson <chris.patterson@canonical.com>
2019-09-18 11:15:22 -0400

    command: refactor to work with Snap meta
    - Rename get_command() to `command` property
    - Introduce `prime_command()` to massage command given
      `prime_dir`, `can_use_wrapper`, and `massage_command`.
    - Introduce `requires_wrapper` property.
    - Introduce `wrapped_command` property to hold wrapped
      command instead of overriding command.  `write_command_wrapper`
      will use this instead of command.
    - Add some function docs for _massage_command to summarize.
    - Rename generate_wrapper -> write_wrapper for consistency
    - Update test_command.py tests
    
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tests/unit/meta/test_command.py
+++ tests/unit/meta/test_command.py
@@ -31,168 +31,147 @@ def _create_file(file_path: str, *, mode=0o755) -> None:
 class CommandWithoutWrapperTest(unit.TestCase):
     def test_command(self):
         _create_file(os.path.join(self.path, "foo"))
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo",
-            prime_dir=self.path,
-            can_use_wrapper=False,
+        cmd = command.Command(app_name="foo", command_name="command", command="foo")
+
+        cmd.prime_command(
+            can_use_wrapper=False, massage_command=True, prime_dir=self.path
         )
 
-        self.assertThat(cmd.get_command(), Equals("foo"))
+        self.assertThat(cmd.command, Equals("foo"))
 
     def test_command_with_args(self):
         _create_file(os.path.join(self.path, "foo"))
         cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo bar -baz",
-            prime_dir=self.path,
-            can_use_wrapper=False,
+            app_name="foo", command_name="command", command="foo bar -baz"
+        )
+        cmd.prime_command(
+            can_use_wrapper=False, massage_command=True, prime_dir=self.path
         )
 
-        self.assertThat(cmd.get_command(), Equals("foo bar -baz"))
+        self.assertThat(cmd.command, Equals("foo bar -baz"))
 
     def test_command_does_not_match_snapd_pattern(self):
         _create_file(os.path.join(self.path, "foo"))
         cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo /!option",
-            prime_dir=self.path,
-            can_use_wrapper=False,
+            app_name="foo", command_name="command", command="foo /!option"
         )
 
-        self.assertRaises(errors.InvalidAppCommandFormatError, cmd.get_command)
+        self.assertRaises(
+            errors.InvalidAppCommandFormatError,
+            cmd.prime_command,
+            can_use_wrapper=False,
+            massage_command=True,
+            prime_dir=self.path,
+        )
 
     def test_command_starts_with_slash(self):
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="/foo",
-            prime_dir=self.path,
-            can_use_wrapper=False,
-        )
+        cmd = command.Command(app_name="foo", command_name="command", command="/foo")
 
-        self.assertRaises(errors.InvalidAppCommandFormatError, cmd.get_command)
+        self.assertRaises(
+            errors.InvalidAppCommandFormatError,
+            cmd.prime_command,
+            can_use_wrapper=False,
+            massage_command=True,
+            prime_dir=self.path,
+        )
 
     def test_command_relative_command_found_in_slash(self):
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="sh",
-            prime_dir=self.path,
-            can_use_wrapper=False,
-        )
+        cmd = command.Command(app_name="foo", command_name="command", command="sh")
 
-        self.assertRaises(errors.InvalidAppCommandFormatError, cmd.get_command)
+        self.assertRaises(
+            errors.InvalidAppCommandFormatError,
+            cmd.prime_command,
+            can_use_wrapper=False,
+            massage_command=True,
+            prime_dir=self.path,
+        )
 
     def test_command_not_executable(self):
         _create_file(os.path.join(self.path, "foo"), mode=0o644)
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo",
-            prime_dir=self.path,
-            can_use_wrapper=False,
-        )
+        cmd = command.Command(app_name="foo", command_name="command", command="foo")
 
-        self.assertRaises(errors.InvalidAppCommandNotExecutable, cmd.get_command)
+        self.assertRaises(
+            errors.InvalidAppCommandNotExecutable,
+            cmd.prime_command,
+            can_use_wrapper=False,
+            massage_command=True,
+            prime_dir=self.path,
+        )
 
 
 class CommandWithWrapperTest(unit.TestCase):
     def test_command(self):
         _create_file(os.path.join(self.path, "foo"))
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo",
-            prime_dir=self.path,
-            can_use_wrapper=True,
+        cmd = command.Command(app_name="foo", command_name="command", command="foo")
+        cmd.prime_command(
+            can_use_wrapper=True, massage_command=True, prime_dir=self.path
         )
 
-        self.assertThat(cmd.get_command(), Equals("foo"))
+        self.assertThat(cmd.command, Equals("foo"))
 
     def test_command_with_args(self):
         _create_file(os.path.join(self.path, "foo"))
         cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo bar -baz",
-            prime_dir=self.path,
-            can_use_wrapper=False,
+            app_name="foo", command_name="command", command="foo bar -baz"
         )
 
-        self.expectThat(cmd.get_command(), Equals("foo bar -baz"))
-        self.expectThat(cmd.generate_wrapper(), Is(None))
+        cmd.prime_command(
+            can_use_wrapper=True, massage_command=True, prime_dir=self.path
+        )
+        wrapper_path = cmd.write_wrapper(prime_dir=self.path)
+
+        self.expectThat(cmd.command, Equals("foo bar -baz"))
+        self.expectThat(wrapper_path, Is(None))
 
     def test_command_does_not_match_snapd_pattern(self):
         _create_file(os.path.join(self.path, "foo"))
         cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo /!option",
-            prime_dir=self.path,
-            can_use_wrapper=True,
+            app_name="foo", command_name="command", command="foo /!option"
         )
 
-        app_command = cmd.get_command()
-        wrapper_path = cmd.generate_wrapper()
-        self.expectThat(app_command, Equals("command-foo.wrapper"))
+        cmd.prime_command(
+            can_use_wrapper=True, massage_command=True, prime_dir=self.path
+        )
+        wrapper_path = cmd.write_wrapper(prime_dir=self.path)
+
+        self.expectThat(cmd.command, Equals("command-foo.wrapper"))
         self.assertThat(wrapper_path, FileExists())
         self.assertThat(
             wrapper_path, FileContains('#!/bin/sh\nexec $SNAP/foo /!option "$@"\n')
         )
 
     def test_command_starts_with_slash(self):
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="/foo",
-            prime_dir=self.path,
-            can_use_wrapper=True,
-        )
+        cmd = command.Command(app_name="foo", command_name="command", command="/foo")
 
-        app_command = cmd.get_command()
-        wrapper_path = cmd.generate_wrapper()
-        self.expectThat(app_command, Equals("command-foo.wrapper"))
+        cmd.prime_command(
+            can_use_wrapper=True, massage_command=True, prime_dir=self.path
+        )
+        wrapper_path = cmd.write_wrapper(prime_dir=self.path)
+
+        self.expectThat(cmd.command, Equals("command-foo.wrapper"))
         self.assertThat(wrapper_path, FileExists())
         self.assertThat(wrapper_path, FileContains('#!/bin/sh\nexec /foo "$@"\n'))
 
     def test_command_relative_command_found_in_slash(self):
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="sh",
-            prime_dir=self.path,
-            can_use_wrapper=True,
-        )
+        cmd = command.Command(app_name="foo", command_name="command", command="sh")
 
-        app_command = cmd.get_command()
-        wrapper_path = cmd.generate_wrapper()
-        self.expectThat(app_command, Equals("command-foo.wrapper"))
+        cmd.prime_command(
+            can_use_wrapper=True, massage_command=True, prime_dir=self.path
+        )
+        wrapper_path = cmd.write_wrapper(prime_dir=self.path)
+
+        self.expectThat(cmd.command, Equals("command-foo.wrapper"))
         self.assertThat(wrapper_path, FileExists())
         self.assertThat(wrapper_path, FileContains('#!/bin/sh\nexec /bin/sh "$@"\n'))
 
     def test_command_not_executable(self):
         _create_file(os.path.join(self.path, "foo"), mode=0o644)
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="foo",
-            prime_dir=self.path,
+        cmd = command.Command(app_name="foo", command_name="command", command="foo")
+
+        self.assertRaises(
+            errors.InvalidAppCommandNotExecutable,
+            cmd.prime_command,
             can_use_wrapper=True,
-        )
-
-        self.assertRaises(errors.InvalidAppCommandNotExecutable, cmd.get_command)
-
-    def test_generate_wrapper_called_before_get_command(self):
-        cmd = command.Command(
-            app_name="foo",
-            command_name="command",
-            command="/foo",
+            massage_command=True,
             prime_dir=self.path,
-            can_use_wrapper=True,
         )
-
-        self.assertRaises(RuntimeError, cmd.generate_wrapper)
