2fb241e5622fd14327a77ab54c6c0d046ec40968
Ken VanDine <ken.vandine@canonical.com>
2019-10-24 11:04:15 -0400

    Drop slots from extension tests to match changes needed for snap validate

--- tests/unit/project_loader/extensions/test_gnome_3_28.py
+++ tests/unit/project_loader/extensions/test_gnome_3_28.py
@@ -35,22 +35,22 @@ class ExtensionTest(ProjectLoaderBaseTest):
                         "gtk-3-themes": {
                             "interface": "content",
                             "target": "$SNAP/data-dir/themes",
-                            "default-provider": "gtk-common-themes:gtk-3-themes",
+                            "default-provider": "gtk-common-themes",
                         },
                         "icon-themes": {
                             "interface": "content",
                             "target": "$SNAP/data-dir/icons",
-                            "default-provider": "gtk-common-themes:icon-themes",
+                            "default-provider": "gtk-common-themes",
                         },
                         "sound-themes": {
                             "interface": "content",
                             "target": "$SNAP/data-dir/sounds",
-                            "default-provider": "gtk-common-themes:sound-themes",
+                            "default-provider": "gtk-common-themes",
                         },
                         "gnome-3-28-1804": {
                             "interface": "content",
                             "target": "$SNAP/gnome-platform",
-                            "default-provider": "gnome-3-28-1804:gnome-3-28-1804",
+                            "default-provider": "gnome-3-28-1804",
                         },
                     },
                     "environment": {"SNAP_DESKTOP_RUNTIME": "$SNAP/gnome-platform"},
