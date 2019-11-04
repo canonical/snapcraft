4321b4ba6851f4c5fcbe372a33771b44b917926a
Heather Ellsworth <hellsworth@gnome.org>
2019-11-04 16:21:44 -0700

    Add expected GTK_USE_PORTALS in test

--- tests/unit/project_loader/extensions/test_gnome_3_34.py
+++ tests/unit/project_loader/extensions/test_gnome_3_34.py
@@ -53,7 +53,10 @@ class ExtensionTest(ProjectLoaderBaseTest):
                             "default-provider": "gnome-3-34-1804",
                         },
                     },
-                    "environment": {"SNAP_DESKTOP_RUNTIME": "$SNAP/gnome-platform"},
+                    "environment": {
+                        "SNAP_DESKTOP_RUNTIME": "$SNAP/gnome-platform",
+                        "GTK_USE_PORTALS": "1",
+                    },
                     "layout": {
                         "/usr/bin/gjs": {"symlink": "$SNAP/gnome-platform/usr/bin/gjs"},
                         "/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/webkit2gtk-4.0": {
