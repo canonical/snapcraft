bad4d2d69cf467c9457e6a2f8a658721b28c462d
Chris Patterson <chris.patterson@canonical.com>
2020-01-13 14:20:36 -0500

    add support for system-usernames (#2858)
    - Support short-form and long-form definitions.
    - Support "shared" scope.
    - Support usernames matching the following regex:
      "^[a-zA-Z0-9](?:-?[a-zA-Z0-9])*$".
    
    Currently, snapd only supports "snap_daemon", but eventually
    other user names will follow.
    
    Implement support for system-usernames in Snap
    
    - Add new SystemUser class.
    - Add new enum for SystemUserScope to enumerate valid scopes.
    - Support short-form and long-form options.
    - Introduce SystemUsernamesValidationError for user-specific
      parsing issues.
    - Add/update test coverage for introduced code.
    
    The change is a little ugly due to the key lists and __dict__
    usage in Snap, but that will be removed shortly.
    
    LP #1845267
    
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

---
+++ snapcraft/internal/meta/system_user.py
@@ -0,0 +1,63 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2019 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import OrderedDict
+import enum
+import logging
+from typing import Dict
+
+from snapcraft.internal.meta import errors
+
+logger = logging.getLogger(__name__)
+
+
+@enum.unique
+class SystemUserScope(enum.Enum):
+    SHARED = 1
+
+
+class SystemUser:
+    def __init__(self, *, name: str, scope: SystemUserScope) -> None:
+        self.name = name
+        self.scope = scope
+
+    @classmethod
+    def from_dict(self, *, user_name: str, user_dict: Dict[str, str]) -> "SystemUser":
+        raw_scope = user_dict.get("scope", None)
+        if raw_scope is None:
+            raise errors.SystemUsernamesValidationError(
+                name=user_name, message="'scope' is undefined"
+            )
+
+        try:
+            scope = SystemUserScope[raw_scope.upper()]
+        except KeyError:
+            raise errors.SystemUsernamesValidationError(
+                name=user_name, message=f"scope {raw_scope!r} is invalid"
+            )
+
+        return SystemUser(name=user_name, scope=scope)
+
+    def to_dict(self):
+        user_dict = OrderedDict()
+        user_dict["scope"] = self.scope.name.lower()
+        return user_dict
+
+    def validate(self):
+        if not isinstance(self.scope, SystemUserScope):
+            raise errors.SystemUsernamesValidationError(
+                name=self.name, message=f"scope {self.scope!r} is invalid"
+            )
