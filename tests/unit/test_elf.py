canonical/snapcraft

commit 76e8a3f3f1c3f69d957b2806c9387ac3c57e926a

elf: consider content directories for determining dependencies
- Ensure LDD uses the correct library paths for resolution.
- Improve dependency resolution performance by fist checking if
  matched path is provided, rather than searching all dirs.
- Update crystal plugin update to use arch_triplet param and
  content directories.
- Update pluginhandler to use content_dirs.
- Removed redundant if-statement.
- Updated elf tests.
- Refactor Library, pulling in crawl_for_path.

Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

snapcraft/internal/elf.py
@@ -111,35 +111,23 @@ class Library:
         self,
         *,
         soname: str,
-        path: str,
-        root_path: str,
+        soname_path: str,
+        search_paths: List[str],
         core_base_path: str,
         arch: ElfArchitectureTuple,
         soname_cache: SonameCache
     ) -> None:
+
         self.soname = soname
+        self.soname_path = soname_path
+        self.search_paths = search_paths
+        self.core_base_path = core_base_path
+        self.arch = arch
+        self.soname_cache = soname_cache
 
-        # We need to always look for the soname inside root first,
-        # and after exhausting all options look in core_base_path.
-        if path.startswith(root_path):
-            self.path = path
-        else:
-            self.path = _crawl_for_path(
-                soname=soname,
-                root_path=root_path,
-                core_base_path=core_base_path,
-                arch=arch,
-                soname_cache=soname_cache,
-            )
+        # Resolve path, if possible.
+        self.path = self._crawl_for_path()
 
-        if not self.path and path.startswith(core_base_path):
-            self.path = path
-
-        # Required for libraries on the host and the fetching mechanism
-        if not self.path:
-            self.path = path
-
-        # self.path has the correct resulting path.
         if self.path.startswith(core_base_path):
             self.in_base_snap = True
         else:
@@ -148,44 +136,48 @@ class Library:
         logger.debug(
             "{soname} with original path {original_path} found on {path} in base: {in_base}".format(
                 soname=soname,
-                original_path=path,
+                original_path=soname_path,
                 path=self.path,
                 in_base=self.in_base_snap,
             )
         )
 
+    def _update_soname_cache(self, resolved_path: str) -> None:
+        self.soname_cache[self.arch, self.soname] = resolved_path
 
-def _crawl_for_path(
-    *,
-    soname: str,
-    root_path: str,
-    core_base_path: str,
-    arch: ElfArchitectureTuple,
-    soname_cache: SonameCache
-) -> str:
-    # Speed things up and return what was already found once.
-    if (arch, soname) in soname_cache:
-        return soname_cache[arch, soname]
+    def _is_valid_elf(self, resolved_path: str) -> bool:
+        return (
+            os.path.exists(resolved_path)
+            and ElfFile.is_elf(resolved_path)
+            and ElfFile(path=resolved_path).arch == self.arch
+        )
 
-    logger.debug("Crawling to find soname {!r}".format(soname))
-    for path in (root_path, core_base_path):
-        if not os.path.exists(path):
-            continue
-        for root, directories, files in os.walk(path):
-            for file_name in files:
-                if file_name == soname:
-                    file_path = os.path.join(root, file_name)
-                    if ElfFile.is_elf(file_path):
-                        # We found a match by name, anyway. Let's verify that
-                        # the architecture is the one we want.
-                        elf_file = ElfFile(path=file_path)
-                        if elf_file.arch == arch:
-                            soname_cache[arch, soname] = file_path
-                            return file_path
+    def _crawl_for_path(self) -> str:
+        # Speed things up and return what was already found once.
+        if (self.arch, self.soname) in self.soname_cache:
+            return self.soname_cache[self.arch, self.soname]
 
-    # If not found we cache it too
-    soname_cache[arch, soname] = None
-    return None
+        logger.debug("Crawling to find soname {!r}".format(self.soname))
+
+        if self._is_valid_elf(self.soname_path):
+            self._update_soname_cache(self.soname_path)
+            return self.soname_path
+
+        for path in self.search_paths:
+            if not os.path.exists(path):
+                continue
+            for root, directories, files in os.walk(path):
+                if self.soname not in files:
+                    continue
+
+                file_path = os.path.join(root, self.soname)
+                if self._is_valid_elf(file_path):
+                    self._update_soname_cache(file_path)
+                    return file_path
+
+        # Required for libraries on the host and the fetching mechanism.
+        self._update_soname_cache(self.soname_path)
+        return self.soname_path
 
 
 # Old versions of pyelftools return bytes rather than strings for
@@ -323,7 +315,12 @@ class ElfFile:
         return version_required
 
     def load_dependencies(
-        self, root_path: str, core_base_path: str, soname_cache: SonameCache = None
+        self,
+        root_path: str,
+        core_base_path: str,
+        content_dirs: Set[str],
+        arch_triplet: str,
+        soname_cache: SonameCache = None,
     ) -> Set[str]:
         """Load the set of libraries that are needed to satisfy elf's runtime.
 
@@ -343,11 +340,26 @@ class ElfFile:
 
         logger.debug("Getting dependencies for {!r}".format(self.path))
         ldd_out = []  # type: List[str]
+
+        search_paths = [root_path, *content_dirs, core_base_path]
+
+        ld_library_paths: List[str] = list()
+        for path in search_paths:
+            ld_library_paths.extend(common.get_library_paths(path, arch_triplet))
+
+        # Set environment for ldd with LD_LIBRARY_PATH.
+        env = os.environ.copy()
+        env["LD_LIBRARY_PATH"] = ":".join(ld_library_paths)
+
         try:
             # ldd output sample:
             # /lib64/ld-linux-x86-64.so.2 (0x00007fb3c5298000)
             # libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007fb3bef03000)
-            ldd_out = common.run_output(["ldd", self.path]).split("\n")
+            ldd_out = (
+                subprocess.check_output(["ldd", self.path], env=env)
+                .decode()
+                .split("\n")
+            )
         except subprocess.CalledProcessError:
             logger.warning(
                 "Unable to determine library dependencies for {!r}".format(self.path)
@@ -360,8 +372,8 @@ class ElfFile:
                 libs.add(
                     Library(
                         soname=ldd_line[0],
-                        path=ldd_line[2],
-                        root_path=root_path,
+                        soname_path=ldd_line[2],
+                        search_paths=search_paths,
                         core_base_path=core_base_path,
                         arch=self.arch,
                         soname_cache=soname_cache,

tests/unit/test_elf.py
@@ -22,6 +22,7 @@ import sys
 from testtools.matchers import Contains, EndsWith, Equals, NotEquals, StartsWith
 from unittest import mock
 
+from snapcraft import ProjectOptions
 from snapcraft.internal import errors, elf
 from tests import unit, fixture_setup
 
@@ -32,6 +33,8 @@ class TestElfBase(unit.TestCase):
 
         self.fake_elf = fixture_setup.FakeElf(root_path=self.path)
         self.useFixture(self.fake_elf)
+        self.content_dirs = []
+        self.arch_triplet = ProjectOptions().arch_triplet
 
 
 class TestLdLibraryPathParser(unit.TestCase):
@@ -123,6 +126,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
         )
 
         self.assertThat(
@@ -142,6 +147,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
             soname_cache=soname_cache,
         )
 
@@ -156,6 +163,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
         )
 
         self.assertThat(
@@ -177,6 +186,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
         )
 
         self.assertThat(libs, Equals(frozenset(["/lib/foo.so.1", "/usr/lib/bar.so.2"])))
@@ -186,6 +197,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
         )
 
         self.assertThat(
@@ -200,6 +213,8 @@ class TestGetLibraries(TestElfBase):
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
             core_base_path=self.fake_elf.core_base_path,
+            arch_triplet=self.arch_triplet,
+            content_dirs=self.content_dirs,
         )
 
         self.assertThat(libs, Equals(set()))

snapcraft/internal/pluginhandler/__init__.py
@@ -817,16 +817,24 @@ class PluginHandler:
 
         # Clear the cache of all libs that aren't already in the primedir
         self._soname_cache.reset_except_root(self.primedir)
+
+        # Determine content directories.
+        content_dirs = self._project_options._get_provider_content_dirs()
+
         for elf_file in elf_files:
             all_dependencies.update(
                 elf_file.load_dependencies(
                     root_path=self.primedir,
                     core_base_path=core_path,
+                    content_dirs=content_dirs,
+                    arch_triplet=self._project_options.arch_triplet,
                     soname_cache=self._soname_cache,
                 )
             )
 
-        dependency_paths = self._handle_dependencies(all_dependencies)
+        dependency_paths = self._handle_dependencies(
+            all_dependencies=all_dependencies, content_dirs=content_dirs
+        )
 
         if not self._build_attributes.keep_execstack():
             clear_execstack(elf_files=elf_files)
@@ -895,12 +903,14 @@ class PluginHandler:
         # part.
         _clean_migrated_files(primed_files, primed_directories, shared_directory)
 
-    def _handle_dependencies(self, all_dependencies: Set[str]):
+    def _handle_dependencies(
+        self, *, all_dependencies: Set[str], content_dirs: Set[str]
+    ):
         # Split the necessary dependencies into their corresponding location.
         # We'll only track the part and staged dependencies, since they should have
         # already been primed by other means, and migrating them again could
         # potentially override the `stage` or `snap` filtering.
-        dirs = [self.plugin.installdir, self.stagedir, self.primedir]
+        dirs = [self.plugin.installdir, self.stagedir, self.primedir, *content_dirs]
 
         dependencies = _split_dependencies(all_dependencies, dirs)
         dependency_paths: Set[str] = set()

snapcraft/plugins/crystal.py
@@ -104,6 +104,8 @@ class CrystalPlugin(snapcraft.BasePlugin):
             elf_dependencies_path = elf_file.load_dependencies(
                 root_path=self.installdir,
                 core_base_path=common.get_core_path(self.project.info.get_build_base()),
+                arch_triplet=self.project.arch_triplet,
+                content_dirs=self.project._get_provider_content_dirs(),
             )
             for elf_dependency_path in elf_dependencies_path:
                 lib_install_path = os.path.join(

commit f2ddec570279ba252820afca9ac583bf3d99a981

elf: search for host libraries within search paths
A regression was introduced with commit 76e8a3f3f1c3f69d957b2806c9387ac3c57e926a
which considered elf files on the host system to be valid. Additionally, since
the host-found libraries were absolute paths, an os.path.join suffixing the
search paths snapcraft cared about were ignored.

A unit test which considers this scenario was added.

LP: #1860766

Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

snapcraft/internal/elf.py
@@ -209,18 +209,23 @@ class Library:
 
         logger.debug("Crawling to find soname {!r}".format(self.soname))
 
-        if self._is_valid_elf(self.soname_path):
+        valid_search_paths = [p for p in self.search_paths if os.path.exists(p)]
+        in_search_paths = any(
+            [self.soname_path.startswith(p) for p in valid_search_paths]
+        )
+
+        # Expedite path crawling if we have a valid elf file that lives
+        # inside the search paths.
+        if in_search_paths and self._is_valid_elf(self.soname_path):
             self._update_soname_cache(self.soname_path)
             return self.soname_path
 
-        for path in self.search_paths:
-            if not os.path.exists(path):
-                continue
+        for path in valid_search_paths:
             for root, directories, files in os.walk(path):
                 if self.soname not in files:
                     continue
 
-                file_path = os.path.join(root, self.soname)
+                file_path = os.path.join(root, self.soname.strip("/"))
                 if self._is_valid_elf(file_path):
                     self._update_soname_cache(file_path)
                     return file_path

tests/bin/elf/ldd
@@ -20,6 +20,9 @@ _LDD_OUT = {
         'barsnap.so.2 => {CORE_PATH}/barsnap.so.2 (0xbeef)',
         '/lib/baz.so.2 (0x1234)',
     ],
+    'fake_elf-with-host-libraries': [
+        'moo.so.2 => /usr/lib/moo.so.2 (0xbeef)',
+    ]
 }
 
 

tests/fixture_setup/_unittests.py
@@ -128,6 +128,7 @@ def _fake_elffile_extract_attributes(self):
         "fake_elf-with-core-libs",
         "fake_elf-with-missing-libs",
         "fake_elf-bad-patchelf",
+        "fake_elf-with-host-libraries",
     ]:
         glibc = elf.NeededLibrary(name="libc.so.6")
         glibc.add_version("GLIBC_2.2.5")
@@ -299,6 +300,9 @@ class FakeElf(fixtures.Fixture):
             "fake_elf-shared-object": elf.ElfFile(
                 path=os.path.join(self.root_path, "fake_elf-shared-object")
             ),
+            "fake_elf-with-host-libraries": elf.ElfFile(
+                path=os.path.join(self.root_path, "fake_elf-with-host-libraries")
+            ),
             "fake_elf-bad-ldd": elf.ElfFile(
                 path=os.path.join(self.root_path, "fake_elf-bad-ldd")
             ),
@@ -329,9 +333,13 @@ class FakeElf(fixtures.Fixture):
                 if elf_file.path.endswith("fake_elf-bad-patchelf"):
                     f.write(b"nointerpreter")
 
-        self.root_libraries = {"foo.so.1": os.path.join(self.root_path, "foo.so.1")}
+        self.root_libraries = {
+            "foo.so.1": os.path.join(self.root_path, "foo.so.1"),
+            "moo.so.2": os.path.join(self.root_path, "non-standard", "moo.so.2"),
+        }
 
         for root_library in self.root_libraries.values():
+            os.makedirs(os.path.dirname(root_library), exist_ok=True)
             with open(root_library, "wb") as f:
                 f.write(b"\x7fELF")
 

tests/unit/test_elf.py
@@ -139,14 +139,7 @@ class TestGetLibraries(TestElfBase):
     def setUp(self):
         super().setUp()
 
-        patcher = mock.patch("os.path.exists")
-        self.path_exists_mock = patcher.start()
-        self.addCleanup(patcher.stop)
-
-        self.path_exists_mock.return_value = True
-
-        self.fake_logger = fixtures.FakeLogger(level=logging.WARNING)
-        self.useFixture(self.fake_logger)
+        self.useFixture(fixtures.MockPatch("os.path.exists", return_value=True))
 
     def test_get_libraries(self):
         elf_file = self.fake_elf["fake_elf-2.23"]
@@ -236,6 +229,9 @@ class TestGetLibraries(TestElfBase):
         )
 
     def test_get_libraries_ldd_failure_logs_warning(self):
+        self.fake_logger = fixtures.FakeLogger(level=logging.WARNING)
+        self.useFixture(self.fake_logger)
+
         elf_file = self.fake_elf["fake_elf-bad-ldd"]
         libs = elf_file.load_dependencies(
             root_path=self.fake_elf.root_path,
@@ -249,6 +245,29 @@ class TestGetLibraries(TestElfBase):
             self.fake_logger.output,
             Contains("Unable to determine library dependencies for"),
         )
+
+    def test_existing_host_library_searched_for(self):
+        elf_file = self.fake_elf["fake_elf-with-host-libraries"]
+
+        class MooLibrary(elf.Library):
+            """A Library implementation that always returns valid for moo."""
+
+            def _is_valid_elf(self, resolved_path: str) -> bool:
+                #  This path is defined in ldd for fake_elf-with-host-libraries.
+                if resolved_path == "/usr/lib/moo.so.2":
+                    return True
+                else:
+                    return super()._is_valid_elf(resolved_path)
+
+        with mock.patch("snapcraft.internal.elf.Library", side_effect=MooLibrary):
+            libs = elf_file.load_dependencies(
+                root_path=self.fake_elf.root_path,
+                core_base_path=self.fake_elf.core_base_path,
+                arch_triplet=self.arch_triplet,
+                content_dirs=self.content_dirs,
+            )
+
+        self.assertThat(libs, Equals({self.fake_elf.root_libraries["moo.so.2"]}))
 
 
 class TestGetElfFiles(TestElfBase):