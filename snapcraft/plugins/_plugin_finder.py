488c7e05a6b1dcf75560315084b2d276d5af3449
Sergio Schvezov <sergio.schvezov@canonical.com>
2020-04-07 17:05:03 -0300

    work on other platforms

--- snapcraft/plugins/_plugin_finder.py
+++ snapcraft/plugins/_plugin_finder.py
@@ -14,43 +14,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from typing import Type
 
 from snapcraft.internal import errors
 from . import v1
 
 # TODO: segregate into base specific plugins.
-_PLUGINS = {
-    "legacy": {
-        "ant": v1.AntPlugin,
-        "autotools": v1.AutotoolsPlugin,
-        "catkin": v1.CatkinPlugin,
-        "catkin_tools": v1.CatkinToolsPlugin,
-        "cmake": v1.CMakePlugin,
-        "colcon": v1.ColconPlugin,
-        "conda": v1.CondaPlugin,
-        "crystal": v1.CrystalPlugin,
-        "dotnet": v1.DotNetPlugin,
-        "dump": v1.DumpPlugin,
-        "go": v1.GoPlugin,
-        "godeps": v1.GodepsPlugin,
-        "gradle": v1.GradlePlugin,
-        "kbuild": v1.KBuildPlugin,
-        "kernel": v1.KernelPlugin,
-        "make": v1.MakePlugin,
-        "maven": v1.MavenPlugin,
-        "meson": v1.MesonPlugin,
-        "nil": v1.NilPlugin,
-        "nodejs": v1.NodePlugin,
-        "plainbox-provider": v1.PlainboxProviderPlugin,
-        "python": v1.PythonPlugin,
-        "qmake": v1.QmakePlugin,
-        "ruby": v1.RubyPlugin,
-        "rust": v1.RustPlugin,
-        "scons": v1.SconsPlugin,
-        "waf": v1.WafPlugin,
+if sys.platform == "linux":
+    _PLUGINS = {
+        "legacy": {
+            "ant": v1.AntPlugin,
+            "autotools": v1.AutotoolsPlugin,
+            "catkin": v1.CatkinPlugin,
+            "catkin_tools": v1.CatkinToolsPlugin,
+            "cmake": v1.CMakePlugin,
+            "colcon": v1.ColconPlugin,
+            "conda": v1.CondaPlugin,
+            "crystal": v1.CrystalPlugin,
+            "dotnet": v1.DotNetPlugin,
+            "dump": v1.DumpPlugin,
+            "go": v1.GoPlugin,
+            "godeps": v1.GodepsPlugin,
+            "gradle": v1.GradlePlugin,
+            "kbuild": v1.KBuildPlugin,
+            "kernel": v1.KernelPlugin,
+            "make": v1.MakePlugin,
+            "maven": v1.MavenPlugin,
+            "meson": v1.MesonPlugin,
+            "nil": v1.NilPlugin,
+            "nodejs": v1.NodePlugin,
+            "plainbox-provider": v1.PlainboxProviderPlugin,
+            "python": v1.PythonPlugin,
+            "qmake": v1.QmakePlugin,
+            "ruby": v1.RubyPlugin,
+            "rust": v1.RustPlugin,
+            "scons": v1.SconsPlugin,
+            "waf": v1.WafPlugin,
+        }
     }
-}
+else:
+    # We cannot import the plugins on anything but linux.
+    _PLUGINS = dict()
 
 
 def get_plugin_for_base(plugin_name: str, *, build_base: str) -> Type[v1.PluginV1]:
