77f4a4a8c4125d3597bc886b1fa8027b95dfed10
Chris Patterson <chris.patterson@canonical.com>
2020-04-26 13:20:04 -0400

    tests: add tests for python with stage and python-package dep
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

---
+++ tests/spread/plugins/v2/snaps/python-hello-with-stage-package-dep/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hello-world",
+    version="0.0.1",
+    author="Canonical LTD",
+    author_email="snapcraft@lists.snapcraft.io",
+    description="A simple hello world in python",
+    scripts=["hello"],
+)
