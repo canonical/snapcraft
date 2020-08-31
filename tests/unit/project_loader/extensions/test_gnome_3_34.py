0a5896e2456d08e65ffa956e9f5881a92552158a
heather <heather.ellsworth@canonical.com>
2020-08-31 14:21:30 -0600

    Update PYTHONPATH to use $SNAP/gnome-platform

--- tests/unit/project_loader/extensions/test_gnome_3_34.py
+++ tests/unit/project_loader/extensions/test_gnome_3_34.py
@@ -59,7 +59,7 @@ class ExtensionTest(ProjectLoaderBaseTest, CommandBaseTestCase):
                     "environment": {
                         "SNAP_DESKTOP_RUNTIME": "$SNAP/gnome-platform",
                         "GTK_USE_PORTALS": "1",
-                        "PYTHONPATH": "$PYTHONPATH:$SNAP_DESKTOP_RUNTIME/usr/lib/python3.6/site-packages:$SNAP/usr/lib/python3.6/site-packages:$SNAP/lib/python3.6/site-packages",
+                        "PYTHONPATH": "$PYTHONPATH:$SNAP/gnome-platform/usr/lib/python3.6/site-packages:$SNAP/usr/lib/python3.6/site-packages:$SNAP/lib/python3.6/site-packages",
                     },
                     "layout": {
                         "/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/webkit2gtk-4.0": {
@@ -225,7 +225,7 @@ class ExtensionTest(ProjectLoaderBaseTest, CommandBaseTestCase):
                         environment:
                           GTK_USE_PORTALS: \'1\'
                           SNAP_DESKTOP_RUNTIME: $SNAP/gnome-platform
-                          PYTHONPATH: $PYTHONPATH:$SNAP_DESKTOP_RUNTIME/usr/lib/python3.6/site-packages:$SNAP/usr/lib/python3.6/site-packages:$SNAP/lib/python3.6/site-packages
+                          PYTHONPATH: $PYTHONPATH:$SNAP/gnome-platform/usr/lib/python3.6/site-packages:$SNAP/usr/lib/python3.6/site-packages:$SNAP/lib/python3.6/site-packages
                     """
                 )
             ),
