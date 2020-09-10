262f118ad4199dd706697c275a4223d0b364028b
Sergio Schvezov <sergio.schvezov@canonical.com>
2020-09-10 11:33:58 -0300

    cli: client side check for setting default tracks (#3278)
    Check for valid track names within the active set of tracks before
    trying to set a default track.
    
    This is in part a workaround for the generic django error provided by
    the Snap Store but can live on even after that issue is fixed.
    
    LP: #1892553
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- snapcraft/cli/store.py
+++ snapcraft/cli/store.py
@@ -739,6 +739,23 @@ def set_default_track(snap_name: str, track_name: str):
     The track must be a valid active track for this operation to be successful.
     """
     store_client_cli = StoreClientCLI()
+
+    # Client-side check to verify that the selected track exists.
+    snap_channel_map = store_client_cli.get_snap_channel_map(snap_name=snap_name)
+    active_tracks = [
+        track.name
+        for track in snap_channel_map.snap.tracks
+        if track.status in ("default", "active")
+    ]
+    if track_name not in active_tracks:
+        echo.exit_error(
+            brief=f"The specified track {track_name!r} does not exist for {snap_name!r}.",
+            resolution=f"Ensure the {track_name!r} track exists for the {snap_name!r} snap and try again.",
+            details="Valid tracks for {!r}: {}.".format(
+                snap_name, ", ".join([f"{t!r}" for t in active_tracks])
+            ),
+        )
+
     metadata = dict(default_track=track_name)
     store_client_cli.upload_metadata(snap_name=snap_name, metadata=metadata, force=True)
 
