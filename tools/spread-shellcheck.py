9abfb5071bbdf77dec4216137f3ce4bec64a7cff
Chris Patterson <chris.patterson@canonical.com>
2020-11-05 07:07:29 -0500

    tools: sort imports with isort (#3353)
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tools/spread-shellcheck.py
+++ tools/spread-shellcheck.py
@@ -14,10 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import argparse
 import logging
 import os
 import subprocess
-import argparse
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import cpu_count
 
