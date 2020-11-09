4b73a2ec58c10edbf15f8bb0f5eb8895f29e230c
Chris Patterson <chris.patterson@canonical.com>
2020-11-09 07:27:34 -0500

    project schema tests: ensure json format checkers are loaded (#3357)
    Many of the tests would fail when not running the whole test suite.
    This is because the json format checkers in project_loader._config
    have not been loaded.  Ensure this module is imported, and mark the
    import to ignore related warnings.
    
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tests/unit/project/test_schema.py
+++ tests/unit/project/test_schema.py
@@ -21,6 +21,8 @@ import pytest
 from testtools import TestCase
 from testtools.matchers import Contains, Equals
 
+# required for schema format checkers
+import snapcraft.internal.project_loader._config  # noqa: F401
 from snapcraft.project import errors
 from snapcraft.project._schema import Validator
 
