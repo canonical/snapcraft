2b7b89a665aa6c01623329cbd13ab2819aef5c2c
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2020-11-27 15:41:34 +0000

    plugins/v1: move build base test in v1 plugins to base class
    V1 plugins can be used only if the build base is core, core16 or
    core18. As an additional sanity check, each plugin tests for base
    consistency. To clean that up, move the base check from each
    individual plugin to the V1 plugin base class constructor.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- snapcraft/plugins/v1/waf.py
+++ snapcraft/plugins/v1/waf.py
@@ -32,7 +32,6 @@ In addition, this plugin uses the following plugin-specific keywords:
       ./waf --help
 """
 
-from snapcraft.internal import errors
 from snapcraft.plugins.v1 import PluginV1
 
 
@@ -55,14 +54,10 @@ class WafPlugin(PluginV1):
 
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
+        self._setup_base_tools()
 
-        self._setup_base_tools(project._get_build_base())
-
-    def _setup_base_tools(self, base):
-        if base in ("core", "core16", "core18"):
-            self.build_packages.append("python-dev:native")
-        else:
-            raise errors.PluginBaseError(part_name=self.name, base=base)
+    def _setup_base_tools(self):
+        self.build_packages.append("python-dev:native")
 
     @classmethod
     def get_build_properties(cls):
