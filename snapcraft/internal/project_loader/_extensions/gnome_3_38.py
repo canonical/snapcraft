25cf5d2e3fdfcdcf88a229740ba284244d3b6052
Ken VanDine <ken.vandine@canonical.com>
2021-01-12 13:20:29 -0500

    Mark gnome-3-38 as experimental

--- snapcraft/internal/project_loader/_extensions/gnome_3_38.py
+++ snapcraft/internal/project_loader/_extensions/gnome_3_38.py
@@ -57,6 +57,10 @@ class ExtensionImpl(Extension):
     def get_supported_confinement() -> Tuple[str, ...]:
         return ("strict", "devmode")
 
+    @staticmethod
+    def is_experimental(base: Optional[str]) -> bool:
+        return True
+
     def __init__(self, *, extension_name: str, yaml_data: Dict[str, Any]) -> None:
         super().__init__(extension_name=extension_name, yaml_data=yaml_data)
 
