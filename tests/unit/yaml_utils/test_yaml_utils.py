3e7d4932e9260120ba9d9ca21d283a1c5065752a
Chris Patterson <chris.patterson@canonical.com>
2021-05-04 14:28:10 +0000

    yaml: detect and log warn on duplicate keys in snapcraft.yaml
    A very primitive check to warn if duplicate keys are found in
    the snapcraft.yaml.  At the moment the file is loaded multiple
    times which may result in duplicate warnings (ha ha) which we
    need to address in future work.  Given that there shouldn't be
    duplicates in YAML files, the extra verbosity should be OK.
    
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tests/unit/yaml_utils/test_yaml_utils.py
+++ tests/unit/yaml_utils/test_yaml_utils.py
@@ -15,39 +15,48 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import io
-import os
+import pytest
 
-from testtools.matchers import Equals
 
 from snapcraft import yaml_utils
 from snapcraft.yaml_utils import YamlValidationError
-from tests import unit
 
 
-class YamlLoadTests(unit.TestCase):
-    def test_load_yaml_file(self):
-        path = os.path.join(self.path, "test.yaml")
-        with open(path, "w") as f:
-            f.write("foo: bar")
+def test_load_yaml_file(caplog, tmp_path):
+    yaml_path = tmp_path / "test.yaml"
+    yaml_path.write_text("foo: bar")
 
-        cfg = yaml_utils.load_yaml_file(path)
+    cfg = yaml_utils.load_yaml_file(str(yaml_path))
 
-        self.assertThat(cfg, Equals({"foo": "bar"}))
-
-    def test_load_yaml_file_bad_yaml(self):
-        path = os.path.join(self.path, "test.yaml")
-        with open(path, "w") as f:
-            # Note the missing newlines...
-            f.write("foo: bar")
-            f.write("a-b-c: xyz")
-
-        self.assertRaises(YamlValidationError, yaml_utils.load_yaml_file, path)
+    assert cfg == {"foo": "bar"}
+    assert [r.message for r in caplog.records] == []
 
 
-class OctIntTest(unit.TestCase):
-    def test_octint_dump(self):
-        output = io.StringIO()
-        yaml_utils.dump(dict(number=yaml_utils.OctInt(8)), stream=output)
-        output.seek(0)
+def test_load_yaml_file_bad_yaml(caplog, tmp_path):
+    yaml_path = tmp_path / "test.yaml"
+    yaml_path.write_text("foo: bara-b-c: xyz\n")
 
-        self.assertThat(output.read().strip(), Equals("number: 0010"))
+    with pytest.raises(YamlValidationError):
+        yaml_utils.load_yaml_file(str(yaml_path))
+
+    assert [r.message for r in caplog.records] == []
+
+
+def test_load_yaml_file_with_duplicate_warnings(caplog, tmp_path):
+    yaml_path = tmp_path / "test.yaml"
+    yaml_path.write_text("foo: bar\nfoo: bar2")
+
+    cfg = yaml_utils.load_yaml_file(str(yaml_path), warn_duplicate_keys=True)
+
+    assert cfg == {"foo": "bar2"}
+    assert [r.message for r in caplog.records] == [
+        "Duplicate key in YAML detected: 'foo'"
+    ]
+
+
+def test_dump_octint(tmp_path):
+    output = io.StringIO()
+    yaml_utils.dump(dict(number=yaml_utils.OctInt(8)), stream=output)
+    output.seek(0)
+
+    assert output.read().strip() == "number: 0010"
