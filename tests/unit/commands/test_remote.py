5d8926418a97cbcafb9d317bdfe807c83b4fd3bf
Chris Patterson <chris.patterson@canonical.com>
2021-05-24 18:55:02 +0000

    remote unit tests: formatting fix
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- tests/unit/commands/test_remote.py
+++ tests/unit/commands/test_remote.py
@@ -29,10 +29,7 @@ class RemoteBuildTests(CommandBaseTestCase):
     def setUp(self):
         super().setUp()
         self.snapcraft_yaml = fixture_setup.SnapcraftYaml(
-            self.path,
-            parts={
-                "part0": {"plugin": "nil"},
-            },
+            self.path, parts={"part0": {"plugin": "nil"}},
         )
         self.useFixture(self.snapcraft_yaml)
 
