96668a6153c6393b047ae5f070771cd0151ab998
Isaac True <isaac.true@canonical.com>
2022-09-28 14:52:21 +0000

    tests: legacy: plugins: v2: kernel: use testtools functions
    Use the standard testtools functions instead of assert.
    
    Signed-off-by: Isaac True <isaac.true@canonical.com>

--- tests/legacy/unit/plugins/v2/test_kernel.py
+++ tests/legacy/unit/plugins/v2/test_kernel.py
@@ -15,11 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from testtools import TestCase
-from testtools.matchers import Equals
 
 from snapcraft_legacy.plugins.v2.kernel import KernelPlugin
-
-# pylint: disable=attribute-defined-outside-init
 
 
 class TestPluginKernel(TestCase):
@@ -57,164 +54,166 @@ class TestPluginKernel(TestCase):
         self._plugin = KernelPlugin(part_name="kernel", options=Options())
 
     def test_schema(self):
-        schema = KernelPlugin.get_schema()
-
-        self.assertThat(
-            schema,
-            Equals(
-                {
-                    "$schema": "http://json-schema.org/draft-04/schema#",
-                    "type": "object",
-                    "additionalProperties": False,
-                    "properties": {
-                        "kernel-kdefconfig": {
-                            "type": "array",
-                            "default": ["defconfig"],
-                        },
-                        "kernel-kconfigfile": {"type": "string", "default": None},
-                        "kernel-kconfigflavour": {"type": "string", "default": ""},
-                        "kernel-kconfigs": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-image-target": {
-                            "oneOf": [{"type": "string"}, {"type": "object"}],
-                            "default": "",
-                        },
-                        "kernel-with-firmware": {
-                            "type": "boolean",
-                            "default": True,
-                        },
-                        "kernel-device-trees": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-initrd-modules": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-initrd-configured-modules": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-initrd-firmware": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-initrd-compression": {
-                            "type": "string",
-                            "enum": ["lz4", "xz", "gz", "zstd"],
-                        },
-                        "kernel-initrd-compression-options": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-initrd-channel": {
-                            "type": "string",
-                            "default": "stable",
-                        },
-                        "kernel-initrd-overlay": {
-                            "type": "string",
-                            "default": "",
-                        },
-                        "kernel-initrd-addons": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-compiler": {
-                            "type": "string",
-                            "default": "",
-                        },
-                        "kernel-compiler-paths": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-compiler-parameters": {
-                            "type": "array",
-                            "minitems": 1,
-                            "uniqueItems": True,
-                            "items": {"type": "string"},
-                            "default": [],
-                        },
-                        "kernel-enable-zfs-support": {
-                            "type": "boolean",
-                            "default": False,
-                        },
-                        "kernel-enable-perf": {
-                            "type": "boolean",
-                            "default": False,
-                        },
-                        "kernel-add-ppa": {
-                            "type": "boolean",
-                            "default": True,
-                        },
+        self.assertEqual(
+            KernelPlugin.get_schema(),
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "object",
+                "additionalProperties": False,
+                "properties": {
+                    "kernel-kdefconfig": {
+                        "type": "array",
+                        "default": ["defconfig"],
+                    },
+                    "kernel-kconfigfile": {
+                        "type": "string",
+                        "default": None,
+                    },
+                    "kernel-kconfigflavour": {"type": "string", "default": ""},
+                    "kernel-kconfigs": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-image-target": {
+                        "oneOf": [{"type": "string"}, {"type": "object"}],
+                        "default": "",
+                    },
+                    "kernel-with-firmware": {
+                        "type": "boolean",
+                        "default": True,
+                    },
+                    "kernel-device-trees": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-initrd-modules": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-initrd-configured-modules": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-initrd-firmware": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-initrd-compression": {
+                        "type": "string",
+                        "enum": ["lz4", "xz", "gz", "zstd"],
+                    },
+                    "kernel-initrd-compression-options": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-initrd-channel": {
+                        "type": "string",
+                        "default": "stable",
+                    },
+                    "kernel-initrd-overlay": {
+                        "type": "string",
+                        "default": "",
+                    },
+                    "kernel-initrd-addons": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-compiler": {
+                        "type": "string",
+                        "default": "",
+                    },
+                    "kernel-compiler-paths": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-compiler-parameters": {
+                        "type": "array",
+                        "minitems": 1,
+                        "uniqueItems": True,
+                        "items": {"type": "string"},
+                        "default": [],
+                    },
+                    "kernel-enable-zfs-support": {
+                        "type": "boolean",
+                        "default": False,
+                    },
+                    "kernel-enable-perf": {
+                        "type": "boolean",
+                        "default": False,
+                    },
+                    "kernel-add-ppa": {
+                        "type": "boolean",
+                        "default": True,
                     },
                 },
-            ),
+            },
         )
 
     def test_get_base_build_packages(self):
-        assert self._plugin.get_build_packages() == {
-            "bc",
-            "binutils",
-            "gcc",
-            "cmake",
-            "cryptsetup",
-            "dracut-core",
-            "kmod",
-            "kpartx",
-            "lz4",
-            "systemd",
-        }
+        self.assertEqual(
+            self._plugin.get_build_packages(),
+            {
+                "bc",
+                "binutils",
+                "gcc",
+                "cmake",
+                "cryptsetup",
+                "dracut-core",
+                "kmod",
+                "kpartx",
+                "lz4",
+                "systemd",
+            },
+        )
 
     def test_get_build_snaps(self):
-        assert self._plugin.get_build_snaps() == set()
+        self.assertEqual(self._plugin.get_build_snaps(), set())
 
     def test_check_configuration(self):
         opt = self._plugin.options
 
-        assert opt.kernel_kdefconfig == ["defconfig"]
-        assert opt.kernel_kconfigfile is None
-        assert opt.kernel_kconfigflavour == ""
-        assert opt.kernel_kconfigs == []
-        assert opt.kernel_image_target == "bzImage"
-        assert opt.kernel_with_firmware
-        assert opt.kernel_device_trees == []
-        assert opt.kernel_compiler == "gcc"
-        assert opt.kernel_compiler_paths == []
-        assert opt.kernel_compiler_parameters == []
-        assert opt.kernel_initrd_modules == []
-        assert opt.kernel_initrd_configured_modules == []
-        assert opt.kernel_initrd_firmware == []
-        assert opt.kernel_initrd_compression == "lz4"
-        assert opt.kernel_initrd_compression_options == []
-        assert opt.kernel_initrd_channel == "stable"
-        assert opt.kernel_initrd_overlay == ""
-        assert opt.kernel_initrd_addons == []
-        assert not opt.kernel_enable_zfs_support
-        assert not opt.kernel_enable_perf
-        assert not opt.kernel_add_ppa
+        self.assertEqual(opt.kernel_kdefconfig, ["defconfig"])
+        self.assertIs(opt.kernel_kconfigfile, None)
+        self.assertEqual(opt.kernel_kconfigflavour, "")
+        self.assertEqual(opt.kernel_kconfigs, [])
+        self.assertEqual(opt.kernel_image_target, "bzImage")
+        self.assertTrue(opt.kernel_with_firmware)
+        self.assertEqual(opt.kernel_device_trees, [])
+        self.assertEqual(opt.kernel_compiler, "gcc")
+        self.assertEqual(opt.kernel_compiler_paths, [])
+        self.assertEqual(opt.kernel_compiler_parameters, [])
+        self.assertEqual(opt.kernel_initrd_modules, [])
+        self.assertEqual(opt.kernel_initrd_configured_modules, [])
+        self.assertEqual(opt.kernel_initrd_firmware, [])
+        self.assertEqual(opt.kernel_initrd_compression, "lz4")
+        self.assertEqual(opt.kernel_initrd_compression_options, [])
+        self.assertEqual(opt.kernel_initrd_channel, "stable")
+        self.assertEqual(opt.kernel_initrd_overlay, "")
+        self.assertEqual(opt.kernel_initrd_addons, [])
+        self.assertFalse(opt.kernel_enable_zfs_support)
+        self.assertFalse(opt.kernel_enable_perf)
+        self.assertFalse(opt.kernel_add_ppa)
