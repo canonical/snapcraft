0169604931abdafff9dd4b79e425d59d30e07eb9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 11:23:27 +0000

    Adding tarfile member sanitization to extractall()

--- snapcraft_legacy/internal/sources/_tar.py
+++ snapcraft_legacy/internal/sources/_tar.py
@@ -107,7 +107,26 @@ class Tar(FileBase):
                     m.mode = m.mode | 0o200
                     yield m
 
-            tar.extractall(members=filter_members(tar), path=dst)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, members=filter_members(tar), path=dst)
 
     def _strip_prefix(self, common, member):
         if member.name.startswith(common + "/"):
