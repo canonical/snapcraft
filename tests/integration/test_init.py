70a15517e40d14afc10dce7dac4cf15fcc455a60
Alex Lowe <alex.lowe@canonical.com>
2023-01-25 15:47:02 -0500

    Better comment

--- tests/integration/test_init.py
+++ tests/integration/test_init.py
@@ -3,8 +3,8 @@
 # Copyright 2023 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Basic Starcraft package demo tests."""
+"""Basic Starcraft package demo integration tests."""
 import subprocess
 
 
