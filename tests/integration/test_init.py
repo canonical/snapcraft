63fc646670125a24e3ee57ec59852bf203b6a15c
Alex Lowe <alex.lowe@canonical.com>
2023-01-25 19:32:57 -0500

    tools: Split unit and integration tests (#7)

---
+++ tests/integration/test_init.py
@@ -0,0 +1,25 @@
+# This file is part of starcraft.
+#
+# Copyright 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Basic Starcraft package demo integration tests."""
+import subprocess
+
+
+def test_cli():
+    expected = "Hello *craft team!\n"
+
+    actual = subprocess.check_output(["starcraft-hello"], text=True)
+
+    assert expected == actual
