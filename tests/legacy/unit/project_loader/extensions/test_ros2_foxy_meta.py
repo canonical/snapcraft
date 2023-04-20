0fadf06a4df5a644793b19fb6b5f571924af46d7
artivis <jeremie.deray@canonical.com>
2023-04-20 12:02:49 +0200

    ros: test ros2 foxy meta extentions
    Signed-off-by: artivis <jeremie.deray@canonical.com>

---
+++ tests/legacy/unit/project_loader/extensions/test_ros2_foxy_meta.py
@@ -0,0 +1,119 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2023 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from snapcraft_legacy.internal.project_loader._extensions.ros2_foxy_desktop import (
+    ExtensionImpl as Ros2FoxyDesktopExtension,
+)
+from snapcraft_legacy.internal.project_loader._extensions.ros2_foxy_ros_base import (
+    ExtensionImpl as Ros2FoxyRosBaseExtension,
+)
+from snapcraft_legacy.internal.project_loader._extensions.ros2_foxy_ros_core import (
+    ExtensionImpl as Ros2FoxyRosCoreExtension,
+)
+
+
+class TestClass:
+    scenarios = [
+        (
+            "desktop",
+            {
+                "extension_name": "ros2-foxy-desktop",
+                "extension_class": Ros2FoxyDesktopExtension,
+                "meta": "ros2-meta-desktop",
+                "meta_dev": "ros2-meta-desktop",
+            },
+            "ros-base",
+            {
+                "extension_name": "ros2-foxy-ros-base",
+                "extension_class": Ros2FoxyRosBaseExtension,
+                "meta": "ros2-meta-ros-base",
+                "meta_dev": "ros2-meta-ros-base",
+            },
+            "ros-core",
+            {
+                "extension_name": "ros2-foxy-ros-core",
+                "extension_class": Ros2FoxyRosCoreExtension,
+                "meta": "ros2-meta-ros-core",
+                "meta_dev": "ros2-meta-ros-core",
+            },
+        ),
+    ]
+
+    def test_extension(self, extension_name, extension_class, meta, meta_dev):
+        ros_extension = extension_class(
+            extension_name=extension_name, yaml_data=dict(base="core20")
+        )
+
+        assert ros_extension.root_snippet == {
+            "package-repositories": [
+                {
+                    "components": ["main"],
+                    "formats": ["deb"],
+                    "key-id": "C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654",
+                    "key-server": "keyserver.ubuntu.com",
+                    "suites": ["focal"],
+                    "type": "apt",
+                    "url": "http://repo.ros2.org/ubuntu/main",
+                }
+            ],
+            "plugs": {
+                "ros-meta": {
+                    "interface": "content",
+                    "content": "ros2-meta",
+                    "target": "$SNAP/opt/ros/underlay_ws",
+                    "default-provider": meta,
+                }
+            },
+        }
+
+        assert ros_extension.app_snippet == {
+            "command-chain": ["snap/command-chain/ros2-launch"],
+            "environment": {
+                "PYTHONPATH": "$SNAP/opt/ros/foxy/lib/python3.8/site-packages:$SNAP/usr/lib/python3/dist-packages:${PYTHONPATH}",
+                "ROS_VERSION": "2",
+                "ROS_DISTRO": "foxy",
+            },
+        }
+
+        assert ros_extension.part_snippet == {
+            "build-environment": [{"ROS_VERSION": "2"}, {"ROS_DISTRO": "foxy"}],
+            "build-snaps": [meta_dev],
+        }
+
+        assert ros_extension.parts == {
+            "ros2-foxy-extension": {
+                "build-packages": [
+                    "ros-foxy-ros-environment",
+                    "ros-foxy-ros-workspace",
+                    "ros-foxy-ament-index-cpp",
+                    "ros-foxy-ament-index-python",
+                ],
+                "stage-packages": [
+                    "ros-foxy-ros-environment",
+                    "ros-foxy-ros-workspace",
+                    "ros-foxy-ament-index-cpp",
+                    "ros-foxy-ament-index-python",
+                ],
+                "plugin": "make",
+                "source": "$SNAPCRAFT_EXTENSIONS_DIR/ros2",
+            }
+        }
+
+    def test_supported_bases(self, extension_name, extension_class, meta, meta_dev):
+        assert extension_class.get_supported_bases() == ("core20",)
+
+    def test_supported_confinement(self, extension_name, extension_class, meta, meta_dev):
+        extension_class.get_supported_confinement() == ("strict", "devmode")
