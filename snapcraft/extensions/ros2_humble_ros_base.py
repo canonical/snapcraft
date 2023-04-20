bb6b45bd1d68de58ca184cbf371f5a39d9c2fad9
artivis <jeremie.deray@canonical.com>
2023-04-20 11:41:25 +0200

    ros: add humble meta extensions

---
+++ snapcraft/extensions/ros2_humble_ros_base.py
@@ -0,0 +1,38 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Import types and tell flake8 to ignore the "unused" List.
+
+"""Extension to the Colcon plugin for ROS 2 Humble using content sharing."""
+
+from typing import Optional
+
+from overrides import overrides
+from typing_extensions import Final
+
+from ._ros2_humble_meta import ROS2HumbleMetaBase
+
+
+class ROS2HumbleRosBaseExtension(ROS2HumbleMetaBase):
+    """Drives ROS 2 build and runtime environment for snap using content-sharing."""
+
+    @staticmethod
+    @overrides
+    def is_experimental(base: Optional[str]) -> bool:
+        return True
+
+    ROS_META: Final[str] = "ros-humble-ros-base"
+    ROS_META_DEV: Final[str] = "ros-humble-ros-base"
