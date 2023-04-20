cb22a72f620eb044900f5fd9b79d74c3fee13fd7
artivis <jeremie.deray@canonical.com>
2023-04-20 09:10:46 +0200

    ros: typo
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- snapcraft_legacy/plugins/v2/colcon.py
+++ snapcraft_legacy/plugins/v2/colcon.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""The colcon plugin for ROS2 parts.
+"""The colcon plugin for ROS 2 parts.
 
     - colcon-packages:
       (list of strings)
