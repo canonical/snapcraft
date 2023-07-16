8389da6223c5298ef6815e373f37b366a6d627d4
Scarlett Moore <sgmoore@kde.org>
2023-07-16 05:49:39 -0700

    kde-neon-extension: new content pack, ensure XDG_CONFIG_HOME is set so that qt default.conf is found.

--- tests/unit/extensions/test_kde_neon.py
+++ tests/unit/extensions/test_kde_neon.py
@@ -38,7 +38,7 @@ def kde_neon_extension_with_build_snap():
             "base": "core22",
             "parts": {
                 "part1": {
-                    "build-snaps": ["kf5-5-105-qt-5-15-9-core22-sdk/latest/stable"]
+                    "build-snaps": ["kf5-5-108-qt-5-15-10-core22-sdk/latest/stable"]
                 }
             },
         },
@@ -53,7 +53,7 @@ def kde_neon_extension_with_default_build_snap_from_latest_edge():
         yaml_data={
             "base": "core22",
             "parts": {
-                "part1": {"build-snaps": ["kf5-5-105-qt-5-15-9-core22-sdk/latest/edge"]}
+                "part1": {"build-snaps": ["kf5-5-108-qt-5-15-10-core22-sdk/latest/edge"]}
             },
         },
         arch="amd64",
@@ -109,10 +109,10 @@ def test_get_root_snippet(kde_neon_extension):
                 "target": "$SNAP/data-dir/sounds",
                 "default-provider": "gtk-common-themes",
             },
-            "kf5-5-105-qt-5-15-9-core22": {
-                "content": "kf5-5-105-qt-5-15-9-core22-all",
+            "kf5-5-108-qt-5-15-10-core22": {
+                "content": "kf5-5-108-qt-5-15-10-core22-all",
                 "interface": "content",
-                "default-provider": "kf5-5-105-qt-5-15-9-core22",
+                "default-provider": "kf5-5-108-qt-5-15-10-core22",
                 "target": "$SNAP/kf5",
             },
         },
@@ -143,10 +143,10 @@ def test_get_root_snippet_with_external_sdk(kde_neon_extension_with_build_snap):
                 "target": "$SNAP/data-dir/sounds",
                 "default-provider": "gtk-common-themes",
             },
-            "kf5-5-105-qt-5-15-9-core22": {
-                "content": "kf5-5-105-qt-5-15-9-core22-all",
+            "kf5-5-108-qt-5-15-10-core22": {
+                "content": "kf5-5-108-qt-5-15-10-core22-all",
                 "interface": "content",
-                "default-provider": "kf5-5-105-qt-5-15-9-core22",
+                "default-provider": "kf5-5-108-qt-5-15-10-core22",
                 "target": "$SNAP/kf5",
             },
         },
@@ -172,19 +172,25 @@ class TestGetPartSnippet:
             "build-environment": [
                 {
                     "PATH": (
-                        "/snap/kf5-5-105-qt-5-15-9-core22-sdk/current/usr/bin${PATH:+:$PATH}"
+                        "/snap/kf5-5-108-qt-5-15-10-core22-sdk/current/usr/bin${PATH:+:$PATH}"
                     )
                 },
                 {
                     "XDG_DATA_DIRS": (
-                        "$CRAFT_STAGE/usr/share:/snap/kf5-5-105-qt-5-15-9-core22-sdk"
+                        "$CRAFT_STAGE/usr/share:/snap/kf5-5-108-qt-5-15-10-core22-sdk"
                         "/current/usr/share:/usr/share${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+                    )
+                },
+                {
+                    "XDG_CONFIG_HOME": (
+                        "$CRAFT_STAGE/etc/xdg:/snap/kf5-5-108-qt-5-15-10-core22-sdk/current/etc/xdg"
+                        "/etc/xdg"
                     )
                 },
                 {
                     "SNAPCRAFT_CMAKE_ARGS": (
                         "-DCMAKE_FIND_ROOT_PATH="
-                        "/snap/kf5-5-105-qt-5-15-9-core22-sdk/current"
+                        "/snap/kf5-5-108-qt-5-15-10-core22-sdk/current"
                         "${SNAPCRAFT_CMAKE_ARGS:+:$SNAPCRAFT_CMAKE_ARGS}"
                     )
                 },
@@ -197,20 +203,26 @@ def test_get_part_snippet_with_external_sdk(kde_neon_extension_with_build_snap):
         "build-environment": [
             {
                 "PATH": (
-                    "/snap/kf5-5-105-qt-5-15-9-core22-sdk/current/"
+                    "/snap/kf5-5-108-qt-5-15-10-core22-sdk/current/"
                     "usr/bin${PATH:+:$PATH}"
                 )
             },
             {
                 "XDG_DATA_DIRS": (
-                    "$CRAFT_STAGE/usr/share:/snap/kf5-5-105-qt-5-15-9-core22-sdk"
+                    "$CRAFT_STAGE/usr/share:/snap/kf5-5-108-qt-5-15-10-core22-sdk"
                     "/current/usr/share:/usr/share${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+                )
+            },
+            {
+                "XDG_CONFIG_HOME": (
+                    "$CRAFT_STAGE/etc/xdg:/snap/kf5-5-108-qt-5-15-10-core22-sdk/current/etc/xdg"
+                    "/etc/xdg"
                 )
             },
             {
                 "SNAPCRAFT_CMAKE_ARGS": (
                     "-DCMAKE_FIND_ROOT_PATH="
-                    "/snap/kf5-5-105-qt-5-15-9-core22-sdk/current"
+                    "/snap/kf5-5-108-qt-5-15-10-core22-sdk/current"
                     "${SNAPCRAFT_CMAKE_ARGS:+:$SNAPCRAFT_CMAKE_ARGS}"
                 )
             },
@@ -225,8 +237,8 @@ def test_get_parts_snippet(kde_neon_extension):
         "kde-neon/sdk": {
             "source": str(source),
             "plugin": "make",
-            "make-parameters": ["PLATFORM_PLUG=kf5-5-105-qt-5-15-9-core22"],
-            "build-snaps": ["kf5-5-105-qt-5-15-9-core22-sdk"],
+            "make-parameters": ["PLATFORM_PLUG=kf5-5-108-qt-5-15-10-core22"],
+            "build-snaps": ["kf5-5-108-qt-5-15-10-core22-sdk"],
         }
     }
 
@@ -238,7 +250,7 @@ def test_get_parts_snippet_with_external_sdk(kde_neon_extension_with_build_snap)
         "kde-neon/sdk": {
             "source": str(source),
             "plugin": "make",
-            "make-parameters": ["PLATFORM_PLUG=kf5-5-105-qt-5-15-9-core22"],
+            "make-parameters": ["PLATFORM_PLUG=kf5-5-108-qt-5-15-10-core22"],
         }
     }
 
@@ -253,7 +265,7 @@ def test_get_parts_snippet_with_external_sdk_different_channel(
             "kde-neon/sdk": {
                 "source": str(source),
                 "plugin": "make",
-                "make-parameters": ["PLATFORM_PLUG=kf5-5-105-qt-5-15-9-core22"],
+                "make-parameters": ["PLATFORM_PLUG=kf5-5-108-qt-5-15-10-core22"],
             }
         }
     )
