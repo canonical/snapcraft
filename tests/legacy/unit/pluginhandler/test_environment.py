d53501a26ea1ab379bc6d555d9603fc3c4e37867
Callahan Kovacs <callahan.kovacs@canonical.com>
2023-08-15 09:05:29 -0500

    tests: add another test, mock platform
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/legacy/unit/pluginhandler/test_environment.py
+++ tests/legacy/unit/pluginhandler/test_environment.py
@@ -14,11 +14,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
+import pytest
+
 from snapcraft_legacy.internal.pluginhandler._part_environment import (
     get_snapcraft_global_environment,
 )
 from snapcraft_legacy.project import Project
 from tests.legacy.fixture_setup import SnapcraftYaml
+
+
+@pytest.fixture(autouse=True)
+def mock_platform(mocker):
+    mocker.patch.object(sys, "platform", "linux")
+    mocker.patch("platform.machine", return_value="aarch64")
 
 
 def test_no_build_for_arch(tmp_path):
@@ -28,6 +38,30 @@ def test_no_build_for_arch(tmp_path):
         base="core20",
         parts={"test-part": {"plugin": "nil"}},
         architectures=[{"build-on": ["amd64", "arm64"], "run-on": "amd64"}],
+    )
+    snapcraft_yaml.write_snapcraft_yaml()
+    project = Project(snapcraft_yaml_file_path=snapcraft_yaml.snapcraft_yaml_file_path)
+
+    environment = get_snapcraft_global_environment(project)
+
+    assert environment["SNAPCRAFT_ARCH_BUILD_ON"] == project.arch_build_on
+    assert (
+        environment["SNAPCRAFT_ARCH_TRIPLET_BUILD_ON"] == project.arch_triplet_build_on
+    )
+    assert environment["SNAPCRAFT_ARCH_BUILD_FOR"] == project.arch_build_for
+    assert (
+        environment["SNAPCRAFT_ARCH_TRIPLET_BUILD_FOR"]
+        == project.arch_triplet_build_for
+    )
+
+
+def test_implicit_build_for_arch(tmp_path):
+    """build-for envvars should exist for when the build-for arch is implicit."""
+    snapcraft_yaml = SnapcraftYaml(
+        tmp_path,
+        base="core20",
+        parts={"test-part": {"plugin": "nil"}},
+        architectures=[{"build-on": "arm64"}],
     )
     snapcraft_yaml.write_snapcraft_yaml()
     project = Project(snapcraft_yaml_file_path=snapcraft_yaml.snapcraft_yaml_file_path)
