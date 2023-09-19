ae891d1ddcb4de2128b64bec7a05fb3fd7b67954
Callahan <callahan.kovacs@canonical.com>
2023-09-19 10:08:22 -0500

    fix(environment): sort core20 project variables before replacement (#4366)
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/legacy/unit/project_loader/test_config.py
+++ tests/legacy/unit/project_loader/test_config.py
@@ -129,6 +129,50 @@ class VariableExpansionTest(LoadPartBaseTest):
             },
         )
 
+    def test_replace_snapcraft_variables_order(self):
+        """The largest replacements should occur first.
+
+        $SNAPCRAFT_ARCH_TRIPLET_BUILD_{ON|FOR} should be replaced before
+        $SNAPCRAFT_ARCH_TRIPLET.
+        """
+        project_config = self.make_snapcraft_project(
+            dedent(
+                """\
+            name: project-name
+            base: core20
+            version: "1"
+            summary: test
+            description: test
+            confinement: strict
+            environment:
+              SNAPCRAFT_ARCH_TRIPLET: ${SNAPCRAFT_ARCH_TRIPLET}
+              SNAPCRAFT_ARCH_BUILD_FOR: ${SNAPCRAFT_ARCH_BUILD_FOR}
+              SNAPCRAFT_ARCH_TRIPLET_BUILD_FOR: ${SNAPCRAFT_ARCH_TRIPLET_BUILD_FOR}
+              SNAPCRAFT_ARCH_BUILD_ON: ${SNAPCRAFT_ARCH_BUILD_ON}
+              SNAPCRAFT_ARCH_TRIPLET_BUILD_ON: ${SNAPCRAFT_ARCH_TRIPLET_BUILD_ON}
+              SNAPCRAFT_TARGET_ARCH: ${SNAPCRAFT_TARGET_ARCH}
+
+            parts:
+              test:
+                plugin: nil
+            """
+            )
+        )
+
+        self.assertThat(
+            project_config.data["environment"],
+            Equals(
+                {
+                    "SNAPCRAFT_ARCH_BUILD_FOR": "amd64",
+                    "SNAPCRAFT_ARCH_BUILD_ON": "amd64",
+                    "SNAPCRAFT_ARCH_TRIPLET": "x86_64-linux-gnu",
+                    "SNAPCRAFT_ARCH_TRIPLET_BUILD_FOR": "x86_64-linux-gnu",
+                    "SNAPCRAFT_ARCH_TRIPLET_BUILD_ON": "x86_64-linux-gnu",
+                    "SNAPCRAFT_TARGET_ARCH": "amd64",
+                }
+            ),
+        )
+
 
 class DependenciesTest(ProjectLoaderBaseTest):
     def setUp(self):
