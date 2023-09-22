aa64666ef1916d23495efb62b9a1df4dcdcae32f
Sergio Schvezov <sergio.schvezov@canonical.com>
2023-09-22 13:21:30 -0300

    docs: update sphinx theme (#4367)
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>
    Co-authored-by: David Boddie <david.boddie@canonical.com>
    Co-authored-by: Callahan Kovacs <callahan.kovacs@canonical.com>
    Co-authored-by: Alex Lowe <alex.lowe@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,47 +21,145 @@ author = "Canonical Group Ltd"
 copyright = "%s, %s" % (datetime.date.today().year, author)
 release = snapcraft.__version__
 
+# Open Graph configuration - defines what is displayed in the website preview
+ogp_site_url = "https://canonical-snapcraft.readthedocs-hosted.com"
+ogp_site_name = project
+ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
+
+# Update with the favicon for your product
+html_favicon = "sphinx-resources/.sphinx/_static/favicon.png"
+
+html_context = {
+    "discourse": "https://forum.snapcraft.io",
+    "github_url": "https://github.com/snapcore/snapcraft",
+    "github_version": "main",
+    "github_folder": "/docs/",
+    "github_issues": "enabled",
+}
+
+# Used for related links - no need to change
+if "discourse" in html_context:
+    html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
     "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinx_reredirects",
+    "youtube-links",
+    "related-links",
+    "custom-rst-roles",
+    "terminal-output",
     "sphinx_copybutton",
-    "sphinx-pydantic",
     "sphinx.ext.autodoc",  # Must be loaded after more_autodoc
+    "sphinxext.opengraph",
+    "myst_parser",
 ]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".sphinx"]
+myst_enable_extensions = ["substitution", "deflist"]
 
-show_authors = False
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "sphinx-resources"]
 
 rst_epilog = """
 .. include:: /reuse/links.txt
 """
 
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
 # Links to ignore when checking links
 linkcheck_ignore = ["http://127.0.0.1:8000"]
 
-html_theme = "furo"
-html_static_path = ["_static"]
-html_css_files = [
-    "css/custom.css",
-]
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
+# Find the current builder
+builder = "dirhtml"
+if "-b" in sys.argv:
+    builder = sys.argv[sys.argv.index("-b") + 1]
+
+# Setting templates_path for epub makes the build fail
+if builder == "dirhtml" or builder == "html":
+    templates_path = ["sphinx-resources/.sphinx/_templates"]
+
+html_theme = "furo"
+html_last_updated_fmt = ""
+html_permalinks_icon = "¶"
+html_theme_options = {
+    "light_css_variables": {
+        "color-sidebar-background-border": "none",
+        "font-stack": "Ubuntu, -apple-system, Segoe UI, Roboto, Oxygen, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+        "font-stack--monospace": "Ubuntu Mono, Consolas, Monaco, Courier, monospace",
+        "color-foreground-primary": "#111",
+        "color-foreground-secondary": "var(--color-foreground-primary)",
+        "color-foreground-muted": "#333",
+        "color-background-secondary": "#FFF",
+        "color-background-hover": "#f2f2f2",
+        "color-brand-primary": "#111",
+        "color-brand-content": "#06C",
+        "color-api-background": "#cdcdcd",
+        "color-inline-code-background": "rgba(0,0,0,.03)",
+        "color-sidebar-link-text": "#111",
+        "color-sidebar-item-background--current": "#ebebeb",
+        "color-sidebar-item-background--hover": "#f2f2f2",
+        "toc-font-size": "var(--font-size--small)",
+        "color-admonition-title-background--note": "var(--color-background-primary)",
+        "color-admonition-title-background--tip": "var(--color-background-primary)",
+        "color-admonition-title-background--important": "var(--color-background-primary)",
+        "color-admonition-title-background--caution": "var(--color-background-primary)",
+        "color-admonition-title--note": "#24598F",
+        "color-admonition-title--tip": "#24598F",
+        "color-admonition-title--important": "#C7162B",
+        "color-admonition-title--caution": "#F99B11",
+        "color-highlighted-background": "#EbEbEb",
+        "color-link-underline": "var(--color-background-primary)",
+        "color-link-underline--hover": "var(--color-background-primary)",
+        "color-version-popup": "#772953",
+    },
+    "dark_css_variables": {
+        "color-foreground-secondary": "var(--color-foreground-primary)",
+        "color-foreground-muted": "#CDCDCD",
+        "color-background-secondary": "var(--color-background-primary)",
+        "color-background-hover": "#666",
+        "color-brand-primary": "#fff",
+        "color-brand-content": "#06C",
+        "color-sidebar-link-text": "#f7f7f7",
+        "color-sidebar-item-background--current": "#666",
+        "color-sidebar-item-background--hover": "#333",
+        "color-admonition-background": "transparent",
+        "color-admonition-title-background--note": "var(--color-background-primary)",
+        "color-admonition-title-background--tip": "var(--color-background-primary)",
+        "color-admonition-title-background--important": "var(--color-background-primary)",
+        "color-admonition-title-background--caution": "var(--color-background-primary)",
+        "color-admonition-title--note": "#24598F",
+        "color-admonition-title--tip": "#24598F",
+        "color-admonition-title--important": "#C7162B",
+        "color-admonition-title--caution": "#F99B11",
+        "color-highlighted-background": "#666",
+        "color-link-underline": "var(--color-background-primary)",
+        "color-link-underline--hover": "var(--color-background-primary)",
+        "color-version-popup": "#F29879",
+    },
 }
 
-# Type hints configuration
-set_type_checking_flag = True
-typehints_fully_qualified = False
-always_document_param_types = True
+html_static_path = ["sphinx-resources/.sphinx/_static"]
+html_css_files = [
+    "custom.css",
+    "github_issue_links.css",
+]
 
-# Github config
-github_username = "snapcore"
-github_repository = "snapcraft"
+html_js_files = []
+if "github_issues" in html_context and html_context["github_issues"]:
+    html_js_files.append("github_issue_links.js")
+
+
+# Set up redirects (https://documatt.gitlab.io/sphinx-reredirects/usage.html)
+# For example: "explanation/old-name.html": "../how-to/prettify.html",
+# redirects = {}
 
 
 def generate_cli_docs(nil):
