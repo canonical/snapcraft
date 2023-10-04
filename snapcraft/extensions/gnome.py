90c96abf897df21261011f48ce6dc8acb26f65c3
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-10-04 19:42:31 +0530

    added layout for webkit2gtk-4.1 and changed all `SNAPCRAFT`s to `CRAFT`

--- snapcraft/extensions/gnome.py
+++ snapcraft/extensions/gnome.py
@@ -156,8 +156,11 @@ class GNOME(Extension):
                 }
             },
             "layout": {
-                "/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/webkit2gtk-4.0": {
-                    "bind": "$SNAP/gnome-platform/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/webkit2gtk-4.0"
+                "/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.0": {
+                    "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.0"
+                },
+                "/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.1": {
+                    "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.1"    
                 },
                 "/usr/share/xml/iso-codes": {
                     "bind": "$SNAP/gnome-platform/usr/share/xml/iso-codes"
