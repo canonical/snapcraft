ee517b54c37178b3fd54a45e2c658f2a2016f2b5
Ondrej Kubik <ondrej.kubik@canonical.com>
2023-11-09 15:44:34 +0000

    feat(plugins): kernel: removing obsolete plugin options
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- snapcraft_legacy/plugins/v2/kernel.py
+++ snapcraft_legacy/plugins/v2/kernel.py
@@ -20,23 +20,18 @@
 The following kernel-specific options are provided by this plugin:
 
     - kernel-kdefconfig:
-      (list of kdefconfigs)
+      (list of kdefconfigs, default: none))
       defconfig target to use as the base configuration. default: "defconfig"
 
-    - kernel-kconfigfile:
-      (filepath; default: none)
-      path to file to use as base configuration. If provided this option wins
-      over everything else. default: None
-
     - kernel-kconfigflavour:
-      (string; default: none)
+      (string; default: generic)
       Ubuntu config flavour to use as base configuration. If provided this
       option wins over kernel-kdefconfig. default: None
 
     - kernel-kconfigs:
       (list of strings; default: none)
       explicit list of configs to force; this will override the configs that
-      were set as base through kernel-kdefconfig and kernel-kconfigfile and dependent configs
+      were set as base through kernel-kdefconfig and dependent configs
       will be fixed using the defaults encoded in the kbuild config
       definitions.  If you don't want default for one or more implicit configs
       coming out of these, just add them to this list as well.
@@ -50,24 +45,6 @@ The following kernel-specific options are provided by this plugin:
       yaml object can be used. This yaml object would be a map of
       debian architecture and kernel image build targets.
 
-    - kernel-with-firmware:
-      (boolean; default: True)
-      use this flag to disable shipping binary firmwares.
-
-    - kernel-device-trees:
-      (array of string; default: none)
-      list of device trees to build, the format is <device-tree-name>.dts.
-
-    - kernel-compiler
-      (string; default: none)
-      Optional, define compiler to use, by default gcc compiler is used.
-      Other permitted compilers: clang
-
-    - kernel-compiler-parameters
-      (array of string)
-      Optional, define extra compiler parameters to be passed to the compiler.
-      Default value is empty.
-
     - kernel-enable-zfs-support
       (boolean; default: False)
       use this flag to build in zfs support through extra ko modules
@@ -76,11 +53,6 @@ The following kernel-specific options are provided by this plugin:
       (boolean; default: False)
       use this flag to build the perf binary
 
-    - kernel-use-llvm
-      (boolean or string to specify version suffix; default: False)
-      Use the LLVM substitutes for the GNU binutils utilities. Set this to a
-      string (e.g. "-12") to use a specific version of the LLVM utilities.
-
 This plugin support cross compilation, for which plugin expects
 the build-environment is configured accordingly and has foreign architectures
 setup accordingly.
@@ -88,9 +60,8 @@ setup accordingly.
 
 import logging
 import os
-import re
 import sys
-from typing import Any, Dict, List, Optional, Set
+from typing import Any, Dict, List, Set
 
 from overrides import overrides
 
@@ -103,9 +74,8 @@ logger = logging.getLogger(__name__)
 
 _default_kernel_image_target = {
     "amd64": "bzImage",
-    "i386": "bzImage",
     "armhf": "zImage",
-    "arm64": "Image.gz",
+    "arm64": "Image",
     "powerpc": "uImage",
     "ppc64el": "vmlinux.strip",
     "s390x": "bzImage",
@@ -121,9 +91,8 @@ class KernelPlugin(PluginV2):
             "type": "object",
             "additionalProperties": False,
             "properties": {
-                "kernel-kdefconfig": {"type": "array", "default": ["defconfig"]},
-                "kernel-kconfigfile": {"type": "string", "default": None},
-                "kernel-kconfigflavour": {"type": "string", "default": ""},
+                "kernel-kdefconfig": {"type": "array", "default": []},
+                "kernel-kconfigflavour": {"type": "string", "default": "defconfig"},
                 "kernel-kconfigs": {
                     "type": "array",
                     "minitems": 1,
@@ -135,38 +104,12 @@ class KernelPlugin(PluginV2):
                     "oneOf": [{"type": "string"}, {"type": "object"}],
                     "default": "",
                 },
-                "kernel-with-firmware": {
-                    "type": "boolean",
-                    "default": True,
-                },
-                "kernel-device-trees": {
-                    "type": "array",
-                    "minitems": 1,
-                    "uniqueItems": True,
-                    "items": {"type": "string"},
-                    "default": [],
-                },
-                "kernel-compiler": {
-                    "type": "string",
-                    "default": "",
-                },
-                "kernel-compiler-parameters": {
-                    "type": "array",
-                    "minitems": 1,
-                    "uniqueItems": True,
-                    "items": {"type": "string"},
-                    "default": [],
-                },
                 "kernel-enable-zfs-support": {
                     "type": "boolean",
                     "default": False,
                 },
                 "kernel-enable-perf": {
                     "type": "boolean",
-                    "default": False,
-                },
-                "kernel-use-llvm": {
-                    "oneOf": [{"type": "boolean"}, {"type": "string"}],
                     "default": False,
                 },
             },
@@ -187,23 +130,6 @@ class KernelPlugin(PluginV2):
         self._cross_building = False
         if host_arch != self._target_arch:
             self._cross_building = True
-        self._llvm_version = self._determine_llvm_version()
-
-    def _determine_llvm_version(self) -> Optional[str]:
-        if (
-            isinstance(self.options.kernel_use_llvm, bool)
-            and self.options.kernel_use_llvm
-        ):
-            return "1"
-        if isinstance(self.options.kernel_use_llvm, str):
-            suffix = re.match(r"^-\d+$", self.options.kernel_use_llvm)
-            if suffix is None:
-                raise ValueError(
-                    f'kernel-use-llvm must match the format "-<version>" (e.g. "-12"), not "{self.options.kernel_use_llvm}"'
-                )
-            return self.options.kernel_use_llvm
-        # Not use LLVM utilities
-        return None
 
     def _init_build_env(self) -> None:
         # first get all the architectures, new v2 plugin is making life difficult
@@ -217,7 +143,6 @@ class KernelPlugin(PluginV2):
 
         self._check_cross_compilation()
         self._set_kernel_targets()
-        self._set_llvm()
 
     def _check_cross_compilation(self) -> None:
         if self._cross_building:
@@ -238,42 +163,11 @@ class KernelPlugin(PluginV2):
             "INSTALL_MOD_STRIP=1",
             "INSTALL_MOD_PATH=${SNAPCRAFT_PART_INSTALL}",
         ]
-        if self.options.kernel_device_trees:
-            self.dtbs = [f"{i}.dtb" for i in self.options.kernel_device_trees]
-            if self.dtbs:
-                self._make_targets.extend(self.dtbs)
-        elif self._kernel_arch in ("arm", "arm64", "riscv64"):
+        if self._kernel_arch in ("arm", "arm64", "riscv64"):
             self._make_targets.append("dtbs")
             self._make_install_targets.extend(
                 ["dtbs_install", "INSTALL_DTBS_PATH=${SNAPCRAFT_PART_INSTALL}/dtbs"]
             )
-        self._make_install_targets.extend(self._get_fw_install_targets())
-
-    def _set_llvm(self) -> None:
-        if self._llvm_version is not None:
-            self._make_cmd.append(f'LLVM="{self._llvm_version}"')
-
-    def _get_fw_install_targets(self) -> List[str]:
-        if not self.options.kernel_with_firmware:
-            return []
-
-        return [
-            "firmware_install",
-            "INSTALL_FW_PATH=${SNAPCRAFT_PART_INSTALL}/lib/firmware",
-        ]
-
-    def _configure_compiler(self) -> None:
-        # check if we are using gcc or another compiler
-        if self.options.kernel_compiler:
-            # at the moment only clang is supported as alternative, warn otherwise
-            kernel_compiler = re.match(r"^clang(-\d+)?$", self.options.kernel_compiler)
-            if kernel_compiler is None:
-                logger.warning("Only other 'supported' compiler is clang")
-                logger.info("hopefully you know what you are doing")
-            self._make_cmd.append(f'CC="{self.options.kernel_compiler}"')
-        if self.options.kernel_compiler_parameters:
-            for opt in self.options.kernel_compiler_parameters:
-                self._make_cmd.append(str(opt))
 
     @overrides
     def get_build_snaps(self) -> Set[str]:
@@ -307,16 +201,6 @@ class KernelPlugin(PluginV2):
         if self.options.kernel_enable_zfs_support and self._cross_building:
             build_packages |= {f"libc6-dev:{self._target_arch}"}
 
-        if self._llvm_version is not None:
-            # Use the specified version suffix for the packages if it has been
-            # set by the user
-            suffix = self._llvm_version if self._llvm_version != "1" else ""
-            llvm_packages = [
-                "llvm",
-                "lld",
-            ]
-            build_packages |= {f"{f}{suffix}" for f in llvm_packages}
-
         return build_packages
 
     @overrides
@@ -335,25 +219,21 @@ class KernelPlugin(PluginV2):
     @overrides
     def get_build_commands(self) -> List[str]:
         logger.info("Getting build commands...")
-        self._configure_compiler()
         return _kernel_build.get_build_commands(
             make_cmd=self._make_cmd.copy(),
             make_targets=self._make_targets,
             make_install_targets=self._make_install_targets,
             target_arch=self._target_arch,
             target_arch_triplet="${SNAPCRAFT_ARCH_TRIPLET_BUILD_FOR}",
-            config_file=self.options.kernel_kconfigfile,
             config_flavour=self.options.kernel_kconfigflavour,
             defconfig=self.options.kernel_kdefconfig,
             configs=self.options.kernel_kconfigs,
-            device_trees=self.options.kernel_device_trees,
             enable_zfs_support=self.options.kernel_enable_zfs_support,
             enable_perf=self.options.kernel_enable_perf,
             project_dir="${SNAPCRAFT_PROJECT_DIR}",
             source_dir="${SNAPCRAFT_PART_SRC}",
             build_dir="${SNAPCRAFT_PART_BUILD}",
             install_dir="${SNAPCRAFT_PART_INSTALL}",
-            stage_dir="${SNAPCRAFT_STAGE}",
         )
 
     @property
