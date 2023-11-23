12b6dbf6af9b4d047a7283bd1431b4f6a0104e8d
সৌম্যদীপ ঘোষ(सौम्यदीपः घोषः) <72045785+soumyaDghosh@users.noreply.github.com>
2023-11-23 19:20:56 +0530

    u
    Co-authored-by: Alex Lowe <alex.lowe@canonical.com>

--- tests/unit/extensions/test_gnome.py
+++ tests/unit/extensions/test_gnome.py
@@ -144,11 +144,11 @@ def test_get_root_snippet_with_external_sdk(gnome_extension_with_build_snap):
             }
         },
         "layout": {
-            "/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.0": {
-                "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.0"
+            "/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/webkit2gtk-4.0": {
+                "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/webkit2gtk-4.0"
             },
-            "/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.1": {
-                "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET/webkit2gtk-4.1"
+            "/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/webkit2gtk-4.1": {
+                "bind": "$SNAP/gnome-platform/usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/webkit2gtk-4.1"
             },
             "/usr/share/xml/iso-codes": {
                 "bind": "$SNAP/gnome-platform/usr/share/xml/iso-codes"
