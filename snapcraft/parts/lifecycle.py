ffcd7c6705c4f580f25f628f8d9b0323d6e3f885
Mengyi <mengyi.wang@canonical.com>
2024-03-07 13:54:39 +0100

    Stage plugin env script, and flag plugin as experimental

--- snapcraft/parts/lifecycle.py
+++ snapcraft/parts/lifecycle.py
@@ -50,7 +50,7 @@ if TYPE_CHECKING:
     import argparse
 
 
-_EXPERIMENTAL_PLUGINS = ["kernel"]
+_EXPERIMENTAL_PLUGINS = ["kernel, matter-sdk"]
 
 
 def run(command_name: str, parsed_args: "argparse.Namespace") -> None:
