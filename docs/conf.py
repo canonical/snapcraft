cd48e13fb1f331dfd494a9b749bfadb8f75028e6
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-04-25 20:44:38 -0300

    docs: use canonical-sphinx (#209)

--- docs/conf.py
+++ docs/conf.py
@@ -1,47 +1,59 @@
-# Configuration file for the Sphinx documentation builder.
+# This file is part of starbase.
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
 
-project = "starcraft"
-copyright = "2023, Canonical"
+project = "Starbase"
 author = "Canonical"
+
+copyright = "2023-%s, %s" % (datetime.date.today().year, author)
+
+# region Configuration for canonical-sphinx
+ogp_site_url = "https://canonical-starbase.readthedocs-hosted.com/"
+ogp_site_name = project
+ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
+
+html_context = {
+    "product_page": "github.com/canonical/starbase",
+    "github_url": "https://github.com/canonical/starbase",
+}
+
+extensions = [
+    "canonical_sphinx",
+]
+# endregion
 
 # region General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
-    "sphinx_design",
-    "sphinx_copybutton",
-    "sphinx-pydantic",
-    "sphinx_toolbox",
-    "sphinx_toolbox.more_autodoc",
-    "sphinx.ext.autodoc",  # Must be loaded after more_autodoc
-]
-
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-show_authors = False
+extensions.extend(
+    [
+        "sphinx.ext.intersphinx",
+        "sphinx.ext.viewcode",
+        "sphinx.ext.coverage",
+        "sphinx.ext.doctest",
+        "sphinx-pydantic",
+        "sphinx_toolbox",
+        "sphinx_toolbox.more_autodoc",
+        "sphinx.ext.autodoc",  # Must be loaded after more_autodoc
+    ]
+)
 
 # endregion
-# region Options for HTML output
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-html_static_path = ["_static"]
-html_css_files = [
-    "css/custom.css",
-]
-
-# endregion
 # region Options for extensions
 # Intersphinx extension
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
@@ -57,6 +69,6 @@ always_document_param_types = True
 
 # Github config
 github_username = "canonical"
-github_repository = "starcraft-base"
+github_repository = "starbase"
 
 # endregion
