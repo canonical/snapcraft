2ef86ae5a55316f7a4d918c4330e13e9f392ab1e
Michał Sawicz <michal@sawicz.net>
2024-04-26 10:47:03 +0000

    gnome: fix lint more

--- tests/unit/extensions/test_gnome.py
+++ tests/unit/extensions/test_gnome.py
@@ -97,7 +97,7 @@ def test_get_app_snippet_core24(gnome_extension_core24):
     assert gnome_extension_core24.get_app_snippet() == {
         "command-chain": [
             "snap/command-chain/gpu-2404-wrapper",
-            "snap/command-chain/desktop-launch"
+            "snap/command-chain/desktop-launch",
         ],
         "plugs": ["desktop", "desktop-legacy", "gsettings", "opengl", "wayland", "x11"],
     }
