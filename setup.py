e1dcb33b25cd4cb1223e35a2fb3816d111e52d9d
Callahan Kovacs <callahan.kovacs@canonical.com>
2024-06-21 13:51:59 -0500

    build(deps): bump craft-parts to 1.30.1
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ install_requires = [
     "craft-archives",
     "craft-cli",
     "craft-grammar",
-    "craft-parts",
+    "craft-parts>=1.30.1",
     "craft-providers",
     "craft-store",
     "docutils<0.20",  # Frozen until we can update sphinx dependencies.
