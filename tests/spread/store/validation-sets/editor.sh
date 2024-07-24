88c9e62e5ff99e768475ffa572ceb04fe1d45ce8
Callahan Kovacs <callahan.kovacs@canonical.com>
2024-07-24 11:14:25 -0500

    chore: debugging
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/spread/store/validation-sets/editor.sh
+++ tests/spread/store/validation-sets/editor.sh
@@ -3,21 +3,22 @@
 validation_set_file="$1"
 
 # flip-flop between two valid revisions: 100 and 101
-if grep -q "^  revision:.*100" "$validation_set_file"; then
-  (( revision=101 ))
-else
-  (( revision=100 ))
-fi
+#if grep -q "^  revision:.*100" "$validation_set_file"; then
+#  (( revision=101 ))
+#else
+#  (( revision=100 ))
+#fi
 
 # capture core22 id from the staging store
-cat "$validation_set_file" > debug.txt
+cat "$validation_set_file" > debug-before.txt
 
 cat << EOF > "$validation_set_file"
 account-id: pv8nW1ZaULF7xXAkE3tiU3TdlOnYlGUr
 name: testset
 sequence: 1
 snaps:
-- name: core22
+- name: test-snapcraft-assertions
   presence: required
-  revision: $revision
 EOF
+
+cat "$validation_set_file" > debug-after.txt
