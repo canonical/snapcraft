ab9589e323939c6025a9637c5bb2bfca1ac21ff4
Callahan Kovacs <callahan.kovacs@canonical.com>
2024-09-27 08:50:25 -0500

    debugging
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/spread/store/validation-sets/editor.sh
+++ tests/spread/store/validation-sets/editor.sh
@@ -2,6 +2,8 @@
 
 validation_set_file="$1"
 
+cat "$validation_set_file"
+
 # flip-flop between two valid revisions of `test-snapcraft-assertions` in the staging store: 1 and 2
 if grep -q "^  revision:.*1" "$validation_set_file"; then
   (( revision=2 ))
