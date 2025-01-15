faf47c5643b553eab59165917f7d7b18f52c24f5
Imani Pelton <imani@bepri.dev>
2025-01-15 08:24:55 -0500

    chore: update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# Copyright 2023-2024 Canonical Ltd.
+# Copyright 2023-2025 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
