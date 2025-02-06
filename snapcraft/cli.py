2b86555bac5686cb36879b23c473ed1279bf2657
Callahan Kovacs <callahan.kovacs@canonical.com>
2025-02-06 13:09:02 -0600

    chore(revert): unregister registries commands
    This reverts commit 385b4039add8b63288c33de6bcabe8a348fffd32 (#5195)

--- snapcraft/cli.py
+++ snapcraft/cli.py
@@ -141,6 +141,13 @@ COMMAND_GROUPS = [
         ],
     ),
     craft_cli.CommandGroup(
+        "Store Registries",
+        [
+            commands.StoreEditRegistriesCommand,
+            commands.StoreListRegistriesCommand,
+        ],
+    ),
+    craft_cli.CommandGroup(
         "Other",
         [
             commands.LintCommand,
