19e62279e464149154bc6dc8df59ddbc0ac6c487
Imani Pelton <imani.pelton@canonical.com>
2025-02-12 13:41:31 -0500

    test: clean up tests

--- tests/unit/models/test_projects.py
+++ tests/unit/models/test_projects.py
@@ -2561,7 +2561,7 @@ def test_project_platform_mismatch():
 
 def test_project_platform_unknown_name():
     """Raise an error if an empty platform is not a valid architecture."""
-    with pytest.raises(CraftValidationError) as raised:
+    with pytest.raises(pydantic.ValidationError) as raised:
         snapcraft.models.project.SnapcraftBuildPlanner.model_validate(
             {
                 "name": "test-snap",
@@ -2573,7 +2573,7 @@ def test_project_platform_unknown_name():
         )
 
     assert (
-        "Invalid architecture: 'unknown' must be a valid debian architecture."
+        "'unknown' is not a valid Debian architecture."
         in str(raised.value)
     )
 
