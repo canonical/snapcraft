4fc5bd3cd8636bd7169aeb74b25acc49d09351c4
Ondrej Kubik <ondrej.kubik@canonical.com>
2025-05-28 21:44:22 +0100

    feat(plugins): initrd: silence the install of the firmware and kernel modules
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- tests/legacy/unit/plugins/v2/test_initrd.py
+++ tests/legacy/unit/plugins/v2/test_initrd.py
@@ -487,10 +487,10 @@ _link_files_fnc = [
         """
         # link files helper, accept wild cards
         # 1: reference dir, 2: file(s) including wild cards, 3: dst dir
-        # 4: quiet mode [ "-quiet" ] (optional)
+        # 4: quiet mode [ "--quiet" ] (optional)
         link_files() {
             set +x
-            link_files_impl "${@}"
+            link_files_impl ${@}
             local retVal=$?
             set -x
             return ${retVal}
@@ -498,7 +498,7 @@ _link_files_fnc = [
 
         # link files helper implementation, accept wild cards
         # 1: reference dir, 2: file(s) including wild cards, 3: dst dir
-        # 4: quiet mode [ "-quiet" ] (optional)
+        # 4: quiet mode [ "--quiet" ] (optional)
         link_files_impl() {
             if [ -z "${2}" ]; then
                 return 0
@@ -540,7 +540,7 @@ _link_files_fnc = [
                     fi
                     dir_path=$(dirname "${rel_path}")
                     mkdir -p "${3}/${dir_path}"
-                    [ "${quiet}" != "-quiet" ] && echo "installing ${f} to ${3}/${dir_path}"
+                    [ "${quiet}" != "--quiet" ] && echo "installing ${f} to ${3}/${dir_path}"
                     ln -f "${f}" "${3}/${dir_path}"
                 fi
                 found="yes"
@@ -770,7 +770,7 @@ _setup_initrd_chroot_fnc = [
 
             if [ ! -e "${work_dir}/.${UC_INITRD_ROOT_NAME}.firmware" ]; then
                 rm -rf "${UC_INITRD_ROOT}"/usr/lib/firmware/*
-                link_files "${KERNEL_FIRMWARE}" "*" "${UC_INITRD_ROOT}/usr/lib/firmware"
+                link_files "${KERNEL_FIRMWARE}" "*" "${UC_INITRD_ROOT}/usr/lib/firmware" "--quiet"
                 touch "${work_dir}/.${UC_INITRD_ROOT_NAME}.firmware"
             fi
 
@@ -781,7 +781,7 @@ _setup_initrd_chroot_fnc = [
 
             if [ ! -e "${work_dir}/.${UC_INITRD_ROOT_NAME}.modules" ]; then
                 rm -rf "${UC_INITRD_ROOT}"/usr/lib/modules/*
-                link_files "${KERNEL_MODULES}" "*" "${UC_INITRD_ROOT}/usr/lib/modules"
+                link_files "${KERNEL_MODULES}" "*" "${UC_INITRD_ROOT}/usr/lib/modules" "--quiet"
                 # remove potentially dangling source link
                 rm -rf ${UC_INITRD_ROOT}/usr/lib/modules/*/build
                 touch "${work_dir}/.${UC_INITRD_ROOT_NAME}.modules"
