26d7ce9a895eca300c3a3fb81d83296ef09a5304
Michael DuBelko <michael.dubelko@canonical.com>
2025-07-30 16:30:46 -0700

    docs: announce legacy status

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,10 @@ html_context = {
 # Target repository for the edit button on pages
 html_theme_options = {
     "source_edit_link": "https://github.com/canonical/snapcraft",
+    "announcement": "<strong>This is legacy documentation for Snapcraft 7</strong>. For up-to-date features and documentation, consider migrating to <a href='https://documentation.ubuntu.com/snapcraft'>Snapcraft 8</a>.",
 }
+
+html_static_path = ["_static"]
 
 extensions = [
     "canonical_sphinx",
@@ -80,6 +83,10 @@ extensions.extend(
 
 exclude_patterns = [
     "sphinx-resources",
+]
+
+html_css_files = [
+    "css/announcement.css",
 ]
 
 # region Options for extensions
