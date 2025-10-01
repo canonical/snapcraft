e2ef78fa2a7645b338a85341a61d0e0b398a9bbb
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-10-01 17:41:45 -0300

    fix: add copyright notices and correct .NET version in comments

--- snapcraft/extensions/dotnet10.py
+++ snapcraft/extensions/dotnet10.py
@@ -1,3 +1,11 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2025 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -14,7 +22,7 @@ from .dotnet_base import DotnetExtensionBase
 class Dotnet10Extension(DotnetExtensionBase):
     """
     An extension that eases the creation of snaps that integrate with
-    the .NET 8 content snaps.
+    the .NET 10 content snaps.
     """
 
     @property
--- snapcraft/extensions/dotnet9.py
+++ snapcraft/extensions/dotnet9.py
@@ -1,3 +1,11 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2025 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
