aec70395031e52b02dadebf7e86df26aae24b364
Michał Sawicz <michal@sawicz.net>
2026-04-07 16:56:56 +0200

    feat: core26 support for the GPU extension (#6083)
    Signed-off-by: Michał Sawicz <michal@sawicz.net>
    Signed-off-by: Imani Pelton <imani.pelton@canonical.com>
    Co-authored-by: Callahan Kovacs <callahan.kovacs@canonical.com>
    Co-authored-by: Imani Pelton <imani.pelton@canonical.com>

--- tests/spread/tools/snapcraft-yaml.sh
+++ tests/spread/tools/snapcraft-yaml.sh
@@ -2,7 +2,9 @@
 
 get_base()
 {
-    if [[ "$SPREAD_SYSTEM" =~ ubuntu-24.04 ]]; then
+    if [[ "$SPREAD_SYSTEM" =~ ubuntu-26.04 ]]; then
+        echo "core26"
+    elif [[ "$SPREAD_SYSTEM" =~ ubuntu-24.04 ]]; then
         echo "core24"
     elif [[ "$SPREAD_SYSTEM" =~ ubuntu-22.04 ]]; then
         echo "core22"
@@ -26,6 +28,16 @@ set_base()
     else
         # Insert at the very top to be safe
         sed -i "1ibase: $base"  "$snapcraft_yaml_path"
+    fi
+
+    # remove once core26 is stable (#6088)
+    if [ "$base" == "core26" ]; then
+        if grep -q "^build-base:" "$snapcraft_yaml_path"; then
+            sed -i "s/^build-base:.*/build-base: devel/g" "$snapcraft_yaml_path"
+        else
+            # Insert at the very top to be safe
+            sed -i "/^base:.*/a\build-base: devel"  "$snapcraft_yaml_path"
+        fi
     fi
 }
 
