c4389868d0a519109f00da667d18a3b8aeb3452e
Florencia Cabral <florencia.a.cabral@gmail.com>
2026-04-15 01:02:24 -0300

    keep copyright notices as per Canonical policy

--- tests/spread/extensions/snaps/ros2-lyrical-hello/src/hello.cpp
+++ tests/spread/extensions/snaps/ros2-lyrical-hello/src/hello.cpp
@@ -1,4 +1,4 @@
-// Copyright 2026 Open Source Robotics Foundation, Inc.
+// Copyright 2016 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
