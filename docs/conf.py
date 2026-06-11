36f541cbfb36e5712fc8e685f26fd5381417db36
Michael DuBelko <michael.dubelko@canonical.com>
2026-06-11 12:50:33 -0700

    docs: configure the link checker to use the more modern timeout reporting

--- docs/conf.py
+++ docs/conf.py
@@ -139,6 +139,9 @@ linkcheck_ignore = [
 # give linkcheck multiple tries on failure
 linkcheck_retries = 20
 
+# Report timeouts as 'timeout' instead of 'broken'
+linkcheck_report_timeouts_as_broken = False
+
 
 ########################
 # Configuration extras #
