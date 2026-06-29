16512cfed18f5d07934b7700c6e1d0266a3968ce
Michael DuBelko <michael.dubelko@canonical.com>
2026-06-29 13:38:42 -0700

    docs: fix version numbering (#6313)
    Apply the patch from #6295.

--- docs/conf.py
+++ docs/conf.py
@@ -26,14 +26,14 @@ import snapcraft
 project = "Snapcraft"
 author = "Canonical Ltd."
 
-# Sidebar documentation title; best kept reasonably short
-# The full version, including alpha/beta/rc tags
-release = snapcraft.__version__
-if ".post" in release:
-    release = "dev"
-else:
-    major, minor, *_ = release.split(".")
+# Version string in sidebar
+if os.environ.get("READTHEDOCS_VERSION_TYPE", "external") == "external":  # PR or local build
+    # Because of setuptools-scm, we can safely assume the version starts with `n.n`
+    major, minor, *_ = snapcraft.__version__.split(".")
     release = f"{major}.{minor}"
+else:  # Branch build
+    rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+    release = "dev" if rtd_version == "latest" else rtd_version
 
 # Copyright string; shown at the bottom of the page
 copyright = "2015-%s, %s" % (datetime.date.today().year, author)
