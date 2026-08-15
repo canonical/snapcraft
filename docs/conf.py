9bde339cb42ad08a70438a976bfe26d4545bed48
jahn-junior <jj.coldiron@canonical.com>
2026-08-15 13:36:07 -0700

    docs: fix version in Snapcraft 7

--- docs/conf.py
+++ docs/conf.py
@@ -26,12 +26,7 @@ project = "Snapcraft"
 author = "Canonical Group Ltd"
 
 # Version string in sidebar
-if os.environ.get("READTHEDOCS_VERSION_TYPE", "external") == "external":  # PR or local build
-    # Because of Autotools, we can safely assume the version starts with `n.n`
-    major, minor, *_ = snapcraft.__version__.split(".")
-    release = f"{major}.{minor}"
-else:  # Branch build
-    release = os.environ.get("READTHEDOCS_VERSION", "latest")
+release = "7.5"
 
 copyright = "2015-%s, %s" % (datetime.date.today().year, author)
 
