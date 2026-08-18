7fb6ad2a78707ff70ad66de0e75e57ce8d962267
Callahan Kovacs <callahan.kovacs@canonical.com>
2026-08-18 07:21:51 -0500

    style: shfmt
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/spread/tools/prepare.sh
+++ tests/spread/tools/prepare.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 
-install_snapcraft()
-{
+install_snapcraft() {
   # If $SNAPCRAFT_CHANNEL is defined, install snapcraft from that channel.
   # Otherwise, look for it in /snapcraft/.
   if [ -z "$SNAPCRAFT_CHANNEL" ]; then
     if stat /snapcraft/tests/*.snap 2>/dev/null; then
       snap install --classic --dangerous /snapcraft/tests/*.snap
     else
-      echo "Expected a snap to exist in /snapcraft/tests/. If your intention"\
-           "was to install from the store, set \$SNAPCRAFT_CHANNEL."
+      echo "Expected a snap to exist in /snapcraft/tests/. If your intention" \
+        "was to install from the store, set \$SNAPCRAFT_CHANNEL."
       exit 1
     fi
   else
@@ -17,8 +16,7 @@ install_snapcraft()
   fi
 }
 
-install_snapcraft_as()
-{
+install_snapcraft_as() {
   # install snapcraft with a particular name
   # If $SNAPCRAFT_CHANNEL is defined, install snapcraft from that channel.
   # Otherwise, look for it in /snapcraft/.
@@ -26,8 +24,8 @@ install_snapcraft_as()
     if stat /snapcraft/tests/*.snap 2>/dev/null; then
       snap install --classic --dangerous /snapcraft/tests/*.snap --name "$1"
     else
-      echo "Expected a snap to exist in /snapcraft/tests/. If your intention"\
-           "was to install from the store, set \$SNAPCRAFT_CHANNEL."
+      echo "Expected a snap to exist in /snapcraft/tests/. If your intention" \
+        "was to install from the store, set \$SNAPCRAFT_CHANNEL."
       exit 1
     fi
   else
@@ -35,8 +33,7 @@ install_snapcraft_as()
   fi
 }
 
-install_lxd()
-{
+install_lxd() {
   # install and setup the lxd snap - use 'retry' to workaround aa-exec issue
   # see https://bugs.launchpad.net/snapd/+bug/1870201
   retry -n 5 --wait 5 sh -c 'snap install lxd'
