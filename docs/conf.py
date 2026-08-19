8205f6d013f0acf0a6cf481e8e1f7d57055152f0
Callahan Kovacs <callahan.kovacs@canonical.com>
2026-08-19 12:50:37 -0500

    chore: unregister gradle-use plugin
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -256,6 +256,7 @@ exclude_patterns = [
     "common/craft-parts/reference/parts_steps.rst",
     "common/craft-parts/reference/step_execution_environment.rst",
     "common/craft-parts/reference/step_output_directories.rst",
+    "common/craft-parts/reference/plugins/gradle_use_plugin.rst",
     "common/craft-parts/reference/plugins/maven_plugin.rst",
     "common/craft-parts/reference/plugins/poetry_plugin.rst",
     "common/craft-parts/reference/plugins/python_plugin.rst",
