3f9a0e02da5145f988b4bcecc17c2aa3a5dfd7d6
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2026-08-20 14:59:01 -0300

    chore: adjust icon return value
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- snapcraft/parts/desktop_file.py
+++ snapcraft/parts/desktop_file.py
@@ -66,7 +66,7 @@ class DesktopFile:
 
     @staticmethod
     def _normalize_icon_path(icon: str) -> str:
-        icon = icon.removeprefix("${SNAP}").lstrip("/")
+        return icon.removeprefix("${SNAP}").lstrip("/")
 
     def _parse_and_reformat_section_exec(self, section: str):
         exec_value = self._parser[section]["Exec"]
